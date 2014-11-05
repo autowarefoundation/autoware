2ecfe2feba03b8e10ac3265d5aa057edd1e455dd
Syohei YOSHIDA <syoshida@axe.bz>
2014-11-05 23:14:06 +0900

    Add node which take dummy image for debugging

---
+++ ros/sensing/drivers_sim/camera/src/image_viewer.cpp
@@ -0,0 +1,27 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include "ros/ros.h"
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+static void image_viewer_callback(const sensor_msgs::Image& image_source)
+{
+	const auto& encoding = sensor_msgs::image_encodings::TYPE_8UC3;
+	cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(image_source,
+							     encoding);
+	IplImage frame = cv_image->image;
+	cvShowImage("Image Viewer", &frame);
+	cvWaitKey(2);
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "image_viewer");
+	ros::NodeHandle n;
+	ros::Subscriber scriber = n.subscribe("/image_viewer", 1,
+					      image_viewer_callback);
+
+	ros::spin();
+	return 0;
+}
