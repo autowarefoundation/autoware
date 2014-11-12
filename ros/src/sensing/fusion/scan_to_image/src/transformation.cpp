51b057d47274a58b7f6eb5e9b247f970c1771de0
kondoh <kondoh@axe-inc.co.jp>
2014-11-12 11:15:04 +0900

    delete RTsched code

--- ros/src/sensing/fusion/scan_to_image/src/transformation.cpp
+++ ros/src/sensing/fusion/scan_to_image/src/transformation.cpp
@@ -60,45 +60,6 @@
 #include <sensor_msgs/LaserScan.h>
 #include <algorithm>
 
-struct sched_attr {
-    uint32_t size;
-    uint32_t sched_policy;
-    uint64_t sched_flags;
-
-    /* SCHED_NORMAL, SCHED_BATCH */
-    int32_t sched_nice;
-    /* SCHED_FIFO, SCHED_RR */
-    uint32_t sched_priority;
-    /* SCHED_DEADLINE */
-    uint64_t sched_runtime;
-    uint64_t sched_deadline;
-    uint64_t sched_period;
-};
-#ifdef __x86_64__
-#define __NR_sched_setattr              314
-#define __NR_sched_getattr              315
-#endif
-
-#ifdef __i386__
-#define __NR_sched_setattr              351
-#define __NR_sched_getattr              352
-#endif
-
-#ifdef __arm__
-#define __NR_sched_setattr              380
-#define __NR_sched_getattr              381
-#endif
-
-#ifndef SCHED_DEADLINE
-#define SCHED_DEADLINE          6
-#endif
-
-#ifndef SCHED_FLAG_RESET_ON_FORK
-#define SCHED_FLAG_RESET_ON_FORK        0x01
-#endif
-
-
-
 
 IplImage *IMAGE_PATH;
 char WINDOW_NAME[] = "CAR_TRACK";
