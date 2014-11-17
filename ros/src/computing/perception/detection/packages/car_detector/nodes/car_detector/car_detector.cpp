92b5955e77ca62c1d644159b25f4890561d337cc
Syohei YOSHIDA <syoshida@axe.bz>
2014-11-17 15:55:59 +0900

    fix topic names which are related to dpm

--- ros/src/computing/perception/detection/packages/car_detector/nodes/car_detector/car_detector.cpp
+++ ros/src/computing/perception/detection/packages/car_detector/nodes/car_detector/car_detector.cpp
@@ -5,7 +5,8 @@
 #define XSTR(x) #x
 #define STR(x) XSTR(x)
 
-extern int dpm_ocv_main(int argc, char* argv[]);
+extern int dpm_ocv_main(int argc, char* argv[],
+			const std::string& detection_type);
 extern int dpm_ttic_main(int argc, char* argv[], const char *cubin,
 			 const std::string& detection_type);
 
@@ -14,7 +15,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "car_detector");
 
   ros::NodeHandle n;
-
+  std::string detection_type("car");
   if(n.hasParam("/car_detector/algorithm")){
     std::string algo("ocv");
     n.getParam("/car_detector/algorithm", algo);
@@ -23,9 +24,8 @@ int main(int argc, char **argv)
       std::string cubin(STR(DEFAULT_CUBIN));
       if (n.hasParam("/car_detector/cubin"))
         n.getParam("/car_detector/cubin", cubin);
-      std::string detection_type("car");
       return dpm_ttic_main(argc, argv, cubin.c_str(), detection_type);
     }
   }
-  return dpm_ocv_main(argc, argv);
+  return dpm_ocv_main(argc, argv, detection_type);
 }
--- ros/src/computing/perception/detection/packages/pedestrian_detector/nodes/pedestrian_detector/pedestrian_detector.cpp
+++ ros/src/computing/perception/detection/packages/pedestrian_detector/nodes/pedestrian_detector/pedestrian_detector.cpp
@@ -5,7 +5,8 @@
 #define XSTR(x) #x
 #define STR(x) XSTR(x)
 
-extern int dpm_ocv_main(int argc, char* argv[]);
+extern int dpm_ocv_main(int argc, char* argv[],
+			const std::string& detection_type);
 extern int dpm_ttic_main(int argc, char* argv[], const char *cubin,
 			 const std::string& detection_type);
 
@@ -14,7 +15,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "pedestrian_detector");
 
   ros::NodeHandle n;
-
+  std::string detection_type("pedestrian");
   if(n.hasParam("/pedestrian_detector/algorithm")){
     std::string algo("ocv");
     n.getParam("/pedestrian_detector/algorithm", algo);
@@ -23,9 +24,8 @@ int main(int argc, char **argv)
       std::string cubin(STR(DEFAULT_CUBIN));
       if (n.hasParam("/pedestrian_detector/cubin"))
         n.getParam("/pedestrian_detector/cubin", cubin);
-      std::string detector_type("pedestrian");
-      return dpm_ttic_main(argc, argv, cubin.c_str(), detector_type);
+      return dpm_ttic_main(argc, argv, cubin.c_str(), detection_type);
     }
   }
-  return dpm_ocv_main(argc, argv);
+  return dpm_ocv_main(argc, argv, detection_type);
 }
