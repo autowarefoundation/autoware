f06e228726bc75f09acbf6a37255336a333a54da
AMC <abrahammonrroy@yahoo.com>
2014-12-01 18:37:13 +0900

    updated viewer to see dpmoutput

--- ros/src/util/packages/viewers/nodes/image_viewer/image_viewer.cpp
+++ ros/src/util/packages/viewers/nodes/image_viewer/image_viewer.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
@@ -5,14 +7,52 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
+#include "dpm/ImageObjects.h"
+
+using namespace std;
+using namespace cv;
+
+vector<Rect> cars;
+
 static void image_viewer_callback(const sensor_msgs::Image& image_source)
 {
 	const auto& encoding = sensor_msgs::image_encodings::TYPE_8UC3;
 	cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(image_source,
 							     encoding);
 	IplImage frame = cv_image->image;
+	
+	cout << "size:"<<cars.size()<<endl;
+	for(int i=0; i<cars.size();i++)
+	{
+		cout << "cars:" << cars[i] <<endl;
+		cvRectangle( &frame, 
+				cvPoint(cars[i].x, cars[i].y),
+				cvPoint(cars[i].x+cars[i].width, cars[i].y+cars[i].height),
+				Scalar( 0, 255, 0,0 ), 1, 8,0 );
+	}
+	
+
 	cvShowImage("Image Viewer", &frame);
 	cvWaitKey(2);
+}
+
+void rect_updater_callback(dpm::ImageObjects image_objects_msg)
+{
+	int num = image_objects_msg.car_num;
+	vector<int> points = image_objects_msg.corner_point; 
+	//points are X,Y,W,H and repeat for each instance
+	
+	for (int i=0; i<num;i++)
+	{
+		Rect tmp;
+		tmp.x = points[i*4 + 0];
+		tmp.y = points[i*4 + 1];
+		tmp.width = points[i*4 + 2];
+		tmp.height = points[i*4 + 3];
+		cars.push_back(tmp);
+		
+	}
+	cout << "cars:"<< cars.size() <<endl;
 }
 
 int main(int argc, char **argv)
@@ -21,7 +61,10 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	ros::Subscriber scriber = n.subscribe("/image_raw", 1,
 					      image_viewer_callback);
+	ros::Subscriber scriber_car = n.subscribe("/car_pos_xy", 1,
+					      rect_updater_callback); 
 
 	ros::spin();
 	return 0;
 }
+
