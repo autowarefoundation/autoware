bad8655c0d2e1584e36bf6aff0a58d5418463c0f
h_ohta <h_ohta@ertl.jp>
2014-12-02 21:17:13 +0900

    fix sensor running script

--- ros/src/util/scripts/running_default_sensor.sh
+++ ros/src/util/scripts/running_default_sensor.sh
@@ -4,10 +4,10 @@ echo "load ros user settings..."
 source devel/setup.bash
 
 echo "run velodyne HDL-64E S2 driver..."
-roslaunch velodyne velodyne_hdl64e.launch
+gnome-terminal --geometry=20x20+0+0 --title "hdl64" -e "sh -c'roslaunch velodyne velodyne_hdl64e.launch'"
 
 echo "run gnss driver..."
-rosrun javad gnss.sh
+gnome-terminal --geometry=20x20+450+0 --title "gnss" -e "sh -c'rosrun javad gnss.sh'"
 
 echo "run pointgrey driver..."
-roslaunch pointgrey grasshopper3.launch
+gnome-terminal --geometry=20x20+1000+0 --title "grasshopper" -e "sh -c'roslaunch pointgrey grasshopper3.launch'"
