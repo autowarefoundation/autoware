76f270cc4a874a09df023dc76a0d7e53f226f154
h_ohta <h_ohta@ertl.jp>
2014-12-05 18:49:26 +0900

    RMCセンテンスもtopicに投げれるように変更。他微調整

--- ros/src/sensing/drivers/gnss/packages/javad/scripts/gnss.sh
+++ ros/src/sensing/drivers/gnss/packages/javad/scripts/gnss.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo ""em,,msg/nmea/GGA" > /dev/ttyUSB0"
-echo "em,,msg/nmea/GGA" > /dev/ttyUSB0
+echo ""em,,msg/nmea/GGA:0.04" > /dev/ttyUSB0"
+echo "em,,msg/nmea/GGA:0.04" > /dev/ttyUSB0
+
+echo "sleep 3 second"
+sleep 3s
+
+echo ""em,,msg/nmea/RMC:0.04" > /dev/ttyUSB0"
+echo "em,,msg/nmea/RMC" > /dev/ttyUSB0
+
+echo "sleep 3 second"
+sleep 3s
 
 echo "launch gnss driver..."
 roslaunch javad nmea_navsat.launch
