ee7989b1f765bd6b206c3be16741cd8612bd881d
syouji <syouji@axe.bz>
2014-12-09 21:11:00 +0900

    Add error_info handling routine

--- ros/src/state/packages/ui_socket/nodes/ui_sender/ui_sender.cpp
+++ ros/src/state/packages/ui_socket/nodes/ui_sender/ui_sender.cpp
@@ -1,46 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <sys/time.h>
 
-//#undef NDEBUG
-#include <assert.h>
+#include <ros/ros.h>
+#include <ros/console.h>
 
-#include "ros/ros.h"
-#include "ui_socket/error_info.h"
+#include <ui_socket/error_info.h>
 
-static constexpr uint32_t_QUEUE_SIZE = 1000;
+static constexpr int ERROR_INFO_TYPE = 3;
 static constexpr int DEFAULT_PORT = 23456;
+static constexpr uint32_t QUEUE_SIZE = 1000;
+static constexpr int LISTEN_BACKLOG = 10;
 
-static port;
+static int port;
+static int connfd;
+static volatile bool socket_ok;
 
 static void subscribe_error_info(const ui_socket::error_info& msg)
 {
-	int info[2];
+	int request[2];
+	int response;
+	ssize_t nbytes;
 
-	info[0] = 3;
-	info[1] = msg.error;
+	request[0] = ERROR_INFO_TYPE;
+	request[1] = msg.error;
+
+	nbytes = send(connfd, request, sizeof(request), 0);
+	if (nbytes < 0) {
+		ROS_ERROR("send: %s", strerror(errno));
+		socket_ok = false;
+		return;
+	}
+	if ((size_t)nbytes < sizeof(request)) {
+		ROS_WARN("send: %zd bytes remaining",
+			 sizeof(request) - nbytes);
+		return;
+	}
+
+	nbytes = recv(connfd, &response, sizeof(response), 0);
+	if (nbytes < 0) {
+		ROS_ERROR("recv: %s", strerror(errno));
+		socket_ok = false;
+		return;
+	}
+	if ((size_t)nbytes < sizeof(response)) {
+		ROS_WARN("recv: %zd bytes remaining",
+			 sizeof(response) - nbytes);
+		return;
+	}
 }
 
 int main(int argc, char **argv)
 {
-	int sock;
-	int asock;
+	int listenfd;
+	sockaddr_in addr;
 
 	ros::init(argc, argv, "ui_sender");
 
 	ros::NodeHandle n;
-
 	n.param<int>("ui_sender/port", port, DEFAULT_PORT);
+	if (!(port >= 0 && port < 65536)) {
+		ROS_ERROR("Invalid port value %d\n", port);
+		return -1;
+	}
 
-	ros::Subscriber sub;
+	listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (listenfd < 0) {
+		ROS_ERROR("socket: %s", strerror(errno));
+		return -1;
+	}
 
-	sub = n.subscribe("error_info", QUEUE_SIZE, subscribe_error_info);
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	ros::spin();
+	if (bind(listenfd, (const struct sockaddr *)&addr,
+		 sizeof(addr)) < 0) {
+		ROS_ERROR("bind: %s", strerror(errno));
+		close(listenfd);
+		return -1;
+	}
+
+	if (listen(listenfd, LISTEN_BACKLOG) < 0) {
+		ROS_ERROR("listen: %s", strerror(errno));
+		close(listenfd);
+		return -1;
+	}
+
+	ros::Subscriber sub = n.subscribe("error_info", QUEUE_SIZE,
+					  subscribe_error_info);
+
+	while (true) {
+		connfd = accept(listenfd, (struct sockaddr *)nullptr,
+				nullptr);
+		if (connfd < 0) {
+			ROS_ERROR("accept: %s", strerror(errno));
+			close(listenfd);
+			return -1;
+		}
+
+		socket_ok = true;
+		while (socket_ok) {
+			ros::spinOnce();
+		}
+
+		close(connfd);
+	}
+
+	close(listenfd);
 
 	return 0;
 }
