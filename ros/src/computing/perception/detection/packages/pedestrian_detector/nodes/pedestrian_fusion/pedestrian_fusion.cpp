6d857dcd23721952ef378cd4a7aa2aa6de0d2c75
yukihiro saito <yukky@ubi.cs.ritsumei.ac.jp>
2015-01-06 05:55:03 -0800

    Chage from *_pos_xy topic to *_pos_xy_tracked topic

--- ros/src/computing/perception/detection/packages/pedestrian_detector/nodes/pedestrian_fusion/pedestrian_fusion.cpp
+++ ros/src/computing/perception/detection/packages/pedestrian_detector/nodes/pedestrian_fusion/pedestrian_fusion.cpp
@@ -104,7 +104,7 @@ int main(int argc, char **argv)
      * away the oldest ones.
      */
 
-    ros::Subscriber pedestrian_pos_xy_sub = n.subscribe("pedestrian_pos_xy", 1, ImageObjectsCallback);
+    ros::Subscriber pedestrian_pos_xy_sub = n.subscribe("pedestrian_pos_xy_tracked", 1, ImageObjectsCallback);
     ros::Subscriber scan_image_sub = n.subscribe("scan_image", 1, ScanImageCallback);
     ros::Subscriber points_image_sub =n.subscribe("points_image", 1, PointsImageCallback);
 #if _DEBUG
