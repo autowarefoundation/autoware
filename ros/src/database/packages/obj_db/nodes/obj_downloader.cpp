b1d5e3ec6cd7835692b6fdea34ad06d3fcc3bd17
pdsljp <pdsljp@gmail.com>
2015-01-08 19:45:21 +0900

    update sample_mobility 	subscribe /mo 	convert lat, lon to x, y 	publish marker

--- ros/src/database/packages/obj_db/nodes/obj_downloader.cpp
+++ ros/src/database/packages/obj_db/nodes/obj_downloader.cpp
@@ -1,13 +1,122 @@
  //obj_donwloaderのサンプルソースファイルです
 
+#include "std_msgs/String.h"
 #include "ros/ros.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <pthread.h>
+#include <vector>
+#include <boost/array.hpp>
 #include <iostream>
-
-/*void Callback_Name(topic_type topic_name){
-  
-}
+#include <string>
+#include <sstream>
+/*
+#include "opencv/cv.h" 
+#include "opencv/highgui.h" 
+#include "opencv/cxcore.h" 
+#include "std_msgs/Float64.h"
+#include "scan_to_image/ScanImage.h"
 */
+#include "SendData.h"
+
+long startTime[10] = {
+    1255698868,
+    1255723190,
+    1266425473,
+    1266765883,
+    1266851714,
+    1266938129,
+    1267471638,
+    1267542268,
+    1267715826,
+    1268755256
+};
+
+
+
+ros::Publisher pub;
+
+char serverName[100] = "db1.ertl.jp";
+std::string dbres;
+SendData sd;
+bool testFlag;
+int counter;
+
+std::vector<std::string> split(const std::string& input, char delimiter)
+{
+    std::istringstream stream(input);
+
+    std::string field;
+    std::vector<std::string> result;
+    while (std::getline(stream, field, delimiter)) {
+        result.push_back(field);
+    }
+    return result;
+}
+
+//wrap SendData class
+void* wrapSender(void *tsd){
+
+  //I assume that values has 4 value ex: "0 0 0 0"   "1 2 3 4"
+  //And if setting the other number of value , sendData will be failed.
+
+  std::string data;
+  std::ostringstream oss;
+  if(testFlag){
+    oss << "select order\t" << counter << "<E>";
+    data = oss.str();
+    sd.setValue(data);
+    printf("test\n");
+    counter++;
+  }else{
+    data = "select order<E>";
+    sd.setValue(data);
+  }
+  dbres = sd.Sender();
+
+  printf("%lu\n",dbres.size());
+
+  std_msgs::String msg;
+  std::stringstream ss;
+  ss << dbres;
+  msg.data = ss.str();
+
+  if(msg.data.compare("") != 0){
+    ROS_INFO("test\t%s",msg.data.c_str());
+    pub.publish(msg);
+  }
+
+  /*
+  std::vector<std::string>::iterator itr = dbres.begin();
+  while(itr != dbres.end()){
+    std::string temp = *itr;
+    separateData = split(temp,'\t');
+    std::cout << separateData[1] << std::endl;
+    std::cout << *itr << std::endl;
+    itr++;
+  }
+  */
+
+}
+
+void* intervalCall(void *a){
+
+  pthread_t th;
+
+  while(1){
+    //create new thread for socket communication.      
+    if(pthread_create(&th, NULL, wrapSender, NULL)){
+      printf("thread create error\n");
+    }
+    sleep(1);
+    if(pthread_join(th,NULL)){
+      printf("thread join error.\n");
+    }
+  }
+}
+
 
 int main(int argc, char **argv){
   
@@ -15,10 +124,32 @@ int main(int argc, char **argv){
   ros::NodeHandle nh;
   
   std::cout << "obj_downloader" << std::endl;
- 
-  
+
+  pub = nh.advertise<std_msgs::String>("mo",1000); 
   //ros::Subscriber subscriber = nh,subscribe("topic_name",1000,Callback_Name)
-  
+
+
+  if(argc == 1){
+    printf("normal execution\n");
+    testFlag = false;
+  }else if(argc == 2){
+    std::string argFlag = argv[1];
+    if(argFlag.compare("--test") == 0){
+      printf("test access\n");
+      testFlag = true;
+    }else{
+      printf("invalid argment\n");
+      testFlag = false;
+    }
+  }
+  sd = SendData(serverName,5678);
+  counter = 0;
+
+  pthread_t th;
+  if(pthread_create(&th, NULL, intervalCall, NULL)){
+    printf("thread create error\n");
+  }
 
   ros::spin();
+
 }
--- ros/src/util/packages/sample_data/nodes/sample_mobility/sample_mobility.cpp
+++ ros/src/util/packages/sample_data/nodes/sample_mobility/sample_mobility.cpp
@@ -1,178 +1,133 @@
 #include <fstream>
-#include "ros/ros.h"
+#include <string.h>
 #include <visualization_msgs/Marker.h>
+#include <std_msgs/String.h>
 #include <geo_pos_conv.hh>
 
-#define SELF_TRANS	0
-int swap_x_y = 0;
+#include "ros/ros.h"
 
-typedef std::vector<std::vector<std::string>> Tbl;
+ros::Publisher pub;
 
-Tbl read_csv(const char* filename)
-{
-  std::ifstream ifs(filename);
-  std::string line;
-
-  Tbl tbl;
-
-  while (std::getline(ifs, line)) {
-    std::istringstream ss(line);
-
-    std::vector<std::string> columns;
-    std::string column;
-    while (std::getline(ss, column, ',')) {
-      columns.push_back(column);
-    }
-    if(columns[0].compare("$GPGGA") != 0) continue;
-
-    if(columns[2] != "" && columns[4] != "" && columns[9] != "") {
-      tbl.push_back(columns);
-    }
-  }
-  return tbl;
-}
-
-/* for gnssdat.acsv */
-struct GnssData {
+typedef struct{
+  int gps_id;
+  double lat;
+  double lon;
+  double ele;
+  std::string timestamp;
   double x;
   double y;
   double z;
-};
+}car_info;
 
-
-std::vector<GnssData> read_gnssdata(const char* filename)
+std::vector<std::string> split(const std::string& input, char delimiter)
 {
-  Tbl tbl = read_csv(filename);
-  size_t i, n = tbl.size();
-  std::vector<GnssData> ret(n);
+    std::istringstream stream(input);
 
-  for (i=0; i<n; i++) {
-    ret[i].x = std::stod(tbl[i][2]);
-    ret[i].y = std::stod(tbl[i][4]);
-    ret[i].z = std::stod(tbl[i][9]);
-  }
-
-  return ret;
-}
-
-
-void set_marker_data(visualization_msgs::Marker* marker,
-		    double px, double py, double pz, double ox, double oy, double oz, double ow,
-		    double sx, double sy, double sz, double r, double g, double b, double a)
-{
-  if(swap_x_y) {
-    marker->pose.position.x = py;
-    marker->pose.position.y = px;
-    marker->pose.orientation.x = oy;
-    marker->pose.orientation.y = ox;
-  } else {
-    marker->pose.position.x = px;
-    marker->pose.position.y = py;
-    marker->pose.orientation.x = ox;
-    marker->pose.orientation.y = oy;
-  }
-  marker->pose.position.z = pz;
-
-  marker->pose.orientation.z = oz;
-  marker->pose.orientation.w = ow;
-
-  marker->scale.x = sx;
-  marker->scale.y = sy;
-  marker->scale.z = sz;
-
-  marker->color.r = r;
-  marker->color.g = g;
-  marker->color.b = b;
-  marker->color.a = a;
-}
-
-void publish_marker(visualization_msgs::Marker* marker,
-		    ros::Publisher& pub,
-		    ros::Rate& rate)
-{
-#if SELF_TRANS
-    if(swap_x_y) {
-      marker->pose.position.x += 16635;
-      marker->pose.position.y += 86432;
-    } else {
-      marker->pose.position.x += 86432;
-      marker->pose.position.y += 16635;
+    std::string field;
+    std::vector<std::string> result;
+    while (std::getline(stream, field, delimiter)) {
+        result.push_back(field);
     }
-    marker->pose.position.z += -50; // -50
-#endif
+    return result;
+}
 
-    ros::ok();
-    pub.publish(*marker);
+void db_callback(const std_msgs::String::ConstPtr &msg)
+{
+  std::vector<car_info> cars;
+  std::vector<std::string> db_data, tmp;
+  visualization_msgs::Marker sphere_list;
+  car_info a;
+
+  sphere_list.header.frame_id = "/mobility";
+  sphere_list.header.stamp = ros::Time::now();
+  sphere_list.ns = "mo_marker";
+  sphere_list.id = 0;
+  sphere_list.type = visualization_msgs::Marker::SPHERE_LIST;
+  sphere_list.action = visualization_msgs::Marker::ADD;  
+  sphere_list.lifetime = ros::Duration();
+  sphere_list.color.r = 1.0;
+  sphere_list.color.g = 0.0;
+  sphere_list.color.b = 0.0;
+  sphere_list.color.a = 1.0;
+  sphere_list.scale.x = 10.0;
+  sphere_list.scale.y = 10.0;
+  sphere_list.scale.z = 10.0;
+
+  ros::Rate rate(50);
+
+  geo_pos_conv geo;
+  geo.set_plane(7);
+
+  // Loading data.
+  db_data = split(msg->data.c_str(),',');
+
+  for(int i = 0; i < (int)db_data.size(); i++){
+    geometry_msgs::Point p;
+    
+    if(db_data[i].compare("")==0) continue;
+    tmp = split(db_data[i], '\t');
+    if(tmp.size()!=5) continue;
+
+    a.gps_id = std::stoi(tmp[0]);
+    a.lat = std::stod(tmp[1].c_str());
+    a.lon = std::stod(tmp[2].c_str());
+    a.ele = std::stod(tmp[3].c_str());
+    a.timestamp = tmp[4];
+
+    // Convert from lat,lon to x,y
+    // geo.set_llh_nmea_degrees(a.lat, a.lon, a.ele);
+    geo.llh_to_xyz(a.lat, a.lon, a.ele);
+    a.x = geo.x();
+    a.y = geo.y();
+    a.z = geo.z();
+    p.x = a.x;
+    p.y = a.y;
+    p.z = a.z;
+
+    cars.push_back(a);
+    sphere_list.points.push_back(p);
+
+    std::cout << "gps_id: " << cars[i].gps_id << std::endl;
+    std::cout << "lat: " << cars[i].lat << std::endl;
+    std::cout << "lon: " << cars[i].lon << std::endl;
+    std::cout << "ele: " << cars[i].ele << std::endl;
+    std::cout << "timestamp: " << cars[i].timestamp << std::endl;
+    std::cout << "x: " << cars[i].x << std::endl;
+    std::cout << "y: " << cars[i].y << std::endl;
+    std::cout << "z: " << cars[i].z << std::endl;
+  }
+
+  // Publish data
+  while(ros::ok()){
+
+    pub.publish(sphere_list);
+
+    ros::spinOnce();
+
     rate.sleep();
-    //    marker->id++;
+  }
+
+  // Free vector
+  std::vector<car_info>().swap(cars);
+
 }
 
 
 int main(int argc, char **argv)
 {
-
-/*
-
-#!/bin/sh
-rosrun sample_data sample_trajectory gnss.log <swap_x_y_off|swap_x_y_on>
-
-# EOF
-
-*/
+  std::cout << "-------------------" << std::endl;
+  std::cout << "sample_mobility.cpp" << std::endl;
+  std::cout << "-------------------" << std::endl;
 
   ros::init(argc, argv, "sample_mobility");
+
   ros::NodeHandle n;
-  ros::Publisher pub = n.advertise<visualization_msgs::Marker>("/vector_map", 10, true);
+  pub = n.advertise<visualization_msgs::Marker>("mo_marker", 10);
 
+  // Subscribing the data from DB
+  ros::Subscriber sub = n.subscribe("mo", 1000, db_callback);
 
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " csv_file"
-              << std::endl;
-    std::exit(1);
-  }
-
-  geo_pos_conv geo;
-  std::vector<GnssData> gnssdatas = read_gnssdata(argv[1]);
-
-  if(std::string(argv[2]) == "swap_x_y_on") {
-    printf("swap_x_y: on\n");
-    swap_x_y = 1;
-  } else {
-    printf("swap_x_y : off\n");
-  }
-
-  visualization_msgs::Marker marker;
-#if SELF_TRANS
-  marker.header.frame_id = "/map2";
-#else
-  marker.header.frame_id = "/map";
-#endif
-  marker.header.stamp = ros::Time::now();
-
-  marker.ns = "vector_map";
-  marker.id = 0;
-  marker.action = visualization_msgs::Marker::ADD;
-  marker.lifetime = ros::Duration();
-
-  marker.type = visualization_msgs::Marker::SPHERE;
-
-  ros::Rate rate(50);
-
-  size_t i;
-
-  // show gnssdata
-  geo.set_plane(7);
-  for (i=0; i<gnssdatas.size(); i++) {
-    geo.set_llh_nmea_degrees(gnssdatas[i].x,gnssdatas[i].y,gnssdatas[i].z);
-    set_marker_data(&marker,
-		    geo.x()-1.0, 
-		    geo.y()-1.0, 
-		    geo.z()-1.0,
-		    0, 0, 0, 1,
-		    2.0, 2.0, 2.0,
-		    0, 1, 0, 1);
-    publish_marker(&marker, pub, rate);
-  }
+  ros::spin();
 
   return 0;
 }
