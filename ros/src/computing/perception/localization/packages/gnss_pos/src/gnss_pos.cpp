e10ab5b9ffb3f120bdb90537e10c763065540e7b
Hiroki Ohta <h_ohta@ertl.jp>
2015-01-20 00:26:09 +0900

    renamed global values ex)'_values'

--- ros/src/computing/perception/localization/packages/gnss_pos/src/gnss_pos.cpp
+++ ros/src/computing/perception/localization/packages/gnss_pos/src/gnss_pos.cpp
@@ -11,66 +11,76 @@
 
 ros::Publisher pose_publisher;
 
-geometry_msgs::PoseStamped prev_pose;
-geometry_msgs::Quaternion quat;
-double yaw ;
+geometry_msgs::PoseStamped _prev_pose;
+geometry_msgs::Quaternion _quat;
+double yaw;
 
-void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg){
+void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
+{
 
-  geo_pos_conv geo;
+    geo_pos_conv geo;
 
-  geo.set_plane(7);
-  geo.llh_to_xyz(msg->latitude, msg->longitude, msg->altitude);
-  
-  static tf::TransformBroadcaster pose_broadcaster;
-  tf::Transform pose_transform;
-  tf::Quaternion pose_q;
+    geo.set_plane(7);
+    geo.llh_to_xyz(msg->latitude, msg->longitude, msg->altitude);
 
-  pose_transform.setOrigin(tf::Vector3(0,0,0));
-  pose_q.setRPY(0,0,0);
-  pose_transform.setRotation(pose_q);
-  pose_broadcaster.sendTransform(tf::StampedTransform(pose_transform,ros::Time::now(),"map","gps"));
+    static tf::TransformBroadcaster pose_broadcaster;
+    tf::Transform pose_transform;
+    tf::Quaternion pose_q;
 
-  geometry_msgs::PoseStamped pose;
-  pose.header = msg->header;
-  pose.header.stamp = ros::Time::now();
-  pose.header.frame_id = "gps";
-  pose.pose.position.x = geo.y();
-  pose.pose.position.y = geo.x();
-  pose.pose.position.z = geo.z();
+    pose_transform.setOrigin(tf::Vector3(0, 0, 0));
+    pose_q.setRPY(0, 0, 0);
+    pose_transform.setRotation(pose_q);
+    pose_broadcaster.sendTransform(
+            tf::StampedTransform(pose_transform, ros::Time::now(), "map",
+                    "gps"));
 
-  double distance =sqrt(pow(geo.x()-prev_pose.pose.position.y,2) + pow(geo.y()-prev_pose.pose.position.x,2)); 
-  std::cout << "distance : " << distance << std ::endl;
+    geometry_msgs::PoseStamped pose;
+    pose.header = msg->header;
+    pose.header.stamp = ros::Time::now();
+    pose.header.frame_id = "gps";
+    pose.pose.position.x = geo.y();
+    pose.pose.position.y = geo.x();
+    pose.pose.position.z = geo.z();
 
-  if(distance > 0.2){
-    yaw = atan2(geo.x()-prev_pose.pose.position.y,geo.y()-prev_pose.pose.position.x);
-    quat = tf::createQuaternionMsgFromYaw(
-					  yaw);
-    prev_pose = pose;
-  }
-  
-  pose.pose.orientation = quat;
-  pose_publisher.publish(pose);
-  
+    double distance = sqrt(
+            pow(pose.pose.position.y - _prev_pose.pose.position.y, 2)
+                    + pow(pose.pose.position.x - _prev_pose.pose.position.x,
+                            2));
+    std::cout << "distance : " << distance << std::endl;
 
-  //座標変換
-  static tf::TransformBroadcaster br;
-  tf::Transform transform;
-  tf::Quaternion q;
-  transform.setOrigin(tf::Vector3(pose.pose.position.x, pose.pose.position.y, pose.pose.position.z));
-  q.setRPY(0,0,yaw);
-  transform.setRotation(q);
-  br.sendTransform(tf::StampedTransform(transform,ros::Time::now(),"map","gps_frame"));
+    if (distance > 0.2) {
+        yaw = atan2(pose.pose.position.y - _prev_pose.pose.position.y,
+                pose.pose.position.x - _prev_pose.pose.position.x);
+        _quat = tf::createQuaternionMsgFromYaw(yaw);
+        _prev_pose = pose;
+    }
+
+    pose.pose.orientation = _quat;
+    pose_publisher.publish(pose);
+
+    //座標変換
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    tf::Quaternion q;
+    transform.setOrigin(
+            tf::Vector3(pose.pose.position.x, pose.pose.position.y,
+                    pose.pose.position.z));
+    q.setRPY(0, 0, yaw);
+    transform.setRotation(q);
+    br.sendTransform(
+            tf::StampedTransform(transform, ros::Time::now(), "map",
+                    "gps_frame"));
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "pose_visualizer");
-  ros::NodeHandle nh;
-  pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("gnss_pose",1000);
-  ros::Subscriber gnss_pose_subscriber = nh.subscribe("fix",100,GNSSCallback);
+    ros::init(argc, argv, "gnss_pos");
+    ros::NodeHandle nh;
+    pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("gnss_pose",
+            1000);
+    ros::Subscriber gnss_pose_subscriber = nh.subscribe("fix", 100,
+            GNSSCallback);
 
-  
-  ros::spin();
-  return 0;
+    ros::spin();
+    return 0;
 }
