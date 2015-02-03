e1da09b291a8eab825dc29de5a9a48b076ca1c2c
syouji <syouji@axe.bz>
2015-02-03 20:34:09 +0900

    Set timeouts for connect and recv

--- ui/tablet/AutowareTouch/jni/sound_sender.c
+++ ui/tablet/AutowareTouch/jni/sound_sender.c
@@ -1,6 +1,9 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <jni.h>
 #include <stdlib.h>
 
+#include <sys/select.h>
 #include <sys/socket.h>
 
 #include <netinet/in.h>
@@ -13,11 +16,17 @@ Java_com_ghostagent_SoundManagementNative_socket(JNIEnv *env, jobject obj)
 
 JNIEXPORT jint JNICALL
 Java_com_ghostagent_SoundManagementNative_connect(JNIEnv *env, jobject obj,
-						  jint sockfd, jstring address,
-						  jint port)
+						  jint sockfd, jint timeout,
+						  jstring address, jint port)
 {
 	const char *cp;
 	struct sockaddr_in addr;
+	int flags;
+	int ret;
+	fd_set wset;
+	struct timeval tv;
+	int error;
+	socklen_t len;
 
 	cp = (*env)->GetStringUTFChars(env, address, NULL);
 	if (cp == NULL)
@@ -30,7 +39,43 @@ Java_com_ghostagent_SoundManagementNative_connect(JNIEnv *env, jobject obj,
 
 	(*env)->ReleaseStringUTFChars(env, address, cp);
 
-	return (jint)connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	flags = fcntl(sockfd, F_GETFL);
+	if (flags < 0)
+		return -1;
+
+	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) < 0)
+		return -1;
+
+	ret = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	if (ret == 0) {
+		fcntl(sockfd, F_SETFL, flags);
+		return 0;
+	}
+	if (errno != EINPROGRESS) {
+		fcntl(sockfd, F_SETFL, flags);
+		return -1;
+	}
+
+	FD_ZERO(&wset);
+	FD_SET(sockfd, &wset);
+
+	memset(&tv, 0, sizeof(tv));
+	tv.tv_sec = (time_t)timeout;
+
+	if (select(sockfd + 1, NULL, &wset, NULL, &tv) <= 0) {
+		fcntl(sockfd, F_SETFL, flags);
+		return -1;
+	}
+
+	len = sizeof(error);
+	ret = getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len);
+	if (ret < 0 || error != 0) {
+		fcntl(sockfd, F_SETFL, flags);
+		return -1;
+	}
+
+	fcntl(sockfd, F_SETFL, flags);
+	return 0;
 }
 
 JNIEXPORT jint JNICALL
@@ -42,103 +87,105 @@ Java_com_ghostagent_SoundManagementNative_close(JNIEnv *env, jobject obj,
 
 JNIEXPORT void JNICALL
 Java_com_ghostagent_SoundManagementNative_sendInt(JNIEnv *env, jobject obj,
-						  jint sockfd, jint arg0)
+						  jint sockfd, jint timeout,
+						  jint arg0)
 {
+	fd_set wset;
+	struct timeval tv;
 	int buf[1];
-	uint8_t *bufp;
-	size_t bufsize;
-	ssize_t nbytes;
+
+	FD_ZERO(&wset);
+	FD_SET(sockfd, &wset);
+
+	memset(&tv, 0, sizeof(tv));
+	tv.tv_sec = (time_t)timeout;
+
+	if (select(sockfd + 1, NULL, &wset, NULL, &tv) <= 0)
+		return;
 
 	buf[0] = arg0;
 
-	bufp = (uint8_t *)buf;
-	bufsize = sizeof(buf);
-	while (bufsize) {
-		nbytes = send(sockfd, bufp, bufsize, 0);
-		if (nbytes < 0) {
-			break;
-		}
-		bufp += nbytes;
-		bufsize -= nbytes;
-	}
+	send(sockfd, buf, sizeof(buf), 0);
 }
 
 JNIEXPORT void JNICALL
 Java_com_ghostagent_SoundManagementNative_sendIntTuple(JNIEnv *env,
 						       jobject obj,
 						       jint sockfd,
+						       jint timeout,
 						       jint arg0,
 						       jint arg1)
 {
+	fd_set wset;
+	struct timeval tv;
 	int buf[2];
-	uint8_t *bufp;
-	size_t bufsize;
-	ssize_t nbytes;
+
+	FD_ZERO(&wset);
+	FD_SET(sockfd, &wset);
+
+	memset(&tv, 0, sizeof(tv));
+	tv.tv_sec = (time_t)timeout;
+
+	if (select(sockfd + 1, NULL, &wset, NULL, &tv) <= 0)
+		return;
 
 	buf[0] = arg0;
 	buf[1] = arg1;
 
-	bufp = (uint8_t *)buf;
-	bufsize = sizeof(buf);
-	while (bufsize) {
-		nbytes = send(sockfd, bufp, bufsize, 0);
-		if (nbytes < 0) {
-			break;
-		}
-		bufp += nbytes;
-		bufsize -= nbytes;
-	}
+	send(sockfd, buf, sizeof(buf), 0);
 }
 
 JNIEXPORT void JNICALL
 Java_com_ghostagent_SoundManagementNative_sendDoubleArray(JNIEnv *env,
 							  jobject obj,
 							  jint sockfd,
+							  jint timeout,
 							  jdoubleArray arg0)
 {
+	fd_set wset;
+	struct timeval tv;
 	jdouble *buf;
-	uint8_t *bufp;
-	size_t bufsize;
-	ssize_t nbytes;
+
+	FD_ZERO(&wset);
+	FD_SET(sockfd, &wset);
+
+	memset(&tv, 0, sizeof(tv));
+	tv.tv_sec = (time_t)timeout;
+
+	if (select(sockfd + 1, NULL, &wset, NULL, &tv) <= 0)
+		return;
 
 	buf = (*env)->GetDoubleArrayElements(env, arg0, NULL);
 	if (buf == NULL)
 		return;
 
-	bufp = (uint8_t *)buf;
-	bufsize = sizeof(jdouble) * (*env)->GetArrayLength(env, arg0);
-	while (bufsize) {
-		nbytes = send(sockfd, bufp, bufsize, 0);
-		if (nbytes < 0) {
-			break;
-		}
-		bufp += nbytes;
-		bufsize -= nbytes;
-	}
+	send(sockfd, buf,
+	     sizeof(jdouble) * (*env)->GetArrayLength(env, arg0), 0);
 
 	(*env)->ReleaseDoubleArrayElements(env, arg0, buf, 0);
 }
 
 JNIEXPORT jint JNICALL
 Java_com_ghostagent_SoundManagementNative_recvInt(JNIEnv *env, jobject obj,
-						  jint sockfd)
+						  jint sockfd, jint timeout)
 {
+	fd_set rset;
+	struct timeval tv;
 	int buf[1];
-	uint8_t *bufp;
-	size_t bufsize;
 	ssize_t nbytes;
 
-	bufp = (uint8_t *)buf;
-	bufsize = sizeof(buf);
-	while (bufsize) {
-		nbytes = recv(sockfd, bufp, bufsize, 0);
-		if (nbytes < 0) {
-			buf[0] = -1;
-			break;
-		}
-		bufp += nbytes;
-		bufsize -= nbytes;
-	}
+	FD_ZERO(&rset);
+	FD_SET(sockfd, &rset);
+
+	memset(&tv, 0, sizeof(tv));
+	tv.tv_sec = (time_t)timeout;
+
+	if (select(sockfd + 1, &rset, NULL, NULL, &tv) <= 0)
+		return -1;
+
+	nbytes = recv(sockfd, buf, sizeof(buf), 0);
+	if (nbytes != sizeof(buf))
+		return -1;
 
 	return (jint)buf[0];
 }
