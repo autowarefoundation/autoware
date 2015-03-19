d79647f00e64f9270c2992441d85b43d775f8994
Syohei YOSHIDA <syohex@gmail.com>
2015-03-19 17:17:08 +0900

    Add library for publishing message to 'points_image'

--- ros/src/sensing/fusion/packages/points2image/include/points_image.hpp
+++ ros/src/sensing/fusion/packages/points2image/include/points_image.hpp
@@ -1,3 +1,6 @@
+#ifndef _POINTS_IMAGE_H_
+#define _POINTS_IMAGE_H_
+
 /*
  *  Copyright (c) 2015, Nagoya University
  *  All rights reserved.
@@ -28,68 +31,19 @@
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
 #include <sensor_msgs/PointCloud2.h>
 #include "points2image/PointsImage.h"
 #include "points2image/CameraExtrinsic.h"
 
-#include <points_image.hpp>
+points2image::PointsImage
+pointcloud2_to_image(const sensor_msgs::PointCloud2ConstPtr& pointclound2,
+		     const cv::Mat& cameraExtrinsicMat,
+		     const cv::Mat& cameraMat, const cv::Mat& distCoeff,
+		     const cv::Size& imageSize);
 
-#define CAMERAEXTRINSICMAT "CameraExtrinsicMat"
-#define CAMERAMAT "CameraMat"
-#define DISTCOEFF "DistCoeff"
-#define IMAGESIZE "ImageSize"
+points2image::CameraExtrinsic
+pointcloud2_to_3d_calibration(const sensor_msgs::PointCloud2ConstPtr& pointclound2,
+			      const cv::Mat& cameraExtrinsicMat);
 
-#define IMAGE_WIDTH 800
-#define IMAGE_HEIGHT 640
-
-static cv::Mat cameraExtrinsicMat;
-static cv::Mat cameraMat;
-static cv::Mat distCoeff;
-static cv::Size imageSize;
-
-static ros::Publisher pub;
-static ros::Publisher cpub;
-
-static void callback(const sensor_msgs::PointCloud2ConstPtr& msg)
-{
-	points2image::PointsImage pub_msg
-		= pointcloud2_to_image(msg, cameraExtrinsicMat, cameraMat,
-				       distCoeff, imageSize);
-	pub.publish(pub_msg);
-
-	points2image::CameraExtrinsic cpub_msg
-		= pointcloud2_to_3d_calibration(msg, cameraExtrinsicMat);
-	cpub.publish(cpub_msg);
-}
-
-int main(int argc, char *argv[])
-{
-	ros::init(argc, argv, "points2image");
-	ros::NodeHandle n;
-
-	if(argc < 2){
-		std::cout<<"Need calibration filename as the first parameter.";
-		return 0;
-	}
-
-	cv::FileStorage fs(argv[1], cv::FileStorage::READ);
-	if(!fs.isOpened()){
-		std::cout<<"Invalid calibration filename.";
-		return 0;
-	}
-
-	fs[CAMERAEXTRINSICMAT] >> cameraExtrinsicMat;
-	fs[CAMERAMAT] >> cameraMat;
-	fs[DISTCOEFF] >> distCoeff;
-	//fs[IMAGESIZE] >> imageSize;
-	imageSize.width = IMAGE_WIDTH;
-	imageSize.height = IMAGE_HEIGHT;
-
-	pub = n.advertise<points2image::PointsImage>("points_image", 10);
-	cpub = n.advertise<points2image::CameraExtrinsic>("threeD_calibration", 1);
-	ros::Subscriber sub = n.subscribe("velodyne_points", 1, callback);
-
-	ros::spin();
-	return 0;
-}
+#endif /* _POINTS_IMAGE_H_ */
