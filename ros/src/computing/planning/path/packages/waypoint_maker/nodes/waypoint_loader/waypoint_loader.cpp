95839ecd07856a0948341e8f50d76c42b7300d65
syouji <syouji@axe.bz>
2015-04-13 18:53:11 +0900

    Publish signal waypoint

--- ros/src/computing/planning/path/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader.cpp
+++ ros/src/computing/planning/path/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader.cpp
@@ -29,18 +29,22 @@
 */
 
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_broadcaster.h>
 #include <lane_follower/lane.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <runtime_manager/ConfigWaypointLoader.h>
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <sstream>
+
+#include <vmap_parser.h>
 
 struct pose {
     double x;
@@ -51,20 +55,220 @@ struct pose {
 
 std::string PATH_FRAME = "/map";
 
+static constexpr double ACCIDENT_ERROR = 0.000001;
+
+static const std::string VECTOR_MAP_DIRECTORY = "/tmp";
+static const std::string RULED_WAYPOINT_CSV = "/tmp/ruled_waypoint.csv";
+
+static volatile double config_velocity = 40; // Unit: km/h
+static volatile double config_difference_around_signal = 2; // Unit: km/h
+
+static std::vector<Lane> lanes;
+static std::vector<Node> nodes;
+static std::vector<Point> points;
+static std::vector<StopLine> stoplines;
+
+static std::vector<Point> left_lane_points;
+
+std::string vector_map_directory;
+std::string ruled_waypoint_csv;
+
+ros::Publisher lane_pub;
+ros::Publisher ruled_pub;
+ros::Publisher vel_pub;
+ros::Publisher mark_pub;
+ros::Publisher red_pub;
+ros::Publisher green_pub;
+
+static std::vector<Point> search_stopline_point(const nav_msgs::Path& msg)
+{
+    std::vector<Point> stopline_points;
+
+    // msg's X-Y axis is reversed
+    Point start_point = search_nearest(
+        left_lane_points,
+        msg.poses.front().pose.position.y,
+        msg.poses.front().pose.position.x);
+
+    // msg's X-Y axis is reversed
+    Point end_point = search_nearest(
+        left_lane_points,
+        msg.poses.back().pose.position.y,
+        msg.poses.back().pose.position.x);
+
+    int lane_index = start_point.to_lane_index(nodes, lanes);
+    if (lane_index < 0) {
+        ROS_ERROR("start lane is not found");
+        return stopline_points;
+    }
+    Lane lane = lanes[lane_index];
+
+    int point_index = lane.to_beginning_point_index(nodes, points);
+    if (point_index < 0) {
+        ROS_ERROR("start beginning point is not found");
+        return stopline_points;
+    }
+    Point point = points[point_index];
+
+    while (1) {
+        for (const StopLine& stopline : stoplines) {
+            if (stopline.linkid() == lane.lnid())
+                stopline_points.push_back(point);
+        }
+
+        point_index = lane.to_finishing_point_index(nodes, points);
+        if (point_index < 0) {
+            ROS_ERROR("finishing point is not found");
+            return stopline_points;
+        }
+        point = points[point_index];
+
+        if (point.bx() == end_point.bx() &&
+            point.ly() == end_point.ly()) {
+            for (const StopLine& stopline : stoplines) {
+                if (stopline.linkid() == lane.lnid())
+                    stopline_points.push_back(point);
+            }
+
+            return stopline_points;
+        }
+
+        lane_index = lane.to_next_lane_index(lanes);
+        if (lane_index < 0) {
+            ROS_ERROR("next lane is not found");
+            return stopline_points;
+        }
+        lane = lanes[lane_index];
+
+        point_index = lane.to_beginning_point_index(nodes, points);
+        if (point_index < 0) {
+            ROS_ERROR("beginning point is not found");
+            return stopline_points;
+        }
+        point = points[point_index];
+    }
+}
+
+static std::vector<int> search_stopline_index(const nav_msgs::Path& msg)
+{
+    std::vector<int> indexes;
+
+    std::vector<Point> stopline_points = search_stopline_point(msg);
+    for (const Point& point : stopline_points) {
+        int i = 0;
+
+        // msg's X-Y axis is reversed
+        double min = hypot(point.bx() - msg.poses[0].pose.position.y,
+                           point.ly() - msg.poses[0].pose.position.x);
+        int index = i;
+        for (const geometry_msgs::PoseStamped& stamped : msg.poses) {
+            // msg's X-Y axis is reversed
+            double distance = hypot(
+                point.bx() - stamped.pose.position.y,
+                point.ly() - stamped.pose.position.x);
+            if (distance < min) {
+                min = distance;
+                index = i;
+            }
+            ++i;
+        }
+
+        indexes.push_back(index);
+    }
+
+    return indexes;
+}
+
+static std::vector<double> compute_velocity(const nav_msgs::Path& msg,
+                                            double velocity, double difference)
+{
+    std::vector<double> computations;
+    int loops = msg.poses.size();
+
+    std::vector<int> indexes = search_stopline_index(msg);
+
+    if (indexes.empty() || difference < ACCIDENT_ERROR) {
+        ROS_WARN_COND(difference < ACCIDENT_ERROR,
+                      "too small difference");
+        for (int i = 0; i < loops; ++i)
+            computations.push_back(velocity);
+        return computations;
+    }
+
+    int npaths = static_cast<int>(velocity / difference);
+
+    std::vector<int>::const_iterator iter = indexes.cbegin();
+    int start = *iter - npaths;
+    int end = *iter + npaths;
+    for (int i = 0; i < loops; ++i) {
+        double vel;
+        if (i <= start)
+            vel = velocity;
+        else if (start < i && i < *iter)
+            vel = velocity - (difference * (i - start));
+        else if (i == *iter)
+            vel = 0;
+        else if (*iter < i && i < end)
+            vel = velocity - (difference * (end - i));
+        else {
+            vel = velocity;
+            if ((iter + 1) != indexes.cend()) {
+                ++iter;
+                start = *iter - npaths;
+                end = *iter + npaths;
+            }
+        }
+
+        computations.push_back(vel);
+    }
+
+    return computations;
+}
+
+static void config_callback(const runtime_manager::ConfigWaypointLoader& msg)
+{
+    config_velocity = msg.velocity;
+    config_difference_around_signal = msg.difference_around_signal;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "waypoint_loader");
     ros::NodeHandle nh;
-    ros::NodeHandle private_nh("~");
-    ros::Publisher lane_pub;
-    ros::Publisher ruled_pub;
-    ros::Publisher vel_pub;
-    ros::Publisher mark_pub;
+
+    nh.param<std::string>("waypoint_loader/vector_map_directory",
+                          vector_map_directory, VECTOR_MAP_DIRECTORY);
+
+    nh.param<std::string>("waypoint_loader/ruled_waypoint_csv",
+                          ruled_waypoint_csv, RULED_WAYPOINT_CSV);
+
+    lanes = read_lane((vector_map_directory + std::string("/lane.csv")).c_str());
+    nodes = read_node((vector_map_directory + std::string("/node.csv")).c_str());
+    points = read_point((vector_map_directory + std::string("/point.csv")).c_str());
+    stoplines = read_stopline((vector_map_directory + std::string("/stopline.csv")).c_str());
+
+    for (const Lane& lane : lanes) {
+        if (lane.lno() != 1) // leftmost lane
+            continue;
+        for (const Node& node : nodes) {
+            if (node.nid() != lane.bnid() && node.nid() != lane.fnid())
+                continue;
+            for (const Point& point : points) {
+                if (point.pid() != node.pid())
+                    continue;
+                left_lane_points.push_back(point);
+            }
+        }
+    }
+
+    ros::Subscriber config_sub = nh.subscribe("config/waypoint_loader", 1000, config_callback);
 
     lane_pub = nh.advertise<nav_msgs::Path>("lane_waypoint", 1000, true);
     ruled_pub = nh.advertise<lane_follower::lane>("ruled_waypoint", 1000, true);
     vel_pub = nh.advertise<visualization_msgs::MarkerArray>("waypoint_velocity", 1000, true);
     mark_pub = nh.advertise<visualization_msgs::Marker>("waypoint_mark", 1000, true);
+    red_pub = nh.advertise<lane_follower::lane>("red_waypoint", 1000, true);
+    green_pub = nh.advertise<lane_follower::lane>("green_waypoint", 1000, true);
     std::vector<pose> Pose;
 
     // display by markers the velocity of each waypoint.
@@ -91,13 +295,7 @@ int main(int argc, char **argv)
     mark.color.r = 1.0;
     mark.color.a = 1.0;
 
-    std::string filename = "";
-    if (private_nh.getParam("filename", filename) == false) {
-        std::cout << "error! usage : rosrun waypoint_maker waypoint_loader _filename:=\"[path file]\"" << std::endl;
-        exit(-1);
-    }
-    std::cout << "filename : " << filename << std::endl;
-    std::ifstream ifs(filename.c_str());
+    std::ifstream ifs(ruled_waypoint_csv.c_str());
     std::string str;
     int id_count = 0;
     while (getline(ifs, str)) {
@@ -181,6 +379,38 @@ int main(int argc, char **argv)
     ruled_pub.publish(lane_cmd);
     vel_pub.publish(marker_array);
     mark_pub.publish(mark);
+
+    lane_follower::lane red_cmd;
+    red_cmd.header = lane_cmd.header;
+    red_cmd.increment = 1;
+
+    lane_follower::lane green_cmd;
+    green_cmd.header = lane_cmd.header;
+    green_cmd.increment = 1;
+
+    std::vector<double> computations =
+        compute_velocity(cmd_path, config_velocity, config_difference_around_signal);
+
+    for (int i = 0; i < static_cast<int>(Pose.size()); i++) {
+
+        // for Red
+        lane_follower::waypoint waypoint;
+        waypoint.pose.header = lane_cmd.header;
+        waypoint.twist.header = lane_cmd.header;
+        waypoint.pose.pose.position.x = Pose[i].x;
+        waypoint.pose.pose.position.y = Pose[i].y;
+        waypoint.pose.pose.position.z = Pose[i].z;
+        waypoint.pose.pose.orientation.w = 1.0;
+        waypoint.twist.twist.linear.x = computations[i] / 3.6;
+        red_cmd.waypoints.push_back(waypoint);
+
+        // for Green
+        waypoint.twist.twist.linear.x = config_velocity / 3.6;
+        green_cmd.waypoints.push_back(waypoint);
+    }
+    red_pub.publish(red_cmd);
+    green_pub.publish(green_cmd);
+
     ros::spin();
     
     
