f537ef32e175436ca9a5343deb42d529b54c70e8
Syohei YOSHIDA <syohex@gmail.com>
2015-04-21 13:19:11 +0900

    cleanup

--- ros/src/computing/perception/detection/lib/fusion/include/fusion_func.h
+++ ros/src/computing/perception/detection/lib/fusion/include/fusion_func.h
@@ -35,57 +35,27 @@
 #define _DEBUG 0
 #endif
 
-//openCV library
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
-#include <opencv/cxcore.h>
-//Debug mode
-#ifndef ROS
-#ifdef _DEBUG
-#pragma comment(lib,"cv200d.lib")
-#pragma comment(lib,"cxcore200d.lib")
-#pragma comment(lib,"cvaux200d.lib")
-#pragma comment(lib,"highgui200d.lib")
-#else
-//Release mode
-#pragma comment(lib,"cv200.lib")
-#pragma comment(lib,"cxcore200.lib")
-#pragma comment(lib,"cvaux200.lib")
-#pragma comment(lib,"highgui200.lib")
-#endif
-#endif
-//C++ library
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <boost/array.hpp>
-//ORIGINAL header files
-#include "std_msgs/String.h"
-#include "ros/ros.h"
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <sensor_msgs/CompressedImage.h>
-//ROS
-#include "dpm/ImageObjects.h"
-#include "scan2image/ScanImage.h"
-#include "points2image/PointsImage.h"
-#include "car_detector/FusedObjects.h"
+#include <vector>
+
+#include <ros/ros.h>
+#include <dpm/ImageObjects.h>
+#include <scan2image/ScanImage.h>
+#include <points2image/PointsImage.h>
 
 #define IMAGE_WIDTH 800
 #define IMAGE_HEIGHT 600
 #define NO_DATA 0
+
 #if _DEBUG
 #define IMAGE_TOPIC "/image_raw"
 #define IMAGE_CALLBACK imageCallback
 #endif
 
 struct Scan_image{
-    float distance[IMAGE_WIDTH][IMAGE_HEIGHT];
-    float intensity[IMAGE_WIDTH][IMAGE_HEIGHT];
-    int max_y;
-    int min_y;
+	float distance[IMAGE_WIDTH][IMAGE_HEIGHT];
+	float intensity[IMAGE_WIDTH][IMAGE_HEIGHT];
+	int max_y;
+	int min_y;
 };
 
 extern void calcDistance();
