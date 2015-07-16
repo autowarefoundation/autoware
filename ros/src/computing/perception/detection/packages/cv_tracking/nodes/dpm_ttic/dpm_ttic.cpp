88ec3c4431be26b3a143328fc9fe3f0cb786bcee
Syohei YOSHIDA <syohex@gmail.com>
2015-07-16 18:41:16 +0900

    Implement image_obj publisher for new specification

--- ros/src/computing/perception/detection/packages/cv_tracking/nodes/dpm_ttic/dpm_ttic.cpp
+++ ros/src/computing/perception/detection/packages/cv_tracking/nodes/dpm_ttic/dpm_ttic.cpp
@@ -40,6 +40,7 @@
 #include <runtime_manager/ConfigPedestrianDpm.h>
 
 #include <dpm_gpu.hpp>
+#include <dpm_ttic.hpp>
 
 #define XSTR(x) #x
 #define STR(x) XSTR(x)
@@ -47,13 +48,20 @@
 static ros::Publisher car_image_obj_pub;
 static ros::Publisher pedestrian_image_obj_pub;
 
-static DPMGPUModel *car_model;
-static DPMGPUModel *pedestrian_model;
+static DPMGPUModel *car_gpu_model;
+static DPMGPUModel *pedestrian_gpu_model;
+static DPMTTICModel *car_ttic_model;
+static DPMTTICModel *pedestrian_ttic_model;
 
-static DPMGPUParam car_param;
-static DPMGPUParam pedestrian_param;
+static DPMGPUParam car_gpu_param;
+static DPMGPUParam pedestrian_gpu_param;
+static DPMTTICParam car_ttic_param;
+static DPMTTICParam pedestrian_ttic_param;
 
-static void set_default_param(DPMGPUParam& param)
+static bool use_gpu = true; // XXX Should be configurable(rosparam etc)
+
+template<typename T>
+static void set_default_param(T& param)
 {
 	param.overlap = 0.4;
 	param.threshold = -0.5;
@@ -61,7 +69,8 @@ static void set_default_param(DPMGPUParam& param)
 	param.num_cells = 8;
 }
 
-static void gpu_result_to_image_obj_message(cv_tracking::image_obj& msg, const DPMGPUResult& result)
+template<typename T>
+static void result_to_image_obj_message(cv_tracking::image_obj& msg, const T& result)
 {
 	for (int i = 0; i < result.num; ++i) {
 		cv_tracking::image_rect rect;
@@ -81,13 +90,18 @@ static void image_raw_car_cb(const sensor_msgs::Image& image_source)
 	IplImage img = cv_image->image;
 	IplImage *img_ptr = &img;
 
-	DPMGPUResult result = car_model->detect_objects(img_ptr, car_param);
-
 	cv_tracking::image_obj msg;
 	msg.header = image_source.header;
 	msg.type = "car";
 
-	gpu_result_to_image_obj_message(msg, result);
+	if (use_gpu) {
+		DPMGPUResult result = car_gpu_model->detect_objects(img_ptr, car_gpu_param);
+		result_to_image_obj_message(msg, result);
+	} else {
+		DPMTTICResult result = car_ttic_model->detect_objects(img_ptr, car_ttic_param);
+		result_to_image_obj_message(msg, result);
+	}
+
 	car_image_obj_pub.publish(msg);
 }
 
@@ -97,35 +111,49 @@ static void image_raw_pedestrian_cb(const sensor_msgs::Image& image_source)
 	IplImage img = cv_image->image;
 	IplImage *img_ptr = &img;
 
-	DPMGPUResult result = pedestrian_model->detect_objects(img_ptr, pedestrian_param);
-
 	cv_tracking::image_obj msg;
 	msg.header = image_source.header;
 	msg.type = "pedestrian";
 
-	gpu_result_to_image_obj_message(msg, result);
+	if (use_gpu) {
+		DPMGPUResult result = pedestrian_gpu_model->detect_objects(img_ptr, pedestrian_gpu_param);
+		result_to_image_obj_message(msg, result);
+	} else {
+		DPMTTICResult result = pedestrian_ttic_model->detect_objects(img_ptr, pedestrian_ttic_param);
+		result_to_image_obj_message(msg, result);
+	}
+
 	pedestrian_image_obj_pub.publish(msg);
 }
 
 static void car_config_cb(const runtime_manager::ConfigCarDpm::ConstPtr& param)
 {
-	car_param.threshold = param->score_threshold;
-	car_param.overlap   = param->group_threshold;
-	car_param.lambda    = param->Lambda;
-	car_param.num_cells = param->num_cells;
+	car_gpu_param.threshold = param->score_threshold;
+	car_gpu_param.overlap   = param->group_threshold;
+	car_gpu_param.lambda    = param->Lambda;
+	car_gpu_param.num_cells = param->num_cells;
+
+	car_ttic_param.threshold = param->score_threshold;
+	car_ttic_param.overlap   = param->group_threshold;
+	car_ttic_param.lambda    = param->Lambda;
+	car_ttic_param.num_cells = param->num_cells;
 }
 
 static void pedestrian_config_cb(const runtime_manager::ConfigPedestrianDpm::ConstPtr& param)
 {
-	pedestrian_param.threshold = param->score_threshold;
-	pedestrian_param.overlap   = param->group_threshold;
-	pedestrian_param.lambda    = param->Lambda;
-	pedestrian_param.num_cells = param->num_cells;
+	pedestrian_gpu_param.threshold = param->score_threshold;
+	pedestrian_gpu_param.overlap   = param->group_threshold;
+	pedestrian_gpu_param.lambda    = param->Lambda;
+	pedestrian_gpu_param.num_cells = param->num_cells;
+
+	pedestrian_ttic_param.threshold = param->score_threshold;
+	pedestrian_ttic_param.overlap   = param->group_threshold;
+	pedestrian_ttic_param.lambda    = param->Lambda;
+	pedestrian_ttic_param.num_cells = param->num_cells;
 }
 
 static std::string get_cubin_path(const ros::NodeHandle& n, const char *default_path)
 {
-	std::string cubin();
 	std::string path;
 	if (n.hasParam("/car_detector/cubin")) {
 		n.getParam("/car_detector/cubin", path);
@@ -145,18 +173,22 @@ int main(int argc, char* argv[])
 	std::string cubin = get_cubin_path(n, STR(DEFAULT_CUBIN));
 	dpm_gpu_init_cuda(cubin);
 
-	set_default_param(car_param);
-	set_default_param(pedestrian_param);
+	set_default_param(car_gpu_param);
+	set_default_param(pedestrian_gpu_param);
+	set_default_param(car_ttic_param);
+	set_default_param(pedestrian_ttic_param);
 
 	const char *car_com_csv  = STR(MODEL_DIR) "car_comp.csv";
 	const char *car_root_csv = STR(MODEL_DIR) "car_root.csv";
 	const char *car_part_csv = STR(MODEL_DIR) "car_part.csv";
-	car_model = new DPMGPUModel(car_com_csv, car_root_csv, car_part_csv);
+	car_gpu_model = new DPMGPUModel(car_com_csv, car_root_csv, car_part_csv);
+	car_ttic_model = new DPMTTICModel(car_com_csv, car_root_csv, car_part_csv);
 
 	const char *pedestrian_com_csv  = STR(MODEL_DIR) "person_comp.csv";
 	const char *pedestrian_root_csv = STR(MODEL_DIR) "person_root.csv";
 	const char *pedestrian_part_csv = STR(MODEL_DIR) "person_part.csv";
-	pedestrian_model = new DPMGPUModel(pedestrian_com_csv, pedestrian_root_csv, pedestrian_part_csv);
+	pedestrian_gpu_model = new DPMGPUModel(pedestrian_com_csv, pedestrian_root_csv, pedestrian_part_csv);
+	pedestrian_ttic_model = new DPMTTICModel(pedestrian_com_csv, pedestrian_root_csv, pedestrian_part_csv);
 
 	ros::Subscriber car_image_sub = n.subscribe("/image_raw", 1, image_raw_car_cb);
 	car_image_obj_pub = n.advertise<cv_tracking::image_obj>("image_obj", 1);
@@ -173,8 +205,10 @@ int main(int argc, char* argv[])
 	ros::spin();
 
 	dpm_gpu_cleanup_cuda();
-	delete car_model;
-	delete pedestrian_model;
+	delete car_gpu_model;
+	delete pedestrian_gpu_model;
+	delete car_ttic_model;
+	delete pedestrian_ttic_model;
 
 	return 0;
 }
