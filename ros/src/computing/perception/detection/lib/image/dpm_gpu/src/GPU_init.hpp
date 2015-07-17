703aa1fa046be081ce6fc2a1d0e29cb80ddbc88e
Syohei YOSHIDA <syohex@gmail.com>
2015-07-17 17:13:44 +0900

    Remove needless code

--- ros/src/computing/perception/detection/lib/image/dpm_gpu/src/GPU_init.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _GPU_INIT_H_
-#define _GPU_INIT_H_
-
-/* functions for using GPU and to calculate on GPU */
-extern void init_cuda(void);
-extern void init_cuda_with_cubin(const char *cubin_path);
-extern void clean_cuda(void);
-
-#endif /* _GPU_INIT_H_ */
--- ros/src/computing/perception/detection/lib/image/dpm_gpu/src/tracking.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _TRACKING_H_
-#define _TRACKING_H_
-
-//get object_rectangles
-extern RESULT *get_new_rects(IplImage *Image,MODEL *MO,FLOAT *boxes,int *NUM);
-
-#endif /* _TRACKING_H_ */
