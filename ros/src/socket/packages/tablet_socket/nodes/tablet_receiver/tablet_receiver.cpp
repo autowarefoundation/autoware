6b3ac0b88e9ed6d1b65546ebf27486c33be35d84
pdsljp <pdsljp@gmail.com>
2015-10-21 20:32:56 +0900

    switched roll and pitch

--- ros/src/socket/packages/tablet_socket/nodes/tablet_receiver/tablet_receiver.cpp
+++ ros/src/socket/packages/tablet_socket/nodes/tablet_receiver/tablet_receiver.cpp
@@ -325,7 +325,8 @@ static int getSensorValue(int sock, ros::Publisher pub[TOPIC_NR])
 		tf::Transform transform;
 		tf::Quaternion q;
 		transform.setOrigin(tf::Vector3(geo.y(), geo.x(), geo.z()));
-		q.setRPY(buf[5], buf[4], buf[3]);
+		fprintf(stderr,"azimuth : %lf\n",buf[3] * 180/M_PI);
+		q.setRPY(buf[4], buf[5], buf[3]);
 		transform.setRotation(q);
 
 		free(buf);
