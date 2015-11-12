ebe85a1ddf05e5d5c803427d27fe2af9782a5850
Hiroki Ohta <h_ohta@ertl.jp>
2015-11-12 14:52:38 +0900

    added comments

--- ros/src/computing/planning/motion/packages/waypoint_follower/include/waypoint_follower/libwaypoint_follower.h
+++ ros/src/computing/planning/motion/packages/waypoint_follower/include/waypoint_follower/libwaypoint_follower.h
@@ -100,7 +100,7 @@ public:
 //inline function (less than 10 lines )
 inline double kmph2mps(double velocity_kmph) { return (velocity_kmph * 1000) / (60 * 60); }
 inline double mps2kmph(double velocity_mps) { return (velocity_mps * 60 * 60) / 1000; }
-inline double deg2rad(double deg){  return deg * M_PI/180;} //convert from degree to radian
+inline double deg2rad(double deg){  return deg * M_PI/180;} //convert degree to radian
 
 
 tf::Vector3 point2vector(geometry_msgs::Point point); //convert point to vector
@@ -109,11 +109,10 @@ tf::Vector3 rotateUnitVector(tf::Vector3 unit_vector, double degree); //rotate u
 geometry_msgs::Point rotatePoint(geometry_msgs::Point point, double degree); //rotate point vector by degree
 
 double DecelerateVelocity(double distance, double prev_velocity);
-geometry_msgs::Point calcRelativeCoordinate(geometry_msgs::Point point, geometry_msgs::Pose current_pose);
-geometry_msgs::Point calcAbsoluteCoordinate(geometry_msgs::Point point, geometry_msgs::Pose current_pose);
-double getPlaneDistance(geometry_msgs::Point target1, geometry_msgs::Point target2);
+geometry_msgs::Point calcRelativeCoordinate(geometry_msgs::Point point, geometry_msgs::Pose current_pose); //transform point into the coordinate of current_pose
+geometry_msgs::Point calcAbsoluteCoordinate(geometry_msgs::Point point, geometry_msgs::Pose current_pose); //transform point into the global coordinate
+double getPlaneDistance(geometry_msgs::Point target1, geometry_msgs::Point target2); //get 2 dimentional distance between target 1 and target 2
 int getClosestWaypoint(const waypoint_follower::lane &current_path,geometry_msgs::Pose current_pose );
-
 bool getLinearEquation(geometry_msgs::Point start,geometry_msgs::Point end,double *slope, double *intercept);
 
 
