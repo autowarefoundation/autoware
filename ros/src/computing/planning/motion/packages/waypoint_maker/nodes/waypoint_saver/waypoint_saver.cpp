dc92015c47e11c7d912fa6a0e420d7753a9a5134
Hiroki Ohta <h_ohta@ertl.jp>
2015-11-19 21:52:31 +0900

    Change Marker style

--- ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_saver/waypoint_saver.cpp
+++ ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_saver/waypoint_saver.cpp
@@ -203,14 +203,13 @@ void WaypointSaver::displayMarker(geometry_msgs::Pose pose, double velocity) con
 
   //create saved waypoint marker
   marker.scale.x = 0.5;
-  marker.scale.y = 0.5;
-  marker.scale.z = 0.5;
+  marker.scale.y = 0.1;
   marker.color.a = 1.0;
   marker.color.r = 0.0;
   marker.color.g = 1.0;
   marker.color.b = 0.0;
-  marker.ns = "saved_waypoint";
-  marker.type = visualization_msgs::Marker::SPHERE;
+  marker.ns = "saved_waypoint_arrow";
+  marker.type = visualization_msgs::Marker::ARROW;
   marker.action = visualization_msgs::Marker::ADD;
   marker.pose = pose;
   marray.markers.push_back(marker);
@@ -225,13 +224,6 @@ void WaypointSaver::displayMarker(geometry_msgs::Pose pose, double velocity) con
   std::ostringstream oss;
   oss << std::fixed << std::setprecision(2) << velocity << " km/h";
   marker.text = oss.str();
-
-  //C++11 version
-  //std::string velocity_str = std::to_string(velocity);
-  //velocity.erase(velocity_str.find_first_of(".") + 3);
-  //std::string kmh = " km/h";
-  //std::string text = velocity + kmh;
-  //marker.text = text;
   marray.markers.push_back(marker);
 
   waypoint_saver_pub_.publish(marray);
