ef237169251404690e20b8a3d80599ff2569e2bf
Hiroki Ohta <h_ohta@ertl.jp>
2015-12-17 19:01:47 +0900

    Extract the part making odometry and Make the function

--- ros/src/computing/planning/motion/packages/waypoint_follower/nodes/odom_gen/odom_gen.cpp
+++ ros/src/computing/planning/motion/packages/waypoint_follower/nodes/odom_gen/odom_gen.cpp
@@ -52,6 +52,7 @@ static bool _pose_set = false;
 static bool _waypoint_set = false;
 //Path _path_og;
 static WayPoints _current_waypoints;
+ros::Publisher g_odometry_publisher;
 
 static void NDTCallback(const geometry_msgs::PoseStamped::ConstPtr& input)
 {
@@ -130,6 +131,85 @@ static void waypointCallback(const waypoint_follower::laneConstPtr &msg)
   ROS_INFO_STREAM("waypoint subscribed");
 }
 
+static void publishOdometry()
+{
+
+  static ros::Time current_time = ros::Time::now();
+  static ros::Time last_time = ros::Time::now();
+  static geometry_msgs::Pose pose;
+  static double th = 0;
+  static tf::TransformBroadcaster odom_broadcaster;
+
+  if (!_pose_set)
+  {
+    pose.position = _initial_pose.position;
+    pose.orientation = _initial_pose.orientation;
+    th = tf::getYaw(pose.orientation);
+    ROS_INFO_STREAM("pose set : (" << pose.position.x << " " << pose.position.y << " " << pose.position.z << " " << th << ")");
+    _pose_set = true;
+  }
+
+  int closest_waypoint = getClosestWaypoint(_current_waypoints.getCurrentWaypoints(), pose);
+  if(closest_waypoint == -1)
+  {
+    ROS_INFO("cannot publish odometry because closest waypoint is -1.");
+    return;
+  }
+  else
+  {
+    pose.position.z = _current_waypoints.getWaypointPosition(closest_waypoint).z;
+  }
+
+  double vx = _current_velocity.linear.x;
+  double vth = _current_velocity.angular.z;
+  current_time = ros::Time::now();
+
+  //compute odometry in a typical way given the velocities of the robot
+  double dt = (current_time - last_time).toSec();
+  double delta_x = (vx * cos(th)) * dt;
+  double delta_y = (vx * sin(th)) * dt;
+  double delta_th = vth * dt;
+
+  pose.position.x += delta_x;
+  pose.position.y += delta_y;
+  th += delta_th;
+  pose.orientation = tf::createQuaternionMsgFromYaw(th);
+
+  // std::cout << "delta (x y th) : (" << delta_x << " " << delta_y << " " << delta_th << ")" << std::endl;
+  //std::cout << "current_velocity(linear.x angular.z) : (" << _current_velocity.linear.x << " " << _current_velocity.angular.z << ")"<< std::endl;
+  //    std::cout << "current_pose : (" << pose.position.x << " " << pose.position.y<< " " << pose.position.z << " " << th << ")" << std::endl << std::endl;
+
+  //first, we'll publish the transform over tf
+  geometry_msgs::TransformStamped odom_trans;
+  odom_trans.header.stamp = current_time;
+  odom_trans.header.frame_id = MAP_FRAME;
+  odom_trans.child_frame_id = SIMULATION_FRAME;
+
+  odom_trans.transform.translation.x = pose.position.x;
+  odom_trans.transform.translation.y = pose.position.y;
+  odom_trans.transform.translation.z = pose.position.z;
+  odom_trans.transform.rotation = pose.orientation;
+
+  //send the transform
+  odom_broadcaster.sendTransform(odom_trans);
+
+  //next, we'll publish the odometry message over ROS
+  nav_msgs::Odometry odom;
+  odom.header.stamp = current_time;
+  odom.header.frame_id = MAP_FRAME;
+
+  //set the position
+  odom.pose.pose = pose;
+
+  //set the velocity
+  odom.child_frame_id = SIMULATION_FRAME;
+  odom.twist.twist.linear.x = vx;
+  odom.twist.twist.angular.z = vth;
+  //publish the message
+  g_odometry_publisher.publish(odom);
+
+  last_time = current_time;
+}
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "odom_gen");
@@ -137,7 +217,7 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   ros::NodeHandle private_nh("~");
 //publish topic
-  ros::Publisher odometry_publisher = nh.advertise<nav_msgs::Odometry>("odom_pose", 10);
+  g_odometry_publisher = nh.advertise<nav_msgs::Odometry>("odom_pose", 10);
 
 //subscribe topic
   ros::Subscriber cmd_subscriber = nh.subscribe("twist_cmd", 10, CmdCallBack);
@@ -147,17 +227,8 @@ int main(int argc, char **argv)
 
   ros::Subscriber waypoint_subcscriber = nh.subscribe("base_waypoints", 10, waypointCallback);
 
-//transform
-  tf::TransformBroadcaster odom_broadcaster;
-
   private_nh.getParam("use_pose", _use_pose);
 
-  ros::Time current_time, last_time;
-  current_time = ros::Time::now();
-  last_time = ros::Time::now();
-
-  geometry_msgs::Pose pose;
-  double th = 0;
 
   ros::Rate loop_rate(50); // 50Hz
   while (ros::ok())
@@ -174,73 +245,7 @@ int main(int argc, char **argv)
       continue;
     }
 
-    if (!_pose_set)
-    {
-      pose.position = _initial_pose.position;
-      pose.orientation = _initial_pose.orientation;
-      tf::Quaternion q(pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w);
-      tf::Matrix3x3 m(q);
-      double roll, pitch, yaw;
-      m.getRPY(roll, pitch, yaw);
-      th = yaw;
-      std::cout << "pose set : (" << pose.position.x << " " << pose.position.y << " " << pose.position.z << " " << th
-	    << ")" << std::endl << std::endl;
-      _pose_set = true;
-    }
-
-    int closest_waypoint = getClosestWaypoint(_current_waypoints.getCurrentWaypoints(),pose);
-    pose.position.z = _current_waypoints.getWaypointPosition(closest_waypoint).z;
-
-
-    double vx = _current_velocity.linear.x;
-    double vth = _current_velocity.angular.z;
-    current_time = ros::Time::now();
-
-    //compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th)) * dt;
-    double delta_y = (vx * sin(th)) * dt;
-    double delta_th = vth * dt;
-
-    pose.position.x += delta_x;
-    pose.position.y += delta_y;
-    th += delta_th;
-    pose.orientation = tf::createQuaternionMsgFromYaw(th);
-
-    // std::cout << "delta (x y th) : (" << delta_x << " " << delta_y << " " << delta_th << ")" << std::endl;
-    //std::cout << "current_velocity(linear.x angular.z) : (" << _current_velocity.linear.x << " " << _current_velocity.angular.z << ")"<< std::endl;
-    //    std::cout << "current_pose : (" << pose.position.x << " " << pose.position.y<< " " << pose.position.z << " " << th << ")" << std::endl << std::endl;
-
-    //first, we'll publish the transform over tf
-    geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = MAP_FRAME;
-    odom_trans.child_frame_id = SIMULATION_FRAME;
-
-    odom_trans.transform.translation.x = pose.position.x;
-    odom_trans.transform.translation.y = pose.position.y;
-    odom_trans.transform.translation.z = pose.position.z;
-    odom_trans.transform.rotation = pose.orientation;
-
-    //send the transform
-    odom_broadcaster.sendTransform(odom_trans);
-
-    //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = MAP_FRAME;
-
-    //set the position
-    odom.pose.pose = pose;
-
-    //set the velocity
-    odom.child_frame_id = SIMULATION_FRAME;
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.angular.z = vth;
-    //publish the message
-    odometry_publisher.publish(odom);
-
-    last_time = current_time;
+    publishOdometry();
 
     loop_rate.sleep();
   }
