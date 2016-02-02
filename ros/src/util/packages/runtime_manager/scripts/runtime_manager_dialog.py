f81cba33ca19856b807e3ca590d6fca21a650650
syouji <syouji@axe.bz>
2016-02-02 15:48:51 +0900

    Publish ConfigLaneStop message

--- ros/src/util/packages/runtime_manager/scripts/runtime_manager_dialog.py
+++ ros/src/util/packages/runtime_manager/scripts/runtime_manager_dialog.py
@@ -70,6 +70,7 @@ from runtime_manager.msg import ConfigCarKf
 from runtime_manager.msg import ConfigPedestrianKf
 from runtime_manager.msg import ConfigLaneRule
 from runtime_manager.msg import ConfigLaneSelect
+from runtime_manager.msg import ConfigLaneStop
 from runtime_manager.msg import ConfigCarFusion
 from runtime_manager.msg import ConfigPedestrianFusion
 from tablet_socket.msg import mode_cmd
@@ -2523,10 +2524,13 @@ class MyDialogLaneStop(rtmgr.MyDialogLaneStop):
 		self.update()
 
 	def OnTrafficLightRecognition(self, event):
-		obj = event.GetEventObject()
-		onoff = obj.GetValue()
-		print 'onoff=', onoff
-		# ...
+		pub = rospy.Publisher('/config/lane_stop', ConfigLaneStop, latch=True, queue_size=10)
+		msg = ConfigLaneStop()
+		if event.GetEventObject().GetValue():
+			msg.manual_detection = False
+		else:
+			msg.manual_detection = True
+		pub.publish(msg)
 
 	def OnOk(self, event):
 		self.EndModal(0)
