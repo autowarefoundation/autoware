352d57afd15ef333b00e63ebb291276c320f35a7
yukikitsukawa <yuki@ertl.jp>
2016-05-11 00:28:18 +0900

    Separate downsampling process of scan data from ndt_matching.

---
+++ ros/src/sensing/filter/packages/distance_filter/src/distance_filter.cpp
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (c) 2015, Nagoya University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Autoware nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
+
+ros::Publisher filtered_points_pub;
+
+static int sample_num = 3000;
+
+static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
+{
+    pcl::PointXYZ p;
+    pcl::PointXYZ sampled_p;
+    pcl::PointCloud<pcl::PointXYZ> scan;
+    pcl::PointCloud<pcl::PointXYZ> sampled_scan;
+
+    pcl::fromROSMsg(*input, scan);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZ>(scan));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZ>());
+
+	int points_num = scan.points.size();
+	double *w_point = new double[points_num];
+	double *w_cumulative = new double[points_num];
+	double w_total = 0.0;
+	double w_step = 0.0;
+	int i = 0;
+	int m;
+	double c = 0.0;
+
+//	current_scan_time = input->header.stamp;
+
+	for(pcl::PointCloud<pcl::PointXYZ>::const_iterator item = scan.begin(); item != scan.end(); item++){
+		// Calculate weight of each points.
+		w_point[i] = item->x*item->x + item->y*item->y + item->z*item->z;
+		w_cumulative[i] = w_total + w_point[i];
+		w_total += w_point[i];
+		i++;
+	}
+	w_step = w_total / sample_num;
+
+	i = 0;
+	c = w_point[0];
+	std::cout << "points_num: " << points_num << std::endl;
+	for(m = 0; m < sample_num; m++){
+		while(c < w_step*(m+1)){
+			i++;
+			c += w_point[i];
+		}
+		if(i < points_num){
+//			std::cout << "scan.size() " << scan.size() << " i: " << i << std::endl;
+			filtered_scan_ptr->points.push_back(scan.at(i));
+		}
+	}
+	std::cout << "i: " << i << std::endl;
+
+	filtered_scan_ptr->header.frame_id = scan.header.frame_id;
+	sensor_msgs::PointCloud2 filtered_msg;
+	pcl::toROSMsg(*filtered_scan_ptr, filtered_msg);
+	filtered_points_pub.publish(filtered_msg);
+
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "distance_filter");
+
+	ros::NodeHandle nh;
+	ros::NodeHandle private_nh("~");
+
+    // Publishers
+    filtered_points_pub = nh.advertise<sensor_msgs::PointCloud2>("/filtered_points", 10);
+
+	// Subscribers
+	ros::Subscriber scan_sub = nh.subscribe("points_raw", 10, scan_callback);
+
+	ros::spin();
+
+	return 0;
+}
