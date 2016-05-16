82a52c23cca7888f4493e175ef7a5a094d42fc98
TomohitoAndo <antm678@ertl.jp>
2016-05-16 18:53:04 +0900

    Add semantics package

---
+++ ros/src/computing/perception/semantics/packages/object_map/nodes/laserscan2costmap/laserscan2costmap.cpp
@@ -0,0 +1,364 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <visualization_msgs/Marker.h>
+#include <tf/transform_listener.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/Point.h>
+
+namespace
+{
+constexpr auto OGM_FRAME = "/map";
+std::string g_sensor_frame = "/velodyne";  // sensor which publihes lasescan message
+std::string g_scan_topic = "/scan";        // laser scan topic
+double g_resolution = 0.1;                 // [m]
+int g_scan_size_x = 1000;                  // actual scanning size
+int g_scan_size_y = 1000;
+int g_map_size_x = 500;  // publishing occupancy grid map size
+int g_map_size_y = 500;
+
+struct Grid
+{
+  int index;
+  int index_x, index_y;
+  double x, y;
+  double weight;
+  double range;
+};
+
+ros::Publisher g_map_pub;
+tf::TransformListener* g_tf_listenerp;
+
+double calcYawFromQuaternion(const tf::Quaternion& q)
+{
+  tf::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  return yaw;
+}
+
+// Calcurate grid's coordinate in sensor frame
+void calcCoordinateOfGrid(Grid* g)
+{
+  // index coordinate
+  g->index_x = g->index % g_scan_size_x;
+  g->index_y = (g->index - g->x) / g_scan_size_x;
+
+  // actual coordinate
+  g->x = (g->index_x - g_scan_size_x / 2.0) * g_resolution;
+  g->y = (g->index_y - g_scan_size_y / 2.0) * g_resolution;
+}
+
+// Calculate a range from sensor in a certain grid
+double calcRangeOfGrid(const Grid& g)
+{
+  double distance_x = g_resolution * fabs(g_scan_size_x / 2 - g.index_x);
+  double distance_y = g_resolution * fabs(g_scan_size_y / 2 - g.index_y);
+
+  return sqrt(distance_x * distance_x + distance_y * distance_y);
+}
+
+// Change local index into global index
+int calcGlobalIndex(const Grid& g, const tf::StampedTransform& transform)
+{
+  // Calculate index in global frame
+  int ix = (g.x + transform.getOrigin().x()) / g_resolution;
+  int iy = (g.y + transform.getOrigin().y()) / g_resolution;
+
+  // Make indexes positive value
+  int global_grid_x = (ix + 100000 * g_scan_size_x) % g_scan_size_x;
+  int global_grid_y = (iy + 100000 * g_scan_size_y) % g_scan_size_y;
+
+  int global_index = global_grid_x + global_grid_y * g_scan_size_x;
+
+  return global_index;
+}
+
+int calcGlobalIndex(const int& grid_x, const int& grid_y, const tf::StampedTransform& transform)
+{
+  // Point coordinate in velodyne(local) frame
+  double local_x = (grid_x - g_scan_size_x / 2.0) * g_resolution;
+  double local_y = (grid_y - g_scan_size_y / 2.0) * g_resolution;
+
+  // Calculate index in global coordinate
+  int ix = (local_x + transform.getOrigin().x()) / g_resolution;
+  int iy = (local_y + transform.getOrigin().y()) / g_resolution;
+
+  // Make indexes positive value
+  int global_grid_x = (ix + 100000 * g_scan_size_x) % g_scan_size_x;
+  int global_grid_y = (iy + 100000 * g_scan_size_y) % g_scan_size_y;
+
+  int global_index = global_grid_x + global_grid_y * g_scan_size_x;
+
+  return global_index;
+}
+
+void preCasting(const sensor_msgs::LaserScan& scan, std::vector<std::vector<Grid>>* precasted_grids)
+{
+  int iangle_size = 2 * M_PI / scan.angle_increment;
+  // We decide if a grid is passed by laser by this search_step
+  double search_step = g_resolution / 10.0;
+
+  // Grid indexes for each laser
+  std::vector<std::vector<int>> grid_indexes(iangle_size);
+
+  for (int iangle = 0; iangle < iangle_size; iangle++)
+  {
+    // Angle of each laser from Lidar
+    double angle = scan.angle_increment * iangle;  // + scan.angle_min;
+    double step = 0;
+
+    while (1)
+    {
+      step += search_step;
+
+      // Calculate a grid index passed by laser scan
+      int grid_x = step * cos(angle) / g_resolution + g_scan_size_x / 2.0;
+      int grid_y = step * sin(angle) / g_resolution + g_scan_size_y / 2.0;
+
+      if (grid_x >= g_scan_size_x || grid_x < 0 || grid_y >= g_scan_size_y || grid_y < 0)
+        break;
+
+      grid_indexes[iangle].push_back(grid_x + grid_y * g_scan_size_x);
+    }
+  }
+
+  // Erase overlapping values
+  auto unique_grid_indexes = grid_indexes;
+  for (auto& vec : unique_grid_indexes)
+    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+
+  // Max size of step counts
+  // Precated laser steps for each grid are less than this value
+  // int max_count = g_resolution / search_step * sqrt(2) + 1;
+
+  for (int i = 0; i < iangle_size; i++)
+  {
+    // Push grid indexes for each laser
+    // Each index has weight which is decided by passing laser rate
+    for (const auto& unique_index : unique_grid_indexes[i])
+    {
+      Grid g;
+      g.index = unique_index;
+      // g.weight = 1.0 * std::count(grid_indexes[i].begin(), grid_indexes[i].end(), unique_index) / max_count;
+      calcCoordinateOfGrid(&g);
+      g.range = calcRangeOfGrid(g);
+      precasted_grids->at(i).push_back(g);
+    }
+  }
+}
+
+// Delete old cost values
+void deleteOldData(std::vector<int>* cost_map, const double& current_x, const double& current_y, const double& prev_x,
+                   const double& prev_y)
+{
+  double begin_x = current_x;
+  double begin_y = current_y;
+  double end_x = prev_x;
+  double end_y = prev_y;
+
+  if (begin_x > end_x)
+    std::swap(begin_x, end_x);
+  if (begin_y > end_y)
+    std::swap(begin_y, end_y);
+
+  int ibegin_x = begin_x / g_resolution + g_scan_size_x / 2;
+  int ibegin_y = begin_y / g_resolution + g_scan_size_y / 2;
+  int iend_x = end_x / g_resolution + g_scan_size_x / 2;
+  int iend_y = end_y / g_resolution + g_scan_size_y / 2;
+
+  for (int i = ibegin_x; i < iend_x; i++)
+  {
+    for (int j = 0; j < g_scan_size_y; j++)
+    {
+      int global_grid_x = (i + 100000 * g_scan_size_x) % g_scan_size_x;
+      cost_map->at(global_grid_x + j * g_scan_size_x) = -1;
+    }
+  }
+
+  for (int i = 0; i < g_scan_size_x; i++)
+  {
+    for (int j = ibegin_y; j < iend_y; j++)
+    {
+      int global_grid_y = (j + 100000 * g_scan_size_y) % g_scan_size_y;
+      cost_map->at(i + global_grid_y * g_scan_size_x) = -1;
+    }
+  }
+}
+
+void setOccupancyGridMap(nav_msgs::OccupancyGrid* map, const std_msgs::Header& header,
+                         const tf::StampedTransform& transform)
+{
+  map->header.stamp = header.stamp;
+  map->header.frame_id = OGM_FRAME;
+  map->info.map_load_time = header.stamp;
+  map->info.resolution = g_resolution;
+  map->info.height = g_map_size_y;
+  map->info.width = g_map_size_x;
+  map->info.origin.position.x = transform.getOrigin().x() - (g_map_size_x / 2) * g_resolution;
+  map->info.origin.position.y = transform.getOrigin().y() - (g_map_size_y / 2) * g_resolution;
+  map->info.origin.position.z = transform.getOrigin().z() - 5;
+  map->info.origin.orientation.x = 0;
+  map->info.origin.orientation.y = 0;
+  map->info.origin.orientation.z = 0;
+  map->info.origin.orientation.w = 1;
+}
+
+void createCostMap(const sensor_msgs::LaserScan& scan, const std::vector<std::vector<Grid>>& precasted_grids)
+{
+  tf::StampedTransform transform;
+
+  try
+  {
+    // What time should we use?
+    g_tf_listenerp->lookupTransform(OGM_FRAME, g_sensor_frame, ros::Time(0), transform);
+  }
+  catch (tf::TransformException ex)
+  {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
+
+  // Save costs in this variable
+  static std::vector<int> cost_map(g_scan_size_x * g_scan_size_y, -1);
+
+  static int count = 0;
+  static double prev_x, prev_y;
+  static nav_msgs::OccupancyGrid map;
+  setOccupancyGridMap(&map, scan.header, transform);
+  if (!count)
+  {
+    map.data.resize(g_map_size_x * g_map_size_y, -1);
+    prev_x = transform.getOrigin().x();
+    prev_y = transform.getOrigin().y();
+  }
+
+  // Since we implement as ring buffer, we have to delete old data regularly
+  deleteOldData(&cost_map, transform.getOrigin().x(), transform.getOrigin().y(), prev_x, prev_y);
+  prev_x = transform.getOrigin().x();
+  prev_y = transform.getOrigin().y();
+
+  // Vehicle's orientation
+  double yaw = calcYawFromQuaternion(transform.getRotation());
+
+  // Original yaw is -PI ~ PI, so make its range 0 ~ 2PI
+  if (yaw < 0)
+    yaw += 2 * M_PI;
+  static double laser_offset = fabs(scan.angle_min);
+
+  // For tough_urg
+  // static double manual_offset = -10.0 / 180 * M_PI + M_PI;
+  // int index_offset = (yaw + laser_offset + manual_offset) / scan.angle_increment;
+
+  int index_offset = (yaw + laser_offset) / scan.angle_increment;
+  static int iangle_size = 2 * M_PI / scan.angle_increment;
+
+  //----------------- RING OCCUPANCY GRID MAPPING --------------
+  // Accumulate grid costs for each laser scan
+  for (size_t i = 0; i < scan.ranges.size(); i++)
+  {
+    double range = scan.ranges[i];
+
+    // If a range is less than range_min, that means there are no obstacles within a sensor range ?
+    if (scan.ranges[i] < scan.range_min)
+      range = scan.range_max;
+
+    int precasted_index = (i + index_offset) % iangle_size;
+    int j = -1;
+    for (const auto& g : precasted_grids[precasted_index])
+    {
+      j++;
+
+      // Free range
+      if (g.range < range)
+      {
+        int global_index = calcGlobalIndex(g, transform);
+        cost_map[global_index] -= 2;
+        if (cost_map[global_index] < 0)
+          cost_map[global_index] = 0;
+        continue;
+      }
+
+      break;
+    }
+
+    // Obstacle
+    int global_index = calcGlobalIndex(precasted_grids[precasted_index][j], transform);
+    cost_map[global_index] += 15;
+    if (cost_map[global_index] > 100)
+      cost_map[global_index] = 100;
+  }
+
+  // Begining of grid index of publishing OGM
+  static int origin_index_x = (g_scan_size_x - g_map_size_x) / 2.0;
+  static int origin_index_y = (g_scan_size_y - g_map_size_y) / 2.0;
+  // static int origin_index = origin_index_x + origin_index_y * g_scan_size_x;
+
+  // Set cost values for publishing OccuppancyGridMap
+  for (int i = 0; i < g_map_size_y; i++)
+  {
+    for (int j = 0; j < g_map_size_x; j++)
+    {
+      int scanmap_index_x = origin_index_x + j;
+      int scanmap_index_y = origin_index_y + i;
+      // int local_index = (origin_index + j) + (i * g_scan_size_x);
+      map.data[j + i * g_map_size_x] = cost_map[calcGlobalIndex(scanmap_index_x, scanmap_index_y, transform)];
+    }
+  }
+
+  g_map_pub.publish(map);
+  count++;
+}
+
+// Make CostMap from LaserScan message
+void laserScanCallback(const sensor_msgs::LaserScanConstPtr& msg)
+{
+  static bool precasted = false;
+  static int iangle_size = 2 * M_PI / msg->angle_increment;
+  static std::vector<std::vector<Grid>> precasted_grids(iangle_size);
+
+  if (!precasted)
+  {
+    preCasting(*msg, &precasted_grids);
+    precasted = true;
+  }
+
+  // Create costmap and publish
+  createCostMap(*msg, precasted_grids);
+
+  return;
+}
+
+}  // namespace
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "laserscan2costmap");
+
+  tf::TransformListener tf_listener;
+  g_tf_listenerp = &tf_listener;
+
+  ros::NodeHandle nh;
+  ros::NodeHandle private_nh("~");
+
+  private_nh.param<double>("resolution", g_resolution, 0.1);
+  private_nh.param<int>("scan_size_x", g_scan_size_x, 1000);
+  private_nh.param<int>("scan_size_y", g_scan_size_y, 1000);
+  private_nh.param<int>("map_size_x", g_map_size_x, 500);
+  private_nh.param<int>("map_size_y", g_map_size_y, 500);
+  private_nh.param<std::string>("scan_topic", g_scan_topic, "/scan");
+  private_nh.param<std::string>("sensor_frame", g_sensor_frame, "/velodyne");
+
+  ros::Subscriber laserscan_sub = nh.subscribe(g_scan_topic, 1, laserScanCallback);
+
+  g_map_pub = nh.advertise<nav_msgs::OccupancyGrid>("/ring_ogm", 1);
+
+  ros::spin();
+
+  return 0;
+}
---
+++ ros/src/computing/perception/semantics/packages/object_map/nodes/points2costmap/points2costmap.cpp
@@ -0,0 +1,128 @@
+/*
+ *  Copyright (c) 2015, Nagoya University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Autoware nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <geometry_msgs/PoseStamped.h>
+
+namespace
+{
+constexpr double HEIGHT_LIMIT = 0.1;  // from sensor
+constexpr double CAR_LENGTH = 4.5;
+constexpr double CAR_WIDTH = 1.75;
+
+ros::Publisher g_costmap_pub;
+double g_resolution;
+int g_cell_width;
+int g_cell_height;
+
+std::vector<int> createCostMap(const pcl::PointCloud<pcl::PointXYZ> &scan)
+{
+  std::vector<int> cost_map(g_cell_width * g_cell_height, 0);
+  double map_center_x = (g_cell_width / 2.0) * g_resolution;
+  double map_center_y = (g_cell_height / 2.0) * g_resolution;
+
+  // scan points are in sensor frame
+  for (const auto &p : scan.points)
+  {
+    if (p.z > HEIGHT_LIMIT)
+      continue;
+    if (std::fabs(p.x) < CAR_LENGTH && std::fabs(p.y) < CAR_WIDTH)
+      continue;
+
+    // Calculate grid index
+    int grid_x = (p.x + map_center_x) / g_resolution;
+    int grid_y = (p.y + map_center_y) / g_resolution;
+    if (grid_x < 0 || grid_x >= g_cell_width || grid_y < 0 || grid_y >= g_cell_height)
+      continue;
+
+    int index = g_cell_width * grid_y + grid_x;
+    cost_map[index] += 15;
+
+    // Max cost value is 100
+    if (cost_map[index] > 100)
+      cost_map[index] = 100;
+  }
+
+  return cost_map;
+}
+
+void setOccupancyGrid(nav_msgs::OccupancyGrid *og)
+{
+  og->info.resolution = g_resolution;
+  og->info.width = g_cell_width;
+  og->info.height = g_cell_height;
+  og->info.origin.position.x = (-1) * (g_cell_width / 2.0) * g_resolution;
+  og->info.origin.position.y = (-1) * (g_cell_height / 2.0) * g_resolution;
+  og->info.origin.orientation.x = 0.0;
+  og->info.origin.orientation.y = 0.0;
+  og->info.origin.orientation.z = 0.0;
+  og->info.origin.orientation.w = 0.0;
+}
+
+void createOccupancyGrid(const sensor_msgs::PointCloud2::ConstPtr &input)
+{
+  static int count = 0;
+  pcl::PointCloud<pcl::PointXYZ> scan;
+  pcl::fromROSMsg(*input, scan);
+
+  static nav_msgs::OccupancyGrid og;
+  if (!count)
+    setOccupancyGrid(&og);
+
+  og.header = input->header;
+
+  // create cost map with pointcloud
+  std::vector<int> cost_map = createCostMap(scan);
+  og.data.insert(og.data.end(), cost_map.begin(), cost_map.end());
+  g_costmap_pub.publish(og);
+  og.data.clear();
+  count++;
+}
+
+}  // namespace
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "points2costmap");
+  ros::NodeHandle nh;
+  ros::NodeHandle private_nh("~");
+
+  private_nh.param<double>("resolution", g_resolution, 1.0);
+  private_nh.param<int>("cell_width", g_cell_width, 50);
+  private_nh.param<int>("cell_height", g_cell_height, 50);
+
+  g_costmap_pub = nh.advertise<nav_msgs::OccupancyGrid>("realtime_cost_map", 10);
+  ros::Subscriber velodyne_sub = nh.subscribe("points_filtered", 10, createOccupancyGrid);
+
+  ros::spin();
+}
