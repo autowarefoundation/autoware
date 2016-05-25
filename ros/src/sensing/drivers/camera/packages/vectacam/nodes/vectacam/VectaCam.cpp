f5441d10cee066cca3e5739957cece594c1fe242
AMC <abrahammonrroy@yahoo.com>
2016-05-25 17:30:31 +0900

    VectaCam driver

---
+++ ros/src/sensing/drivers/camera/packages/vectacam/nodes/vectacam/VectaCam.cpp
@@ -0,0 +1,266 @@
+#include "VectaCam.h"
+
+VectaCam::VectaCam(unsigned int in_configuration_port, unsigned int in_data_port, std::string in_parameter_file)
+{
+	this->data_port_ 			= in_data_port;
+	this->configuration_port_ 	= in_configuration_port;
+	this->camera_ready_ 		= false;
+	this->running_ 				= false;
+	this->fps_ 					= 0.0;
+	this->frame_number_			= 0;
+	this->frame_counter_		= 0;
+	this->payload_offset_ 		= 16;
+	this->image_width_ 			= IMG_WIDTH * NUM_CAMERAS;
+	this->image_height_ 		= IMG_HEIGHT;
+	this->image_buffer_ 		= new char[image_width_ * 3 * image_height_];
+	this->parameter_file_ 		= in_parameter_file;
+	_initialize_camera(configuration_port_, data_port_, parameter_file_);
+}
+
+void VectaCam::_parse_parameter_file(std::string in_parameter_file, std::vector<VectaCamCommand>& out_commands)
+{
+	std::string line;
+	std::ifstream config_file (parameter_file_);
+	std::string delimiter = ":";
+	out_commands.clear();
+	if (config_file.is_open())
+	{
+		while ( getline (config_file,line) )
+		{
+			std::vector<std::string> tokens;
+			boost::split(tokens, line, boost::is_any_of(":"));
+			if (tokens.size()==2)
+			{
+				VectaCamCommand current_command;
+				int full_address = std::stoi(tokens[0], NULL, 16);
+				current_command.low_nibble_address = (unsigned char)( (full_address>>0) & 0xFF);
+				current_command.high_nibble_address = (unsigned char)((full_address>>8) & 0xFF);
+				current_command.command_data = std::stoi(tokens[1], NULL, 16);
+				out_commands.push_back(current_command);
+			}
+			else
+				std::cout << "Unrecognized command in configuration file: " << parameter_file_ << std::endl;
+		}
+		config_file.close();
+	}
+	else
+		std::cout << "Unable to open file:" << parameter_file_;
+}
+
+void VectaCam::_send_commands_to_camera(unsigned int in_port, std::vector<VectaCamCommand> in_commands)
+{
+	int socket_descriptor;
+	struct sockaddr_in socket_address;
+	sockaddr_in socket_local;
+	if ((socket_descriptor=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+		std::cout << "Problem creating socket\n";
+		return;
+	}
+	memset((char *) &socket_address, 0, sizeof(socket_address));
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_port = htons(in_port);
+	socket_local.sin_family = AF_INET;
+	socket_local.sin_addr.s_addr = htonl(INADDR_ANY);
+	socket_local.sin_port = htons(in_port);
+	bind(socket_descriptor, reinterpret_cast<sockaddr *>(&socket_local), sizeof(socket_local));
+	if (inet_aton("10.0.0.1", &socket_address.sin_addr)==0)
+	{
+		std::cout << "Invalid IP address" << std::endl;
+		return;
+	}
+	for (unsigned int i=0; i< in_commands.size(); i++)
+	{
+		VectaCamCommand current_command = in_commands[i];
+		unsigned char command_buffer[12] = {0x05, 0x00, 0x36, 0x00, 0x00, 0x00,
+									current_command.low_nibble_address, current_command.high_nibble_address, current_command.command_data,
+									0x00, 0x00, 0x00};
+		unsigned int sent_data = sendto(socket_descriptor, command_buffer, sizeof(command_buffer), 0, reinterpret_cast<sockaddr *> (&socket_address), sizeof(socket_address));
+		if (sent_data < 0)
+		{
+			std::cout << "Could not send the data to the camera socket" << std::endl;
+		}
+		else
+			std::cout << ".";
+			/*
+			std::cout << std::to_string((int)current_command.high_nibble_address) <<
+						std::to_string((int)current_command.low_nibble_address) << ":" <<
+						std::to_string((int)current_command.command_data) << std::endl;*/
+		usleep(20000);
+	}
+}
+
+void VectaCam::_enable_camera(unsigned int in_port, bool in_enable)
+{
+	unsigned char camonoff = in_enable ? 0xff : 0x00;
+	unsigned char enable_command[] =	{0x00, 0x00, // upldest = 0
+				 0x00, // inspect target
+				 0x02, // command(write)
+				 0x00, 0x00, 0x00, 0x00, // data_output
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00,
+				 0x00, 0x00, 0x00, (camonoff),
+				 0x00, 0x00, 0x00, 0x00, // module_reset
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00, //
+				 0x00, 0x00, 0x00, 0x00 };
+	int socket_descriptor;
+	struct sockaddr_in socket_address;
+	if ((socket_descriptor=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+	{
+		std::cout << "Problem creating socket\n";
+		return;
+	}
+	memset((char *) &socket_address, 0, sizeof(socket_address));
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_port = htons(in_port);
+	if (inet_aton(CAMERA_IP, &socket_address.sin_addr)==0)
+	{
+		std::cout << "Invalid IP address" << std::endl;
+		return;
+	}
+	if (sendto(socket_descriptor, enable_command, sizeof(enable_command), 0, reinterpret_cast<sockaddr *>(&socket_address), sizeof(socket_address)) < 0)
+	{
+		std::cout << "Could not send the enable command to the camera socket" << std::endl;
+	}
+}
+
+void VectaCam::_initialize_camera(unsigned int in_configuration_port, unsigned int in_data_port, std::string in_parameter_file)
+{
+	std::cout << "Reading configuration file...";
+	_parse_parameter_file(parameter_file_, camera_commands_);
+	std::cout << "DONE" << std::endl << "Configuring camera.";
+	_send_commands_to_camera(in_configuration_port, camera_commands_);
+	std::cout << "DONE" << std::endl << "Enabling camera";
+	_enable_camera(in_data_port, true);
+	std::cout << "DONE" << std::endl;
+}
+
+VectaCam::~VectaCam()
+{
+	this->running_=false;
+	delete[] this->image_buffer_;
+}
+
+void VectaCam::GetImage(cv::Mat& out_image)
+{
+	out_image = camera_image_.clone();
+}
+
+void VectaCam::StopCamera()
+{
+	this->running_ = false;
+	_enable_camera(data_port_, false);
+}
+
+long int VectaCam::GetFrameNumber()
+{
+	return this->frame_number_;
+}
+
+void VectaCam::StartCamera()
+{
+
+	int 		socket_descriptor;
+	struct 		sockaddr_in socket_address;
+	unsigned int length;
+
+	if ((socket_descriptor = socket( PF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		std::cout << "Problem creating socket\n";
+		return;
+	}
+
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	socket_address.sin_port = htons(this->data_port_);
+
+	if (bind(socket_descriptor, reinterpret_cast<sockaddr *> (&socket_address), sizeof(socket_address)) < 0)
+	{
+		std::cout << "Problem binding" << std::endl;
+		return;
+	}
+
+	length = sizeof(socket_address);
+	if (getsockname(socket_descriptor, reinterpret_cast<sockaddr *> (&socket_address), &length) < 0)
+	{
+		std::cout << "Error getsockname" << std::endl;
+		return;
+	}
+	this->camera_ready_ = true;
+	this->running_ = true;
+
+	std::cout << "Listening camera in UDP port number " << std::to_string(ntohs(socket_address.sin_port)) << std::endl;
+
+	_udp_receive(socket_descriptor);
+}
+
+void VectaCam::_udp_receive(int in_socket_descriptor)
+{
+	unsigned int 	length, n;
+	char 			buffer_in[MAX_BUFFER];
+	struct 			sockaddr_in remote;
+	length = sizeof(remote);
+
+	int line_number = 0;
+
+	while (this->running_)
+	{
+		n = recvfrom(in_socket_descriptor, buffer_in, MAX_BUFFER, 0, reinterpret_cast<sockaddr *> (&remote), &length);
+		if (n < 0)
+		{
+			std::cout << "Error receiving data" << std::endl;
+		}
+		else
+		{
+			int packet_offset = ntohl(*(uint32_t*)(buffer_in + 4));
+			int packet_length = ntohl(*(uint32_t*)(buffer_in + 8));
+			int header = ntohl(*(uint32_t*)(buffer_in + 12));
+
+			if (header == FRAME_START) {
+				// frame start
+			}
+			else if (header == FRAME_END)
+			{
+				// frame end
+				this->camera_image_ = cv::Mat(image_height_, image_width_, CV_8UC3, image_buffer_);
+				frame_number_++;
+			}
+			else
+			{
+
+				line_number = header - 1;
+
+				int p = packet_offset * 3;
+				int k = payload_offset_ + 4 * packet_length;
+				int total = (packet_length);
+				if (line_number < image_height_ && total < 1500 && p < image_width_ * 3
+						&& total > 10)
+				{
+					for (int i = 0; i < total; ++i)
+					{
+						image_buffer_[line_number * image_width_ * 3 + p + 2] = (((((int) buffer_in[k + 0]) << 4) & 0x3f0) + ((((int) buffer_in[k + 1]) >> 4) & 0xf)) >> 2; // R
+						image_buffer_[line_number * image_width_ * 3 + p + 1] = (((((int) buffer_in[k + 1]) << 6) & 0x3c0) + ((((int) buffer_in[k + 2]) >> 2) & 0x3f)) >>2; // G
+						image_buffer_[line_number * image_width_ * 3 + p + 0] = (((((int) buffer_in[k + 2]) << 8) & 0x300)	+ ((((int) buffer_in[k + 3]) >> 0) & 0xff)) >> 2; // B
+
+						p += 3;
+						k -= 4;
+					}
+				}
+			}
+		}
+	}
+}
+
