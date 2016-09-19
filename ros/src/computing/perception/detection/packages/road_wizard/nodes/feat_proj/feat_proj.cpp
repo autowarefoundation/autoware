c2a090dec2101be2788ecb607102fa9210e24737
syouji <syouji@axe.bz>
2016-09-19 12:36:11 +0900

    Fix codes to use map_file messages and old vector_map_info topics

--- ros/src/computing/perception/detection/packages/road_wizard/nodes/feat_proj/feat_proj.cpp
+++ ros/src/computing/perception/detection/packages/road_wizard/nodes/feat_proj/feat_proj.cpp
@@ -267,11 +267,11 @@ int main (int argc, char *argv[])
   }
   
   /* load vector map */
-  ros::Subscriber sub_point     = rosnode.subscribe("vector_map_info/point_class",
+  ros::Subscriber sub_point     = rosnode.subscribe("vector_map_info/point",
                                                     SUBSCRIBE_QUEUE_SIZE,
                                                     &VectorMap::load_points,
                                                     &vmap);
-  ros::Subscriber sub_line      = rosnode.subscribe("vector_map_info/line_class",
+  ros::Subscriber sub_line      = rosnode.subscribe("vector_map_info/line",
                                                     SUBSCRIBE_QUEUE_SIZE,
                                                     &VectorMap::load_lines,
                                                     &vmap);
@@ -279,7 +279,7 @@ int main (int argc, char *argv[])
                                                     SUBSCRIBE_QUEUE_SIZE,
                                                     &VectorMap::load_lanes,
                                                     &vmap);
-  ros::Subscriber sub_vector    = rosnode.subscribe("vector_map_info/vector_class",
+  ros::Subscriber sub_vector    = rosnode.subscribe("vector_map_info/vector",
                                                     SUBSCRIBE_QUEUE_SIZE,
                                                     &VectorMap::load_vectors,
                                                     &vmap);
