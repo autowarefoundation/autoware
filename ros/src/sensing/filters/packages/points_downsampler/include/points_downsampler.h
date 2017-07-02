8a1bfeac01a2a0b93544d46f229c6716457b4af5
andoh104 <toandh104@gmail.com>
2017-07-02 12:29:22 +0900

    Add Error handring to removePointsByRange()

--- ros/src/sensing/filters/packages/points_downsampler/include/points_downsampler.h
+++ ros/src/sensing/filters/packages/points_downsampler/include/points_downsampler.h
@@ -5,6 +5,14 @@ static pcl::PointCloud<pcl::PointXYZI> removePointsByRange(pcl::PointCloud<pcl::
 {
   pcl::PointCloud<pcl::PointXYZI> narrowed_scan;
   narrowed_scan.header = scan.header;
+
+#if 1     //  This error handling should be detemind.
+  if( min_range>=max_range ) {
+    ROS_ERROR_ONCE("min_range>=max_range @(%lf, %lf)", min_range, max_range );
+    return scan;
+  }
+#endif
+
   double square_min_range = min_range * min_range;
   double square_max_range = max_range * max_range;
 
@@ -21,11 +29,8 @@ static pcl::PointCloud<pcl::PointXYZI> removePointsByRange(pcl::PointCloud<pcl::
       narrowed_scan.points.push_back(p);
     }
   }
-#if 1
+
   return narrowed_scan;
-#else
-  return scan;    //  This is a only tempolary patch for Localization problem.
-#endif
 }
 
 #endif // POINTS_DOWNSAMPLER_H
