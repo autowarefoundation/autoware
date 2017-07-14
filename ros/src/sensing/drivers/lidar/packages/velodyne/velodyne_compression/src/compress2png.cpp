4c5172e751d732ea10acedfe98d4e6b44aa45f66
ChenxiTU <tu.chenxi@g.sp.m.is.nagoya-u.ac.jp>
2017-07-14 19:01:07 +0900

    Rearrange files, delete useless code, rename topic
    Now compressed topic named "velodyne_packets_compressed"

--- ros/src/sensing/drivers/lidar/packages/velodyne/velodyne_compression/src/compress2png.cpp
+++ ros/src/sensing/drivers/lidar/packages/velodyne/velodyne_compression/src/compress2png.cpp
@@ -14,12 +14,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
 #include "std_msgs/UInt16MultiArray.h"
-#include <compression_msgs/CompressedPacket.h>
+#include <velodyne_compression/CompressedPacket.h>
 #include <cmath>
 
-//#include "std_msgs/Int32MultiArray.h"
-//#include "compressed_image_transport/compressed_publisher.h"
-//#include "compressed_image_transport/compression_common.h"
+
 
 #include <vector>
 #include <sstream>
@@ -28,29 +26,16 @@ using namespace std;
 using namespace velodyne_rawdata;
 using namespace cv;
 
-compression_msgs::CompressedPacket CompressedData;
-
-//char *fold;
+velodyne_compression::CompressedPacket CompressedData;
 ros::Publisher *pubp;
 
 int i=1;
 int row_type;
 void velodyneCallback(const velodyne_msgs::VelodyneScan::ConstPtr &msg)
 {
-//cout<<msg->packets.size()<<endl;
 static int count=0;
 count++;
 
-  //char name[100];
-  //char route[100];
-  
-  //sprintf(name,"%sRotation%06d.txt",fold,count);
-  //sprintf(name,"Rotation%06d.txt",count);
-  //cout<<name;
-  //sprintf(route,"%simage%06d.png",fold,count);
-  //sprintf(route,"mage%06d.png",count);
-//  FILE *fp;
-//  fp=fopen(name,"w");
 if (msg->packets.size()==348) //64ES2
 {
 
@@ -91,48 +76,30 @@ Mat img(CompressedData.scan_row, CompressedData.scan_column, CV_16U);
 Mat img2(CompressedData.scan_row, CompressedData.scan_column, CV_8U);
 
 
-
-
-
-
 vector<int>compression_params;
 compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 compression_params.push_back(9);
 CompressedData.rotation.clear();
   
  
-
-
-
-//cout<<msg->header<<endl;
 for (int next=0; next<msg->packets.size();++next)
 {
-//printf("get %d\n",msg->packets.size());
-
  const raw_packet_t *raw=(const raw_packet_t *) &msg->packets[next].data[0];
 
-  //cout<<raw<<endl; 
 for (int i = 0; i < BLOCKS_PER_PACKET; i++) {
 
       // upper bank lasers are numbered [0..31]
       // NOTE: this is a change from the old velodyne_common implementation
       int bank_origin = 0;
-//cout<<raw->blocks[i].header<<endl;
       if (raw->blocks[i].header == LOWER_BANK) {
         // lower bank lasers are [32..63]
         bank_origin = 32;
       }
-     // cout<<i<<endl;
-     // cout<<raw->blocks[i].rotation<<endl;
 for (int j = 0, k = 0; j < SCANS_PER_BLOCK; j++, k += RAW_SCAN_SIZE) {
         
         uint8_t laser_number;       ///< hardware laser number
 
         laser_number=j+bank_origin;
-        //cout<<j<<endl;
-        //cout<<bank_origin<<endl;
-       // printf("get %d\n",laser_number);
-
         union two_bytes tmp;
         tmp.bytes[0] = raw->blocks[i].data[k];
         tmp.bytes[1] = raw->blocks[i].data[k+1];
@@ -147,8 +114,6 @@ for (int j = 0, k = 0; j < SCANS_PER_BLOCK; j++, k += RAW_SCAN_SIZE) {
 
         img.at<ushort>(laser_number, col) = d;
         img2.at<uchar>(laser_number, col) = raw->blocks[i].data[k+2];
-        // printf("%d %d %d\n",laser_number, floor,d);
-        //fprintf(fp,"%d %d %l\n ",laser_number,raw->blocks[i].rotation,d);
 
         if (j==0)
         {  
@@ -162,55 +127,24 @@ for (int j = 0, k = 0; j < SCANS_PER_BLOCK; j++, k += RAW_SCAN_SIZE) {
                CompressedData.rotation.push_back(raw->blocks[i].rotation);
             }
         }
-        //fprintf(fp,"%d ",raw->blocks[i].rotation);
-
-
-
 
 }
 }
 
 } 
 
-//imwrite(route,img,compression_params);
-
-
-//ros::init(1,"no","compressed_velodyne_publisher");
-//ros::NodeHandle nh;
-//image_transport::ImageTransport it(nh);
-//image_transport::Publisher pub=it.advertise("image_raw/compressed",1);
-
-
-
-//sensor_msgs::CompressedImage compressed;
 
 CompressedData.header=msg->header;
-
-//cout<<img.rows<<endl;
-//cout<<img2<<endl;
 if (cv::imencode(".png", img, CompressedData.distance, compression_params))
 {
-//pub.publish(compressed);
-//    ros::Publisher pub=nh.advertise<sensor_msgs::CompressedImage>("Compressed",1);
-//    pub.publish(compressed);
 
-//cout<<img2.rows<<" "<<img2.cols<<endl;
     cv::imencode(".png", img2, CompressedData.intensity, compression_params);
     cout<<"Frame "<<i<<" is Compressing"<<endl;
     i++;
 }
-//else
-//{
-//    ROS_ERROR("cv::imencode (png) failed on input image");
-//}
-
-
 
 pubp->publish(CompressedData);
 
-
-
-//fclose(fp);
 }
 
 int main(int argc, char **argv)
@@ -222,7 +156,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
   ros::NodeHandle nh;
-  ros::Publisher pub=nh.advertise<compression_msgs::CompressedPacket>("CompressedPacket",1000);
+  ros::Publisher pub=nh.advertise<velodyne_compression::CompressedPacket>("velodyne_packets_compressed",1000);
 
 
   pubp=&pub;
--- ros/src/sensing/drivers/lidar/packages/velodyne/velodyne_compression/src/recon2packet.cpp
+++ ros/src/sensing/drivers/lidar/packages/velodyne/velodyne_compression/src/recon2packet.cpp
@@ -23,11 +23,7 @@
 #include <vector>
 #include "std_msgs/MultiArrayDimension.h"
 #include "std_msgs/MultiArrayLayout.h"
-#include <compression_msgs/CompressedPacket.h>
-//#include <image_transport/image_transport.h>
-
-
-
+#include <velodyne_compression/CompressedPacket.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -40,18 +36,11 @@ typedef unsigned short uint16;
 
 int recon_frame;
 int rec=0;
-//int too_fast;
-
 uint16* rota;
 uint16 row;
 ros::Publisher *pubp;
 uint16 column;
-//ros::NodeHandle *node2;
-//std_msgs::UInt16MultiArray rotation;
-//uchar* ptr_distance;
-//uchar* ptr_inten;
-//Mat img_distance(64, 2088, CV_16U);
-//Mat img_inten(64, 2088, CV_8U);
+
 
 namespace velodyne_driver
 {
@@ -173,40 +162,14 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
   uint8_t packet[1206];
   uint8_t head_p[6] = { 30, 30, 30, 30, 30, 30 };
   
- //Mat img_distance(row, column, CV_16U,ptr_distance);
- //cout<<img_distance<<endl;
- //Mat img_inten(row, column, CV_8U,ptr_inten);
-
-  //char rot_route[100];
-  //char image_route[100];
   recon_frame++;
   //cout << config_.npackets;
   // Allocate a new shared pointer for zero-copy sharing with other nodelets.
   velodyne_msgs::VelodyneScanPtr scan(new velodyne_msgs::VelodyneScan);
   scan->packets.resize(config_.npackets);
 
-  //int wa;
-  //cin>>wa;
-  //usleep(too_fast);
 
-  //sprintf(rot_route,"%sRotation%06d.txt",fold,recon_frame);
-  //cout<<rot_route<<endl;
-  //sprintf(image_route,"%simage%06d.png",fold,recon_frame);
-
-
-  //img_distance = imread(image_route, CV_LOAD_IMAGE_UNCHANGED);
-  //img_distance=cv::imdecode(dist, CV_LOAD_IMAGE_UNCHANGED);
-  //ifstream rot(rot_route);
-
-
-//  if (!rot.is_open())
-//  {
-//   cout << "End"; return false;
-//  }
-//  else
-//  {
 	cout << "Frame " << recon_frame << " is reconstructing"<<endl;
-//  }
 
 
   if (row==64)
@@ -216,45 +179,25 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
   ID_packet=0;
   for (i = 0; i < column; i++)
   {
-        //cout<<i<<endl;
-    rot_angle=*(rota+i);
-	//cout << rot_angle << endl;
+  rot_angle=*(rota+i);
 	tmp.uint = uint16_t(0xeeff);
 	packet[ID_block * 100] = tmp.bytes[0];
 	packet[ID_block * 100 + 1] = tmp.bytes[1];
-	//cout << tmp.uint << endl;
-	//cout << uint16_t(0xeeff) << endl;
 
-	//cout << (tmp.uint == uint16_t(0xeeff))<<(1==1)<<endl;
 	tmp.uint = rot_angle;
 	packet[ID_block * 100 + 2] = tmp.bytes[0];
 	packet[ID_block * 100 + 3] = tmp.bytes[1];
 
-	//cout << rot_angle <<" "<<tmp.uint << endl;
 	for (j = 0; j < 32; j++)
 	{
-		//cout << i << "." << id_block << "." << j << endl;
 
   intensity=img_inten.at<uchar>(j,i);  
   dis= img_distance.at<ushort>(j, i);
- //cout<<"666"<<endl;
-
-  //cout<<intensity<<endl;
-	// cout << dis << endl;
   dis = (dis+0.5) / 100.0 / 0.002f;
 	tmp.uint = dis;
 	int loca = ID_block * 100 + 4 + j * 3;
-	// cout << "loca" << loca << endl;
 	packet[loca] = tmp.bytes[0];
 	packet[loca + 1] = tmp.bytes[1];
-
-
-	/*tmp.bytes[0] = block[loca];
-	tmp.bytes[1] = block[loca + 1];;
-	float distance = tmp.uint*0.002f;
-	int d = distance*100.0;
-	cout << "recon" << d << endl;*/
-
   packet[loca + 2] = intensity;  //intensity
 	}
 
@@ -263,36 +206,20 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
 	tmp.uint = uint16_t(0xddff);
 	packet[ID_block * 100] = tmp.bytes[0];
 	packet[ID_block * 100 + 1] = tmp.bytes[1];
-	//cout << tmp.uint << endl;
-	//cout << uint16_t(0xeeff) << endl;
 
-	//cout << (tmp.uint == uint16_t(0xeeff))<<(1==1)<<endl;
 	tmp.uint = rot_angle;
 	packet[ID_block * 100 + 2] = tmp.bytes[0];
 	packet[ID_block * 100 + 3] = tmp.bytes[1];
 
 	for (j = 32; j < 64; j++)
 	{
-		//cout << i << "." << id_block << "." << j << endl;
-
-
     dis = img_distance.at<ushort>(j, i);
     intensity=img_inten.at<uchar>(j,i);
-		// cout << dis << endl;
 		dis = (dis+0.5) / 100.0 / 0.002f;
 		tmp.uint = dis;
 		int loca = ID_block * 100 + 4 + (j-32) * 3;
-		// cout << "loca" << loca << endl;
 		packet[loca] = tmp.bytes[0];
 		packet[loca + 1] = tmp.bytes[1];
-
-
-		/*tmp.bytes[0] = block[loca];
-		tmp.bytes[1] = block[loca + 1];;
-		float distance = tmp.uint*0.002f;
-		int d = distance*100.0;
-		cout << "recon" << d << endl;*/
-
     packet[loca + 2] = intensity;  //intensity
 	}
 
@@ -325,42 +252,23 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
     ID_packet=0;
     for (i = 0; i < column; i++)
     {
-          //cout<<i<<endl;
-      rot_angle=*(rota+i);
-    //cout << rot_angle << endl;
+    rot_angle=*(rota+i);
     tmp.uint = uint16_t(0xeeff);
     packet[ID_block * 100] = tmp.bytes[0];
     packet[ID_block * 100 + 1] = tmp.bytes[1];
-    //cout << tmp.uint << endl;
-    //cout << uint16_t(0xeeff) << endl;
 
-    //cout << (tmp.uint == uint16_t(0xeeff))<<(1==1)<<endl;
     tmp.uint = rot_angle;
     packet[ID_block * 100 + 2] = tmp.bytes[0];
     packet[ID_block * 100 + 3] = tmp.bytes[1];
-    //cout << rot_angle <<" "<<tmp.uint << endl;
     for (j = 0; j < 32; j++)
     {
-      //cout << i << "." << id_block << "." << j << endl;
-
-
     dis= img_distance.at<ushort>(j, i);
     intensity=img_inten.at<uchar>(j,i);
-    // cout << dis << endl;
     dis = (dis+0.5) / 100.0 / 0.002f;
     tmp.uint = dis;
     int loca = ID_block * 100 + 4 + j * 3;
-    // cout << "loca" << loca << endl;
     packet[loca] = tmp.bytes[0];
     packet[loca + 1] = tmp.bytes[1];
-
-
-    /*tmp.bytes[0] = block[loca];
-    tmp.bytes[1] = block[loca + 1];;
-    float distance = tmp.uint*0.002f;
-    int d = distance*100.0;
-    cout << "recon" << d << endl;*/
-
     packet[loca + 2] = intensity;  //intensity
     }
     ID_block++;
@@ -380,34 +288,15 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
 
 }
 
-
-  //Mat img_distance(64, 2088, CV_16U);
-  //img_distance = imread("H:\\alpha.png", CV_LOAD_IMAGE_UNCHANGED);
-  //ifstream rot("H:\\Rotation_angle.txt");
-
-
-
-
-
-  // Since the velodyne delivers data at a very high rate, keep
-  // reading and publishing scans as fast as possible.
-
-
-  // publish message using time of last packet read
   ROS_DEBUG("Publishing a full Velodyne scan.");
   scan->header.stamp = ros::Time(scan->packets[config_.npackets - 1].stamp);
   scan->header.frame_id = config_.frame_id;
-  //cout<<scan->packets[1]<<endl;
   pubp->publish(scan);
-  //output_.publish(scan);
 
-  // notify diagnostics that a message has been published, updating
-  // its status
   diag_topic_->tick(scan->header.stamp);
   diagnostics_.update();
   cout <<rec<<endl;
   rec=0;
-  //delete[] ptr_distance;
   delete[] rota;
   return true;
 }
@@ -415,7 +304,7 @@ bool VelodyneDriver::poll(Mat img_distance,Mat img_inten)
 } // namespace velodyne_driver
 
 
-void CompressedPacketCallback(const compression_msgs::CompressedPacket::ConstPtr& CompressedData)
+void CompressedPacketCallback(const velodyne_compression::CompressedPacket::ConstPtr& CompressedData)
  {
    row=CompressedData->scan_row;
    column=CompressedData->scan_column;
@@ -426,30 +315,20 @@ void CompressedPacketCallback(const compression_msgs::CompressedPacket::ConstPtr
    Mat img_inten(row, column, CV_8U);
 
     for(int i=0; i<column;i++ )
-    //or (int *it=msg->data.begin(); it!=msg->data.end();++it)
     {
         *(rota+i)=CompressedData->rotation[i];
-        //cout<<rota[i]<<endl;
     }
 
     rec=1;
 
 
     img_distance=cv::imdecode(CompressedData->distance, CV_LOAD_IMAGE_UNCHANGED);
-
-
     img_inten=cv::imdecode(CompressedData->intensity, CV_LOAD_IMAGE_UNCHANGED);
-
-   // ptr_distance=img_distance.data;
-    //ptr_inten=img_inten.data;
 
     ros::NodeHandle node2;
     ros::NodeHandle private_nh("~");
     velodyne_driver::VelodyneDriver dvr(node2, private_nh);
     dvr.poll(img_distance,img_inten);
-         //cout<<static_cast<const void*>(ptr_distance)<<endl;
-    //Mat img_distance(row, column, CV_16U,ptr_distance);
-   // cout<<img_distance<<endl;
 
     return;
  }
@@ -461,37 +340,12 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "velodyne_decompression");
   ros::NodeHandle node;
   ros::NodeHandle np;
-
-  //node2=&node;
-
-
-
   printf("start\n");
-//  fold=argv[1];
-//  too_fast=atoi(argv[2]);
-//  too_fast=too_fast*1000;
-//  cout << too_fast;
   recon_frame=0;
   ros::Publisher pub=np.advertise<velodyne_msgs::VelodyneScan>("velodyne_packets", 10);
   pubp=&pub;
-//  ros::NodeHandle n;
-//  ros::NodeHandle nh;
+  ros::Subscriber sub = node.subscribe("velodyne_packets_compressed", 1000, CompressedPacketCallback);
 
-  ros::Subscriber sub = node.subscribe("CompressedPacket", 1000, CompressedPacketCallback);
-
-
-  //ros::spin();
-
-  // start the driver
-
-  // loop until shut down or end of file
-  //while(ros::ok() && dvr.poll())
   ros::spin();
-
-   // ros::spinOnce();
-
-
-
-
   return 0;
 }
