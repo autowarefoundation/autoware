76ce4d0a561dd207cf9bfb80db95cadde4f3ee3b
hironari.yashiro <hironari.yashiro@tier4.jp>
2017-09-20 18:14:27 +0900

    fix setup script

--- ui/web/setup.py
+++ ui/web/setup.py
@@ -7,27 +7,27 @@ from os import getcwd, chdir
 rootpath = getcwd()
 print rootpath
 
-# pip install
+# pip
+#call(["pip", "install", "--upgrade", "pip"])
 call(["sudo", "pip", "install", "-r", "requirements.txt"])
+call(["sudo", "pip", "install", "git+https://github.com/strawlab/python-pcl"])
 
-# npm install
+# ROSBridge
+call(["sudo", "apt-get", "update"])
+call(["sudo", "apt-get", "install", "ros-kinetic-rosbridge-server"])
+call(["sudo", "apt-get", "install", "ros-kinetic-tf2-web-republisher"])
+call(["sudo", "apt-get", "install", "ros-kinetic-web-video-server"])
+
+# npm, node
+#call(["sudo", "apt-get", "install", "-y", "nodejs", "npm"])
+#call(["sudo", "npm", "cache", "clean"])
+#call(["sudo", "npm", "install", "n", "-g"])
+#call(["sudo", "n", "stable"])
+#call(["sudo", "ln", "-sf", "/usr/local/bin/node", "/usr/bin/node"])
+
 chdir("{}/app/views".format(rootpath))
 call(["npm", "install"])
-
-# make_launch_files.py
-chdir("{}/app/controllers".format(rootpath))
-call(["python", "make_launch_files.py"])
-
-# env
-chdir("{}/app".format(rootpath))
-call(["ln", "-s", "./config/.env", "./views/.env"])
-
-chdir("{}/app/config".format(rootpath))
-call(["ln", "-s", "sample.env", ".env"])
-
-# gulp browserify
-chdir("{}/app/views".format(rootpath))
-call(["gulp", "browserify"])
+call(["sudo", "npm", "install", "--save", "-g", "gulp-cli"])
 
 # res
 chdir("{}/app/controllers/res/detection".format(rootpath))
@@ -45,4 +45,18 @@ call(["tar", "-zxvf", "bagfile.tar.gz"])
 
 chdir("{}/app/controllers/res/mission".format(rootpath))
 call(["wget", "https://autoware.blob.core.windows.net/web-ui-resouces/waypoints.csv"])
+
+# make_launch_files.py
+chdir("{}/app/controllers".format(rootpath))
+call(["python", "make_launch_files.py"])
+
+# env
+chdir("{}/app/views".format(rootpath))
+call(["ln", "-s", "../config/.env", ".env"])
+chdir("{}/app/config".format(rootpath))
+call(["ln", "-s", "sample.env", ".env"])
+
+# gulp browserify
+chdir("{}/app/views".format(rootpath))
+call(["gulp", "browserify"])
 
