a88c1df2244433fcfe99f489f5ac2b597819dcc9
Akihito Ohsato <aohsato@gmail.com>
2017-10-18 14:54:52 +0900

    Refactor fusion_filter

--- ros/src/sensing/filters/packages/points_preprocessor/nodes/fusion_filter/fusion_filter.cpp
+++ ros/src/sensing/filters/packages/points_preprocessor/nodes/fusion_filter/fusion_filter.cpp
@@ -18,41 +18,65 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 
-ros::Publisher pub_;
-tf::TransformListener *tfl;
-typedef pcl::PointXYZI PointT;
-typedef pcl::PointCloud<PointT> PointCloudT;
-
-void callback(const PointCloudT::ConstPtr &cloudf, const PointCloudT::ConstPtr &cloudr)
+class FusionFilter
 {
+public:
+  FusionFilter();
+private:
+  typedef pcl::PointXYZI PointT;
+  typedef pcl::PointCloud<PointT> PointCloudT;
+  typedef message_filters::sync_policies::ApproximateTime<PointCloudT, PointCloudT> SyncPolicy;
+  ros::NodeHandle nh_, pnh_;
+  message_filters::Subscriber<PointCloudT> *sub1_, *sub2_;
+  message_filters::Synchronizer<SyncPolicy> *sync_;
+  ros::Publisher pub_;
+  tf::TransformListener tfl;
+  std::string sensor_frame;
+  void callback(const PointCloudT::ConstPtr &msg1, const PointCloudT::ConstPtr &msg2);
+};
+
+FusionFilter::FusionFilter() : nh_(), pnh_("~"), tfl(), sensor_frame("base_link")
+{
+  pnh_.param("sensor_frame", sensor_frame, sensor_frame);
+  sub1_ = new message_filters::Subscriber<PointCloudT>(nh_, "/front/velodyne_points", 1);
+  sub2_ = new message_filters::Subscriber<PointCloudT>(nh_, "/rear/velodyne_points", 1);
+  sync_ = new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(10), *sub1_, *sub2_);
+  sync_->registerCallback(boost::bind(&FusionFilter::callback, this, _1, _2));
+  pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/points_raw", 1);
+}
+
+void FusionFilter::callback(const PointCloudT::ConstPtr &msg1, const PointCloudT::ConstPtr &msg2)
+{
+  PointCloudT::Ptr cloud1(new PointCloudT);
+  PointCloudT::Ptr cloud2(new PointCloudT);
   PointCloudT::Ptr cloud(new PointCloudT);
-  PointCloudT::Ptr cloudfm(new PointCloudT);
-  PointCloudT::Ptr cloudrm(new PointCloudT);
-  tfl->waitForTransform("/base_link", cloudf->header.frame_id, ros::Time(0), ros::Duration(1.0));
-  pcl_ros::transformPointCloud("/base_link", *cloudf, *cloudfm, *tfl);
-  tfl->waitForTransform("/base_link", cloudr->header.frame_id, ros::Time(0), ros::Duration(1.0));
-  pcl_ros::transformPointCloud("/base_link", *cloudr, *cloudrm, *tfl);
-  *cloud += *cloudfm;
-  *cloud += *cloudrm;
-  cloud->header = cloudf->header;
-  cloud->header.frame_id = "base_link";
+  // transform points
+  try {
+    tfl.waitForTransform(sensor_frame, msg1->header.frame_id, ros::Time(0), ros::Duration(1.0));
+    pcl_ros::transformPointCloud(sensor_frame, *msg1, *cloud1, tfl);
+  } catch (tf::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
+  try {
+    tfl.waitForTransform(sensor_frame, msg2->header.frame_id, ros::Time(0), ros::Duration(1.0));
+    pcl_ros::transformPointCloud(sensor_frame, *msg2, *cloud2, tfl);
+  } catch (tf::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
+  // merge points
+  *cloud += *cloud1;
+  *cloud += *cloud2;
+  cloud->header = msg1->header;
+  cloud->header.frame_id = sensor_frame;
   pub_.publish(cloud);
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "fusion_filter");
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_("~");
-  tfl = new tf::TransformListener();
-  ros::Subscriber sub1_, sub2_;
-  typedef message_filters::sync_policies::ApproximateTime<PointCloudT, PointCloudT> SyncPolicy;
-  message_filters::Subscriber<PointCloudT> sub1(nh_, "/front/velodyne_points", 1);
-  message_filters::Subscriber<PointCloudT> sub2(nh_, "/rear/velodyne_points", 1);
-  message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(1), sub1, sub2);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
-  pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/points_raw", 1);
-	ros::spin();
-
-	return 0;
+  FusionFilter node;
+  ros::spin();
+  return 0;
 }
