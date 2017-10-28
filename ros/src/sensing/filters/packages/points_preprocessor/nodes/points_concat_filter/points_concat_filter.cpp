c83ac93a322433f2fc82bb2d8520cbbb391e1a21
Akihito OHSATO <aohsato@gmail.com>
2017-10-29 00:19:15 +0900

    Add configrations for runtime_manager

--- ros/src/sensing/filters/packages/points_preprocessor/nodes/points_concat_filter/points_concat_filter.cpp
+++ ros/src/sensing/filters/packages/points_preprocessor/nodes/points_concat_filter/points_concat_filter.cpp
@@ -17,6 +17,7 @@
 #include <velodyne_pointcloud/point_types.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
+#include "autoware_msgs/ConfigPointsConcatFilter.h"
 
 class PointsConcatFilter
 {
@@ -32,23 +33,32 @@ private:
   ros::NodeHandle node_handle_, private_node_handle_;
   message_filters::Subscriber<PointCloudMsgT> *cloud_subscriber1_, *cloud_subscriber2_;
   message_filters::Synchronizer<SyncPolicyT>* cloud_synchronizer_;
+  ros::Subscriber config_subscriber_;
   ros::Publisher cloud_publisher_;
   tf::TransformListener tf_listener_;
   std::string output_frame_;
 
+  void config_callback(const autoware_msgs::ConfigPointsConcatFilter::ConstPtr& config_msg);
   void pointcloud_callback(const PointCloudMsgT::ConstPtr& cloud_msg1, const PointCloudMsgT::ConstPtr& cloud_msg2);
 };
 
 PointsConcatFilter::PointsConcatFilter()
-  : node_handle_(), private_node_handle_("~"), tf_listener_(), output_frame_("output_frame")
+  : node_handle_(), private_node_handle_("~"), tf_listener_(), output_frame_("lidar_frame")
 {
   private_node_handle_.param("output_frame", output_frame_, output_frame_);
+  config_subscriber_ = node_handle_.subscribe<autoware_msgs::ConfigPointsConcatFilter>(
+      "/config/points_concat_filter", 1, &PointsConcatFilter::config_callback, this);
   cloud_subscriber1_ = new message_filters::Subscriber<PointCloudMsgT>(node_handle_, "/lidar0/points_raw", 1);
   cloud_subscriber2_ = new message_filters::Subscriber<PointCloudMsgT>(node_handle_, "/lidar1/points_raw", 1);
   cloud_synchronizer_ =
       new message_filters::Synchronizer<SyncPolicyT>(SyncPolicyT(10), *cloud_subscriber1_, *cloud_subscriber2_);
   cloud_synchronizer_->registerCallback(boost::bind(&PointsConcatFilter::pointcloud_callback, this, _1, _2));
   cloud_publisher_ = node_handle_.advertise<PointCloudMsgT>("/points_concat", 1);
+}
+
+void PointsConcatFilter::config_callback(const autoware_msgs::ConfigPointsConcatFilter::ConstPtr& config_msg)
+{
+  output_frame_ = config_msg->output_frame;
 }
 
 void PointsConcatFilter::pointcloud_callback(const PointCloudMsgT::ConstPtr& cloud_msg1,
