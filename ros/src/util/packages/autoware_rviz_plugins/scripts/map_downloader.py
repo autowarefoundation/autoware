4e062d0b7301fd3157455d168f406c38c8f4c41f
Masaya Kataoka <ms.kataoka@gmail.com>
2017-11-14 21:19:17 +0900

    check python coding rules

--- ros/src/util/packages/autoware_rviz_plugins/scripts/map_downloader.py
+++ ros/src/util/packages/autoware_rviz_plugins/scripts/map_downloader.py
@@ -6,15 +6,17 @@ import requests
 
 def download_map(request_url):
     rospack = rospkg.RosPack()
-    map_image_path = rospack.get_path('autoware_rviz_plugins') + "/media/map.png"
+    map_image_path = rospack.get_path('autoware_rviz_plugins')
+    map_image_path = map_image_path + "/media/map.png"
     try:
-        f=open(map_image_path,'wb')
+        f = open(map_image_path, 'wb')
         f.write(requests.get(request_url).content)
         f.close()
         return 0
     except:
         f.close()
         return -1
+
 
 if __name__ == '__main__':
     pass
