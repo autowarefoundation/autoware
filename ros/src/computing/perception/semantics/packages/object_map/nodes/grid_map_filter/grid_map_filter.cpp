5165e0997cf0ea87edb5b9c69479fa04a3e84773
AMC <abrahammonrroy@yahoo.com>
2018-02-03 05:07:11 +0900

    Added Comments Documentation Code refactoring to follow standards Added libraries Separation of Vectormap Processing inside Clustering

--- ros/src/computing/perception/semantics/packages/object_map/nodes/grid_map_filter/grid_map_filter.cpp
+++ ros/src/computing/perception/semantics/packages/object_map/nodes/grid_map_filter/grid_map_filter.cpp
@@ -1,281 +1,211 @@
+/*
+ *  Copyright (c) 2018, Nagoya University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Autoware nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************/
+
 #include "grid_map_filter.h"
-#include "grid_map_filter_util.h"
 
 namespace object_map
 {
 
 // Constructor
-GridMapFilter::GridMapFilter() :
-    private_nh_("~")
-{
-  initForROS();
-  initForVMap();
-}
+	GridMapFilter::GridMapFilter() :
+			private_node_handle_("~")
+	{
+		InitializeRosIo();
+		LoadRoadAreasFromVectorMap(private_node_handle_, area_points_);
+	}
 
-// Destructor
-GridMapFilter::~GridMapFilter()
-{
-}
+	void GridMapFilter::InitializeRosIo()
+	{
+		private_node_handle_.param<std::string>("map_frame", map_frame_, "map");
+		private_node_handle_.param<std::string>("map_topic", map_topic_, "/realtime_cost_map");
+		private_node_handle_.param<double>("dist_transform_distance", dist_transform_distance_, 3.0);
+		private_node_handle_.param<bool>("use_dist_transform", use_dist_transform_, false);
+		private_node_handle_.param<bool>("use_wayarea", use_wayarea_, false);
+		private_node_handle_.param<bool>("use_fill_circle", use_fill_circle_, false);
+		private_node_handle_.param<int>("fill_circle_cost_threshold", fill_circle_cost_thresh_, 20);
+		private_node_handle_.param<double>("circle_radius", circle_radius_, 1.7);
 
-void GridMapFilter::initForROS()
-{
-  // rosparam setting
-  private_nh_.param<std::string>("map_frame", map_frame_, "map");
-  private_nh_.param<std::string>("map_topic", map_topic_, "/realtime_cost_map");
-  private_nh_.param<double>("dist_transform_distance", dist_transform_distance_, 3.0);
-  private_nh_.param<bool>("use_dist_transform", use_dist_transform_, false);
-  private_nh_.param<bool>("use_wayarea", use_wayarea_, false);
-  private_nh_.param<bool>("use_fill_circle", use_fill_circle_, false);
-  private_nh_.param<int>("fill_circle_cost_threshold", fill_circle_cost_thresh_, 20);
-  private_nh_.param<double>("circle_radius", circle_radius_, 1.7);
+		occupancy_grid_sub_ = nh_.subscribe<nav_msgs::OccupancyGrid>(map_topic_, 10,
+		                                                             &GridMapFilter::OccupancyGridCallback, this);
 
-  // setup subscriber
-  occupancy_grid_sub_ = nh_.subscribe<nav_msgs::OccupancyGrid>(map_topic_, 10, &GridMapFilter::callbackFromOccupancyGrid, this);
+		grid_map_pub_ = nh_.advertise<grid_map_msgs::GridMap>("filtered_grid_map", 1, true);
 
-  // setup publisher
-  grid_map_pub_ = nh_.advertise<grid_map_msgs::GridMap>("filtered_grid_map", 1, true);
-}
+	}
 
-// subscribe vector map infomation and create wayarea points
-void GridMapFilter::initForVMap()
-{
-  // subscribe vector map data
-  // non blocking
-  vector_map::VectorMap vmap;
-  vmap.subscribe(nh_, vector_map::Category::POINT | vector_map::Category::LINE | vector_map::Category::AREA | vector_map::Category::WAY_AREA, ros::Duration(3.0));
 
-  // all true -> all data
-  std::vector<vector_map_msgs::WayArea> way_areas =
-      vmap.findByFilter([](const vector_map_msgs::WayArea& way_area){return true;});
+	void GridMapFilter::Run()
+	{
+		ros::spin();
+	}
 
-  if (way_areas.empty())
-  {
-    ROS_WARN_STREAM("No WayArea...");
-    return;
-  }
+	void GridMapFilter::OccupancyGridCallback(const nav_msgs::OccupancyGridConstPtr &in_message)
+	{
+		// timer start
+		//auto start = std::chrono::system_clock::now();
 
-  for (const auto& way_area : way_areas)
-  {
-    vector_map_msgs::Area area = vmap.findByKey(vector_map::Key<vector_map::Area>(way_area.aid));
-    area_points_.emplace_back(searchAreaPoints(area, vmap));
-  }
+		std::string original_layer = "original";
 
-}
+		grid_map::GridMap map({original_layer, "distance_transform", "wayarea", "dist_wayarea", "circle"});
 
-void GridMapFilter::run() const
-{
-  ros::spin();
-}
+		//store costmap map_topic_ into the original layer
+		grid_map::GridMapRosConverter::fromOccupancyGrid(*in_message, "original", map);
 
-void GridMapFilter::callbackFromOccupancyGrid(const nav_msgs::OccupancyGridConstPtr& msg) const
-{
-  // timer start
-  auto start = std::chrono::system_clock::now();
+		// apply distance transform to OccupancyGrid
+		if (use_dist_transform_)
+		{
+			CreateDistanceTransformLayer(map, original_layer);
+		}
 
-  ROS_INFO_STREAM("Subscribed Occupancy Grid Map");
+		// fill polygon
+		if (!area_points_.empty() && use_wayarea_)
+		{
+			FillPolygonAreas(map, area_points_, grid_road_layer_, OCCUPANCY_NO_ROAD, OCCUPANCY_ROAD, grid_min_value_,
+			                 grid_max_value_, map.getFrameId(), map_frame_, tf_listener_);
 
-  std::string original_layer = "original";
-  static grid_map::GridMap map({original_layer, "distance_transform", "wayarea", "dist_wayarea", "circle"});
-  grid_map::GridMapRosConverter::fromOccupancyGrid(*msg, "original", map);
+			map["dist_wayarea"] = map["distance_transform"] + map["wayarea"];
+		}
 
-  // apply distance transform to OccupancyGrid
-  if (use_dist_transform_)
-  {
-    distanceTransform(map, original_layer);
-  }
+		// fill circle
+		if (use_fill_circle_)
+		{
+			int cost_threshold = fill_circle_cost_thresh_;
+			// convert to cv image size
+			int radius = circle_radius_ / map.getResolution();
+			DrawCirclesInLayer(map, original_layer, cost_threshold, radius);
+		}
 
-  // fill polygon
-  if (!area_points_.empty() && use_wayarea_)
-  {
-    fillPolygonCV(map, area_points_);
-    map["dist_wayarea"] = map["distance_transform"] + map["wayarea"];
-  }
+		// publish grid map as ROS message
+		PublishGridMap(map, grid_map_pub_);
 
-  // fill circle
-  if (use_fill_circle_)
-  {
-    // OccupancyGrid (0~100) -> cv image (0~255)
-    int cost_threshold = fill_circle_cost_thresh_ * (255.0 / 100.0);
-    //double vehicle_width = 2.4;
-    //int radius = (vehicle_width * std::sqrt(2.0) / 2.0) / map.getResolution();
-    // convert to cv image size
-    int radius = circle_radius_ / map.getResolution();
-    fillCircleCV(map, original_layer, cost_threshold, radius);
-  }
+		// timer end
+		//auto end = std::chrono::system_clock::now();
+		//auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+		//std::cout << "time: " << usec / 1000.0 << " [msec]" << std::endl;
+	}
 
-  // publish grid map as ROS message
-  publishMap(map, grid_map_pub_);
+	void GridMapFilter::CreateDistanceTransformLayer(grid_map::GridMap &out_grid_map, const std::string &in_layer)
+	{
+		cv::Mat original_image;
+		if (!out_grid_map.exists(in_layer))
+		{
+			ROS_INFO("%s layer not yet available", in_layer.c_str());
+			return;
+		}
+		grid_map::GridMapCvConverter::toImage<unsigned char, 1>(out_grid_map,
+		                                                        in_layer,
+		                                                        CV_8UC1,
+		                                                        original_image);
 
-  // timer end
-  auto end = std::chrono::system_clock::now();
-  auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-  std::cout << "time: " << usec / 1000.0 << " [msec]" << std::endl;
-}
+		cv::Mat binary_image;
+		cv::threshold(original_image,
+		              binary_image,
+		              fill_circle_cost_thresh_,
+		              grid_max_value_,
+		              cv::THRESH_BINARY_INV);
 
-void GridMapFilter::distanceTransform(grid_map::GridMap& map, const std::string& layer) const
-{
-  cv::Mat original_image;
-  grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, layer, CV_8UC1, original_image);
+		// distance transform method
+		// 3: fast
+		// 5: slow but accurate
+		cv::Mat dt_image;
+		cv::distanceTransform(binary_image, dt_image, CV_DIST_L2, 5);
 
-  // binalize
-  cv::Mat binary_image;
-  cv::threshold(original_image, binary_image, 20, 100, cv::THRESH_BINARY_INV);
+		// Convert to int...
+		cv::Mat dt_int_image(dt_image.size(), CV_8UC1);
+		cv::Mat dt_int_inv_image(dt_image.size(), CV_8UC1);
 
-  // distance transform
-  // 3: fast
-  // 5: slow but accurate
-  cv::Mat dt_image;
-  cv::distanceTransform(binary_image, dt_image, CV_DIST_L2, 5);
+		// max distance for cost propagation
+		double max_dist = dist_transform_distance_; // meter
+		double resolution = out_grid_map.getResolution();
 
-  // Convert to int...
-  cv::Mat dt_int_image(dt_image.size(), CV_8UC1);
-  cv::Mat dt_int_inv_image(dt_image.size(), CV_8UC1);
+		for (int y = 0; y < dt_image.rows; y++)
+		{
+			for (int x = 0; x < dt_image.cols; x++)
+			{
+				// actual distance [meter]
+				double dist = dt_image.at<float>(y, x) * resolution;
+				if (dist > max_dist)
+					dist = max_dist;
 
-  // max distance for cost propagation
-  double max_dist = dist_transform_distance_; // meter
-  double resolution = map.getResolution();
+				// Make value range 0 ~ 255
+				int round_dist = dist / max_dist * grid_max_value_;
+				int inv_round_dist = grid_max_value_ - round_dist;
 
-  for (int y = 0; y < dt_image.rows; y++)
-  {
-    for (int x = 0; x < dt_image.cols; x++)
-    {
-      // actual distance [meter]
-      double dist = dt_image.at<float>(y, x) * resolution;
-      if (dist > max_dist)
-        dist = max_dist;
+				dt_int_image.at<unsigned char>(y, x) = round_dist;
+				dt_int_inv_image.at<unsigned char>(y, x) = inv_round_dist;
+			}
+		}
 
-      // Make value range 0 ~ 255
-      int round_dist = dist / max_dist * 255;
-      int inv_round_dist = 255 - round_dist;
+		// convert to ROS msg
+		grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(dt_int_inv_image,
+		                                                                  "distance_transform",
+		                                                                  out_grid_map,
+		                                                                  grid_min_value_,
+		                                                                  grid_max_value_);
+	}
 
-      dt_int_image.at<unsigned char>(y, x)     = round_dist;
-      dt_int_inv_image.at<unsigned char>(y, x) = inv_round_dist;
-    }
-  }
+	void GridMapFilter::DrawCirclesInLayer(grid_map::GridMap &out_gridmap,
+	                                       const std::string &in_layer_name,
+	                                       double in_draw_threshold,
+	                                       double in_radius)
+	{
+		cv::Mat original_image;
 
-  // convert to ROS msg
-  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(dt_int_inv_image, "distance_transform", map, 0, 100);
-  //grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(dt_int_inv_image, "dist_elevation", map, 0, 1.5);
-}
+		grid_map::GridMapCvConverter::toImage<unsigned char, 1>(out_gridmap,
+		                                                        in_layer_name,
+		                                                        CV_8UC1,
+		                                                        costmap_min_,
+		                                                        costmap_max_,
+		                                                        original_image);
 
-// convert vector map area points to ros points
-std::vector<geometry_msgs::Point> GridMapFilter::searchAreaPoints(const vector_map::Area& area, const vector_map::VectorMap& vmap) const
-{
-  std::vector<geometry_msgs::Point> area_points;
-  std::vector<geometry_msgs::Point> area_points_empty;
+		cv::Mat filled_image = original_image.clone();
 
-  if (area.aid == 0)
-    return area_points_empty;
+		for (int y = 0; y < original_image.rows; y++)
+		{
+			for (int x = 0; x < original_image.cols; x++)
+			{
+				// uchar -> int
+				int data = original_image.at<unsigned char>(y, x);
 
-  vector_map_msgs::Line line = vmap.findByKey(vector_map::Key<vector_map_msgs::Line>(area.slid));
-  // must set beginning line
-  if (line.lid == 0 || line.blid != 0)
-    return area_points_empty;
-
-  // Search all lines in area
-  while (line.flid != 0)
-  {
-    vector_map_msgs::Point bp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.bpid));
-    if (bp.pid == 0)
-      return area_points_empty;
-
-    vector_map_msgs::Point fp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.fpid));
-    if (fp.pid == 0)
-      return area_points_empty;
-
-    // 2 points of line
-    area_points.push_back(vector_map::convertPointToGeomPoint(bp));
-    area_points.push_back(vector_map::convertPointToGeomPoint(fp));
-
-    line = vmap.findByKey(vector_map::Key<vector_map_msgs::Line>(line.flid));
-    if (line.lid == 0)
-      return area_points_empty;
-  }
-
-  vector_map_msgs::Point bp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.bpid));
-  vector_map_msgs::Point fp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.fpid));
-  if (bp.pid == 0 || fp.pid == 0)
-    return area_points_empty;
-
-  area_points.push_back(vector_map::convertPointToGeomPoint(bp));
-  area_points.push_back(vector_map::convertPointToGeomPoint(fp));
-
-  return area_points;
-}
-
-// fill polygon area of OccupancyGrid in "layer"
-void GridMapFilter::fillPolygonCV(grid_map::GridMap& map, const std::vector<std::vector<geometry_msgs::Point>>& area_points_vec) const
-{
-  map["wayarea"].setConstant(100);
-
-  cv::Mat original_image;
-  grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, "wayarea", CV_8UC1, 0, 100, original_image);
-
-  // deep copy
-  cv::Mat filled_image = original_image.clone();
-
-  // for obtaining vector map coordinate in velodyne frame
-  tf::StampedTransform tf = findTransform(map.getFrameId(), map_frame_, tf_listener_);
-
-  // calculate map position
-  grid_map::Position map_pos = map.getPosition();
-  double origin_x_offset = map.getLength().x() / 2.0 - map_pos.x();
-  double origin_y_offset = map.getLength().y() / 2.0 - map_pos.y();
-
-  for (const auto& points : area_points_vec)
-  {
-    std::vector<cv::Point> cv_points;
-
-    for (const auto& p : points)
-    {
-      // transform to GridMap coordinate
-      geometry_msgs::Point tf_point = transformPoint(p, tf);
-
-      // coordinate conversion for cv image
-      double cv_x = (map.getLength().y() - origin_y_offset - tf_point.y) / map.getResolution();
-      double cv_y = (map.getLength().x() - origin_x_offset - tf_point.x) / map.getResolution();
-      cv_points.emplace_back(cv::Point(cv_x, cv_y));
-    }
-
-    // fill polygon
-    // NOTE: cv::Scalar(255, 255, 255) is white, and white means 100 in Occupancy Grid of ROS
-    //       cv::Scalar(  0,   0,   0) -> 0
-    //       cv::Scalar(0) is Okay... (1 channel)
-    cv::fillConvexPoly(filled_image, cv_points.data(), cv_points.size(), cv::Scalar(0));
-  }
-
-  // convert to ROS msg
-  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(filled_image, "wayarea", map, 0, 100);
-}
-
-// create configuration space from layer
-void GridMapFilter::fillCircleCV(grid_map::GridMap& map, const std::string& layer, const double cost_threshold, const double radius) const
-{
-  map["circle"] = map[layer];
-  cv::Mat original_image;
-
-  // 0 ~ 100 -> 0 ~ 255
-  grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, "circle", CV_8UC1, 0, 100, original_image);
-
-  cv::Mat filled_image = original_image.clone();
-
-  for (int y = 0; y < original_image.rows; y++)
-  {
-    for (int x = 0; x < original_image.cols; x++)
-    {
-      // uchar -> int
-      int data = original_image.at<unsigned char>(y, x);
-
-      if (data > cost_threshold)
-      {
-        cv::circle(filled_image, cv::Point(x, y), radius, cv::Scalar(255), -1, CV_AA);
-      }
-    }
-  }
-
-  // convert to ROS msg
-  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(filled_image, "circle", map, 0, 100);
-}
+				if (data > fill_circle_cost_thresh_)
+				{
+					cv::circle(filled_image, cv::Point(x, y), in_radius, cv::Scalar(OCCUPANCY_CIRCLE), -1, CV_AA);
+				}
+			}
+		}
+		// convert to ROS msg
+		grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(filled_image,
+		                                                                  "circle",
+		                                                                  out_gridmap,
+		                                                                  grid_min_value_,
+		                                                                  grid_max_value_);
+	}
 
 
 }  // namespace object_map
--- ros/src/computing/perception/semantics/packages/object_map/nodes/wayarea2grid/wayarea2grid.cpp
+++ ros/src/computing/perception/semantics/packages/object_map/nodes/wayarea2grid/wayarea2grid.cpp
@@ -1,188 +1,96 @@
+/*
+ *  Copyright (c) 2018, Nagoya University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Autoware nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************/
+
 #include "wayarea2grid.h"
-#include "wayarea2grid_util.h"
 
 namespace object_map
 {
 
-// Constructor
-WayareaToGrid::WayareaToGrid() :
-    private_nh_("~")
-{
-  initForROS();
-  initForVMap();
-}
-
-// Destructor
-WayareaToGrid::~WayareaToGrid()
-{
-}
-
-void WayareaToGrid::initForROS()
-{
-  // rosparam setting
-  private_nh_.param<std::string>("sensor_frame", sensor_frame_, "velodyne");
-  private_nh_.param<std::string>("map_frame", map_frame_, "map");
-  private_nh_.param<double>("map_resolution", map_resolution_, 0.2);
-  private_nh_.param<double>("map_length_x", map_length_x_, 80);
-  private_nh_.param<double>("map_length_y", map_length_y_, 30);
-  private_nh_.param<double>("map_position_x", map_position_x_, 20);
-  private_nh_.param<double>("map_position_y", map_position_y_, 0);
-
-  // setup publisher
-  grid_map_pub_ = nh_.advertise<grid_map_msgs::GridMap>("wayarea_grid_map", 1, true);
-}
-
-// subscribe vector map infomation and create wayarea points
-void WayareaToGrid::initForVMap()
-{
-  // subscribe vector map data
-  // non blocking
-  vector_map::VectorMap vmap;
-  vmap.subscribe(nh_, vector_map::Category::POINT | vector_map::Category::LINE | vector_map::Category::AREA | vector_map::Category::WAY_AREA, ros::Duration(3.0));
-
-  // all true -> all data
-  std::vector<vector_map_msgs::WayArea> way_areas =
-      vmap.findByFilter([](const vector_map_msgs::WayArea& way_area){return true;});
-
-  if (way_areas.empty())
-  {
-    ROS_WARN_STREAM("No WayArea...");
-    return;
-  }
-
-  for (const auto& way_area : way_areas)
-  {
-    vector_map_msgs::Area area = vmap.findByKey(vector_map::Key<vector_map::Area>(way_area.aid));
-    area_points_.emplace_back(searchAreaPoints(area, vmap));
-  }
-
-}
-
-void WayareaToGrid::run() const
-{
-  bool set_map = false;
-  ros::Rate loop_rate(10);
-  while(ros::ok())
-  {
-    static grid_map::GridMap map({"wayarea"});
-    if (!set_map)
-    {
-      map.setFrameId(sensor_frame_);
-      map.setGeometry(grid_map::Length(map_length_x_, map_length_y_),
-                      map_resolution_,
-                      grid_map::Position(map_position_x_, map_position_y_));
-      set_map = true;
-    }
-
-    // timer start
-    auto start = std::chrono::system_clock::now();
-
-    if (!area_points_.empty())
-    {
-      fillPolygonCV(map, area_points_);
-    }
-
-    publishMap(map, grid_map_pub_);
-
-    // timer end
-    auto end = std::chrono::system_clock::now();
-    auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-    std::cout << "time: " << usec / 1000.0 << " [msec]" << std::endl;
-
-    loop_rate.sleep();
-  }
-}
+	WayareaToGrid::WayareaToGrid() :
+			private_node_handle_("~")
+	{
+		InitializeRosIo();
+		LoadRoadAreasFromVectorMap(private_node_handle_, area_points_);
+	}
 
 
-// convert vector map area points to ros points
-std::vector<geometry_msgs::Point> WayareaToGrid::searchAreaPoints(const vector_map::Area& area, const vector_map::VectorMap& vmap) const
-{
-  std::vector<geometry_msgs::Point> area_points;
-  std::vector<geometry_msgs::Point> area_points_empty;
+	void WayareaToGrid::InitializeRosIo()
+	{
+		private_node_handle_.param<std::string>("sensor_frame", sensor_frame_, "velodyne");
+		private_node_handle_.param<std::string>("map_frame", map_frame_, "map");
+		private_node_handle_.param<double>("grid_resolution", grid_resolution_, 0.2);
+		private_node_handle_.param<double>("grid_length_x", grid_length_x_, 80);
+		private_node_handle_.param<double>("grid_length_y", grid_length_y_, 30);
+		private_node_handle_.param<double>("grid_position_x", grid_position_x_, 20);
+		private_node_handle_.param<double>("grid_position_x", grid_position_y_, 0);
 
-  if (area.aid == 0)
-    return area_points_empty;
+		publisher_grid_map_ = node_handle_.advertise<grid_map_msgs::GridMap>("grid_map_wayarea", 1, true);
+		publisher_occupancy_ = node_handle_.advertise<nav_msgs::OccupancyGrid>("occupancy_wayarea", 1, true);
+	}
 
-  vector_map_msgs::Line line = vmap.findByKey(vector_map::Key<vector_map_msgs::Line>(area.slid));
-  // must set beginning line
-  if (line.lid == 0 || line.blid != 0)
-    return area_points_empty;
+	void WayareaToGrid::Run()
+	{
+		bool set_map = false;
+		ros::Rate loop_rate(10);
 
-  // Search all lines in area
-  while (line.flid != 0)
-  {
-    vector_map_msgs::Point bp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.bpid));
-    if (bp.pid == 0)
-      return area_points_empty;
+		while (ros::ok())
+		{
+			if (!set_map)
+			{
+				gridmap_.add(grid_layer_name_);
+				gridmap_.setFrameId(sensor_frame_);
+				gridmap_.setGeometry(grid_map::Length(grid_length_x_, grid_length_y_),
+				                grid_resolution_,
+				                grid_map::Position(grid_position_x_, grid_position_y_));
+				set_map = true;
+			}
 
-    vector_map_msgs::Point fp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.fpid));
-    if (fp.pid == 0)
-      return area_points_empty;
+			// timer start
+			//auto start = std::chrono::system_clock::now();
 
-    // 2 points of line
-    area_points.push_back(vector_map::convertPointToGeomPoint(bp));
-    area_points.push_back(vector_map::convertPointToGeomPoint(fp));
+			if (!area_points_.empty())
+			{
+				FillPolygonAreas(gridmap_, area_points_, grid_layer_name_, OCCUPANCY_NO_ROAD, OCCUPANCY_ROAD, grid_min_value_,
+				                 grid_max_value_, sensor_frame_, map_frame_,
+				                 tf_listener_);
+				PublishGridMap(gridmap_, publisher_grid_map_);
+				PublishOccupancyGrid(gridmap_, publisher_occupancy_, grid_layer_name_, grid_min_value_, grid_max_value_);
+			}
 
-    line = vmap.findByKey(vector_map::Key<vector_map_msgs::Line>(line.flid));
-    if (line.lid == 0)
-      return area_points_empty;
-  }
+			// timer end
+			//auto end = std::chrono::system_clock::now();
+			//auto usec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+			//std::cout << "time: " << usec / 1000.0 << " [msec]" << std::endl;
 
-  vector_map_msgs::Point bp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.bpid));
-  vector_map_msgs::Point fp = vmap.findByKey(vector_map::Key<vector_map_msgs::Point>(line.fpid));
-  if (bp.pid == 0 || fp.pid == 0)
-    return area_points_empty;
-
-  area_points.push_back(vector_map::convertPointToGeomPoint(bp));
-  area_points.push_back(vector_map::convertPointToGeomPoint(fp));
-
-  return area_points;
-}
-
-// fill polygon area of OccupancyGrid in "layer"
-void WayareaToGrid::fillPolygonCV(grid_map::GridMap& map, const std::vector<std::vector<geometry_msgs::Point>>& area_points_vec) const
-{
-  map["wayarea"].setConstant(100);
-
-  cv::Mat original_image;
-  grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, "wayarea", CV_8UC1, 0, 100, original_image);
-
-  // deep copy
-  cv::Mat filled_image = original_image.clone();
-
-  // for obtaining vector map coordinate in velodyne frame
-  tf::StampedTransform tf = findTransform(map.getFrameId(), map_frame_, tf_listener_);
-
-  // calculate map position
-  grid_map::Position map_pos = map.getPosition();
-  double origin_x_offset = map.getLength().x() / 2.0 - map_pos.x();
-  double origin_y_offset = map.getLength().y() / 2.0 - map_pos.y();
-
-  for (const auto& points : area_points_vec)
-  {
-    std::vector<cv::Point> cv_points;
-
-    for (const auto& p : points)
-    {
-      // transform to GridMap coordinate
-      geometry_msgs::Point tf_point = transformPoint(p, tf);
-
-      // coordinate conversion for cv image
-      double cv_x = (map.getLength().y() - origin_y_offset - tf_point.y) / map.getResolution();
-      double cv_y = (map.getLength().x() - origin_x_offset - tf_point.x) / map.getResolution();
-      cv_points.emplace_back(cv::Point(cv_x, cv_y));
-    }
-
-    // fill polygon
-    // NOTE: cv::Scalar(255, 255, 255) is white, and white means 100 in Occupancy Grid of ROS
-    //       cv::Scalar(  0,   0,   0) -> 0
-    //       cv::Scalar(0) is Okay... (1 channel)
-    cv::fillConvexPoly(filled_image, cv_points.data(), cv_points.size(), cv::Scalar(0));
-  }
-
-  // convert to ROS msg
-  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(filled_image, "wayarea", map, 0, 100);
-}
-
+			loop_rate.sleep();
+		}
+	}
 
 }  // namespace object_map
