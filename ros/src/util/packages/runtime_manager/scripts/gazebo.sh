7a17892647d46a2c0e1d7a610a51f4fc3d12a466
Huckleberry Febbo <febbo@umich.edu>
2018-02-21 05:02:48 -0500

    modified ui for car_demo

--- ros/src/util/packages/runtime_manager/scripts/gazebo.sh
+++ ros/src/util/packages/runtime_manager/scripts/gazebo.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-roslaunch car_demo demo.launch &
-# roslaunch catvehicle catvehicle_skidpan.launch &
+roslaunch car_demo demo.launch & # H.F.
+# roslaunch catvehicle catvehicle_skidpan.launch & # H.F.
 gzclient &
-roslaunch point_cloud_converter point_cloud_converter.launch &
-roslaunch laser_scan_converter laser_scan_converter.launch &
-roslaunch twist_cmd_converter twist_cmd_converter.launch &
+# roslaunch point_cloud_converter point_cloud_converter.launch & # H.F.
+# roslaunch laser_scan_converter laser_scan_converter.launch & # H.F.
+# roslaunch twist_cmd_converter twist_cmd_converter.launch & # H.F.
 
 while :; do sleep 10; done
 
