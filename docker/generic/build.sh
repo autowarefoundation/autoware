79e3fac9e6f816baf2123c4cd435973a19e226f9
Koen Lekkerkerker <k.lekkerkerker@robotcaresystems.com>
2018-03-29 09:10:42 +0200

    Accidentally forget to remove some commenting that I added for testing purposes

--- docker/generic/build.sh
+++ docker/generic/build.sh
@@ -4,9 +4,9 @@
 if ([ "$1" = "kinetic" ] || [ "$1" = "indigo" ]) && ([ "$2" = "" ] || [ $2 = "nvidia" ])
 then
     echo "Use $1"
-    #docker build -t autoware-$1 -f Dockerfile.$1 .
+    docker build -t autoware-$1 -f Dockerfile.$1 .
     if [ $2 = "nvidia" ]; then
-      #docker build --build-arg FROM_ARG=autoware-$1 -t autoware-$1-$2 -f Dockerfile.nvidia-docker-v2 .
+      docker build --build-arg FROM_ARG=autoware-$1 -t autoware-$1-$2 -f Dockerfile.nvidia-docker-v2 .
     fi
 else
     echo "Select distribution as first argument 'kinetic' or 'indigo' and optionally 'nvidia' as 2nd argument to target nvidia docker v2"
