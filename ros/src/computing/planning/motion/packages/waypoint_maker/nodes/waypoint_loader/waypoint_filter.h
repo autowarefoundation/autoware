fb284523027c0df1ec513b09c6426c9d235c9f12
Yuma <yuma.nihei@tier4.jp>
2018-04-03 21:18:30 +0900

    Clang-format

--- ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_filter.h
+++ ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_filter.h
@@ -6,7 +6,8 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this
+ *  * Redistributions of source code must retain the above copyright notice,
+ this
  *    list of conditions and the following disclaimer.
  *
  *  * Redistributions in binary form must reproduce the above copyright notice,
@@ -19,30 +20,31 @@
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE
  *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #ifndef __WAYPOINT_FILTER_H__
 #define __WAYPOINT_FILTER_H__
 
-#include <ros/ros.h>
-#include <tf/transform_datatypes.h>
+#include "autoware_msgs/lane.h"
 #include <autoware_msgs/ConfigWaypointLoader.h>
 #include <fstream>
+#include <ros/ros.h>
+#include <tf/transform_datatypes.h>
 #include <unordered_map>
-#include "autoware_msgs/lane.h"
 
-namespace waypoint_maker
-{
+namespace waypoint_maker {
 
-class WaypointFilter
-{
+class WaypointFilter {
 private:
   ros::NodeHandle private_nh_;
   double r_th_, r_min_, r_inf_;
@@ -51,20 +53,30 @@ private:
   double accel_limit_, decel_limit_, resample_interval_;
   int velocity_offset_;
   bool resample_mode_;
+
 public:
   WaypointFilter();
   ~WaypointFilter();
-  void initParameter(const autoware_msgs::ConfigWaypointLoader::ConstPtr& conf);
+  void initParameter(const autoware_msgs::ConfigWaypointLoader::ConstPtr &conf);
   void filterLaneWaypoint(autoware_msgs::lane *lane);
+
 protected:
-  void resampleLaneWaypoint(const double resample_interval, autoware_msgs::lane *lane, std::vector<double> *curve_radius);
-  void getCurveAll(const autoware_msgs::lane& lane, std::vector<double> *curve_radius);
-  const std::vector<double> calcVelParamFromVmax(const double vmax)const;
-  void createCurveList(const std::vector<double>& curve_radius, std::unordered_map<unsigned long, std::pair<unsigned long, double> >* curve_list);
-  void limitAccelDecel(const double vmax, const double vmin_local, const unsigned long idx, autoware_msgs::lane *lane);
-  const std::vector<double> getCurveOnce(const std::vector<geometry_msgs::Point>& point)const;
-  const double calcSquareSum(const double x, const double y)const;
-  const double calcPathLength(const autoware_msgs::lane& lane)const;
+  void resampleLaneWaypoint(const double resample_interval,
+                            autoware_msgs::lane *lane,
+                            std::vector<double> *curve_radius);
+  void getCurveAll(const autoware_msgs::lane &lane,
+                   std::vector<double> *curve_radius);
+  const std::vector<double> calcVelParamFromVmax(const double vmax) const;
+  void createCurveList(
+      const std::vector<double> &curve_radius,
+      std::unordered_map<unsigned long, std::pair<unsigned long, double>>
+          *curve_list);
+  void limitAccelDecel(const double vmax, const double vmin_local,
+                       const unsigned long idx, autoware_msgs::lane *lane);
+  const std::vector<double>
+  getCurveOnce(const std::vector<geometry_msgs::Point> &point) const;
+  const double calcSquareSum(const double x, const double y) const;
+  const double calcPathLength(const autoware_msgs::lane &lane) const;
 };
 }
 #endif
--- ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader_node.cpp
+++ ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader_node.cpp
@@ -5,7 +5,8 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this
+ *  * Redistributions of source code must retain the above copyright notice,
+ * this
  *    list of conditions and the following disclaimer.
  *
  *  * Redistributions in binary form must reproduce the above copyright notice,
@@ -18,13 +19,16 @@
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
  *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
@@ -33,8 +37,7 @@
 
 #include "waypoint_loader_core.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "waypoint_loader");
   waypoint_maker::WaypointLoaderNode wln;
   ros::spin();
