2900bd2da0331ec1dc12f6c26573f71b70c2ce3c
Marko Durkovic <marko@miding.de>
2018-04-16 12:44:35 +0200

    Force allocation of pseudo tty.

--- ros/src/util/packages/runtime_manager/scripts/run-lidartracker.sh
+++ ros/src/util/packages/runtime_manager/scripts/run-lidartracker.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-ssh -t -oBatchMode=yes 10.31.32.250 '.local/bin/ade enter "sudo bash -c \"source /opt/ros2/local_setup.bash && source /opt/apex_ws/local_setup.bash && lidar_detector_exe --proc_cpu_mask=62 --proc_prio=5\""'
+exec ssh -tt -oBatchMode=yes 10.31.32.250 '.local/bin/ade enter "sudo bash -c \"export ROS_DOMAIN_ID=250 && source /opt/ros2/local_setup.bash && source /opt/apex_ws/local_setup.bash && lidar_detector_exe --proc_cpu_mask=62 --proc_prio=5\""'
