d94b0ce42ccf743ec36d5f8062045ca7288eb1d4
Sumanth Nirmal <sumanth.724@gmail.com>
2018-04-17 16:26:44 -0700

    added lidar tracker and bridge into allsensors moved run-lidartracker.sh and run-ros1bridge.sh to apex_ros1

--- ros/src/util/packages/runtime_manager/scripts/run-lidartracker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-exec ssh -tt -oBatchMode=yes 10.31.32.250 '.local/bin/ade enter "sudo bash -c \"export ROS_DOMAIN_ID=250 && source /opt/ros2/local_setup.bash && source /opt/apex_ws/local_setup.bash && lidar_detector_exe --proc_cpu_mask=62 --proc_prio=5\""'
--- ros/src/util/packages/runtime_manager/scripts/run-ros1bridge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source ~/autoware/autoware/ros/devel/setup.bash
-source ~/autoware/autoware/ros/src/apex_ros1/as_vehicle/install/setup.bash --extend
-source ~/autoware/ros1_bridge/install_isolated/ros1_bridge/setup.bash
-ros2 run ros1_bridge dynamic_bridge --bridge-all-2to1-topics
