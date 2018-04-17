3b0ea260a9069a8d82050a48aae7d39cf8eb15b0
Sumanth Nirmal <sumanth.724@gmail.com>
2018-04-17 11:02:25 -0700

    AllSensors button now executes all_process.sh

--- ros/src/util/packages/runtime_manager/scripts/run-allsensors.sh
+++ ros/src/util/packages/runtime_manager/scripts/run-allsensors.sh
@@ -4,4 +4,5 @@ set -e
 
 source ~/autoware/autoware/ros/devel/setup.bash
 source ~/autoware/autoware/ros/src/apex_ros1/as_vehicle/install/setup.bash --extend
-roslaunch demo_scripts all_sensors.launch
+#roslaunch demo_scripts all_sensors.launch
+bash ~/autoware/autoware/ros/src/apex_ros1/demo_scripts/scripts/all_process.sh
