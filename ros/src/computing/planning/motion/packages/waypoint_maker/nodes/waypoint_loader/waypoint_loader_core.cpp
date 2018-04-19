64c61e2df09e2c30ac798ab236957527b45de122
Yuma <yuma.nihei@tier4.jp>
2018-04-19 15:34:23 +0900

    Ignore space character of waypoints.csv

--- ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader_core.cpp
+++ ros/src/computing/planning/motion/packages/waypoint_maker/nodes/waypoint_loader/waypoint_loader_core.cpp
@@ -342,7 +342,14 @@ void parseColumns(const std::string &line, std::vector<std::string> *columns) {
   std::istringstream ss(line);
   std::string column;
   while (std::getline(ss, column, ',')) {
-    columns->push_back(column);
+    while (1) {
+      auto res = std::find(column.begin(), column.end(), ' ');
+      if (res == column.end())
+        break;
+      column.erase(res);
+    }
+    if (!column.empty())
+      columns->push_back(column);
   }
 }
 
