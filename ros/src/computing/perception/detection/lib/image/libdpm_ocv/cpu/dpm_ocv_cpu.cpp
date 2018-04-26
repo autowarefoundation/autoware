480fda82bacee254df96b543cecb797994aade83
amc-nu <abrahammonrroy@yahoo.com>
2018-04-26 22:58:19 +0900

    Fixes to dpm_ocv  ndt_localizer and pcl_omp

--- ros/src/computing/perception/detection/lib/image/libdpm_ocv/cpu/dpm_ocv_cpu.cpp
+++ ros/src/computing/perception/detection/lib/image/libdpm_ocv/cpu/dpm_ocv_cpu.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
-#include <dpm_ocv/dpm_ocv.hpp>
+#include <libdpm_ocv/dpm_ocv.hpp>
 
 // Macros in OpenCV private header files
 #define VAL_OF_TRUNCATE 0.2f
