38c8cd606dacacb8f784a297ffda1989e1a33ff6
Kosuke Murakami <cirpue49@yahoo.co.jp>
2018-05-13 14:28:01 +0900

    Rename obj_reproj

--- ros/src/computing/perception/detection/packages/lidar_tracker/nodes/obj_reproj/axial_move.h
+++ ros/src/computing/perception/detection/packages/lidar_tracker/nodes/obj_reproj/axial_move.h
@@ -32,7 +32,7 @@
 #define AXIALMOVE
 
 #include <math.h>
-#include "CalObjLoc.h"
+#include "cal_obj_loc.h"
 #include "structure.h"
 
 class axiMove{
@@ -41,30 +41,30 @@ public:
 
     LOCATION cal(LOCATION loc,double matrix[4][4]){
 /**
-calibration file is assumed that 
-axial z is front of car, 
-axial y is upper way 
+calibration file is assumed that
+axial z is front of car,
+axial y is upper way
 and axial x is left and right.
 
  */
         LOCATION newloc;
         //rotation around X
-        newloc.X = 
-            matrix[0][0]*loc.X + 
-            matrix[0][1]*loc.Y + 
-            matrix[0][2]*loc.Z + 
+        newloc.X =
+            matrix[0][0]*loc.X +
+            matrix[0][1]*loc.Y +
+            matrix[0][2]*loc.Z +
             matrix[0][3];
 
-        newloc.Y = 
-            matrix[1][0]*loc.X + 
-            matrix[1][1]*loc.Y + 
-            matrix[1][2]*loc.Z + 
+        newloc.Y =
+            matrix[1][0]*loc.X +
+            matrix[1][1]*loc.Y +
+            matrix[1][2]*loc.Z +
             matrix[1][3];
 
-        newloc.Z = 
-            matrix[2][0]*loc.X + 
-            matrix[2][1]*loc.Y + 
-            matrix[2][2]*loc.Z + 
+        newloc.Z =
+            matrix[2][0]*loc.X +
+            matrix[2][1]*loc.Y +
+            matrix[2][2]*loc.Z +
             matrix[2][3];
 
         newloc.W = 1;
@@ -82,7 +82,7 @@ and axial x is left and right.
 
         //rotation around Y
         newloc.X = loc.X*cos(ang.thiY) - loc.Z*sin(ang.thiY);
-        newloc.Z = loc.X*sin(ang.thiY) + loc.Z*cos(ang.thiY); 
+        newloc.Z = loc.X*sin(ang.thiY) + loc.Z*cos(ang.thiY);
         loc.X = newloc.X;
         loc.Z = newloc.Z;
 
