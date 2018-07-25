e65834e3f6d70ae861015bda751696b9a2304124
Esteve Fernandez <esteve@apache.org>
2018-07-25 03:35:38 +0000

    [Fix] Moved C++11 flag to autoware_build_flags (#1395)

--- ros/src/common/cmake/autoware_build_flags/cmake/autoware_build_flags-extras.cmake
+++ ros/src/common/cmake/autoware_build_flags/cmake/autoware_build_flags-extras.cmake
@@ -3,3 +3,10 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-undefined,error")
 endif()
+
+# Enable support for C++11
+if(${CMAKE_VERSION} VERSION_LESS "3.1.0")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+else()
+  set(CMAKE_CXX_STANDARD 11)
+endif()
