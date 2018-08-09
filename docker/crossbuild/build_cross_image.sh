3d697b81c7ca2e9bf2090c21e62f645b444c5a9e
Esteve Fernandez <esteve@apache.org>
2018-08-09 01:07:24 +0000

    Split target and platform. Rewrite build script so that more platforms can be added eventually. (#1429)

--- docker/crossbuild/build_cross_image.sh
+++ docker/crossbuild/build_cross_image.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Build Docker Image
-if [ "$1" = "aarch64" ]
+if [ "$1" = "synquacer" ] || [ "$1" = "generic-aarch64" ]
 then
     # Once we support for targets, change this to the appropriate Docker image
-    DOCKER_ARCH=arm64v8
+    AUTOWARE_DOCKER_ARCH=arm64v8
+    AUTOWARE_TARGET_ARCH=aarch64
+    AUTOWARE_TARGET_PLATFORM=$1
 
-    echo "Using $1 as the target architecture"
+    echo "Using ${AUTOWARE_TARGET_PLATFORM} as the target architecture"
     # Register QEMU as a handler for non-x86 targets
     docker run --rm --privileged multiarch/qemu-user-static:register
 
     # Build Docker Image
-    docker build --build-arg DOCKER_ARCH=${DOCKER_ARCH} --build-arg SYSROOT_ARCH=$1 -t autoware/crossbuild:$1-kinetic-1.0.0 -f Dockerfile.kinetic-crossbuild .
+    docker build \
+        --build-arg AUTOWARE_DOCKER_ARCH=${AUTOWARE_DOCKER_ARCH} \
+        --build-arg AUTOWARE_TARGET_ARCH=${AUTOWARE_TARGET_ARCH} \
+        --build-arg AUTOWARE_TARGET_PLATFORM=${AUTOWARE_TARGET_PLATFORM} \
+        -t autoware/crossbuild:${AUTOWARE_TARGET_PLATFORM}-kinetic-1.0.0 \
+        -f Dockerfile.kinetic-crossbuild .
 
     # Deregister QEMU as a handler for non-x86 targets
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
 else
-    echo "Select target, aarch64"
+    echo "Select target platform: synquacer, generic-aarch64"
 fi
