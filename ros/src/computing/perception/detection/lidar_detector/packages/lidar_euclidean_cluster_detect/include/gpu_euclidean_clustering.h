e3f2a820188c42649295ceffec8b4ebc176385be
Kosuke Murakami <cirpue49@yahoo.co.jp>
2018-08-09 14:56:25 +0900

    applying ros clang formant

--- ros/src/computing/perception/detection/lidar_detector/packages/lidar_euclidean_cluster_detect/include/gpu_euclidean_clustering.h
+++ ros/src/computing/perception/detection/lidar_detector/packages/lidar_euclidean_cluster_detect/include/gpu_euclidean_clustering.h
@@ -4,47 +4,49 @@
 #include <iostream>
 #include <vector>
 
-class GpuEuclideanCluster {
+class GpuEuclideanCluster
+{
 public:
-	typedef struct {
-		int index_value;
-		std::vector<int> points_in_cluster;
-	} GClusterIndex;
+  typedef struct
+  {
+    int index_value;
+    std::vector<int> points_in_cluster;
+  } GClusterIndex;
 
-	typedef struct {
-		float *x;
-		float *y;
-		float *z;
-		int size;
-	} SamplePointListXYZ;
+  typedef struct
+  {
+    float* x;
+    float* y;
+    float* z;
+    int size;
+  } SamplePointListXYZ;
 
-	GpuEuclideanCluster();
+  GpuEuclideanCluster();
 
-	void setInputPoints(float *x, float *y, float *z, int size);
-	void setThreshold(double threshold);
-	void setMinClusterPts(int min_cluster_pts);
-	void setMaxClusterPts(int max_cluster_pts);
-	void extractClustersOld();
-	void extractClusters();
-	void extractClusters2();
-	std::vector<GClusterIndex> getOutput();
+  void setInputPoints(float* x, float* y, float* z, int size);
+  void setThreshold(double threshold);
+  void setMinClusterPts(int min_cluster_pts);
+  void setMaxClusterPts(int max_cluster_pts);
+  void extractClustersOld();
+  void extractClusters();
+  void extractClusters2();
+  std::vector<GClusterIndex> getOutput();
 
-	SamplePointListXYZ generateSample();
+  SamplePointListXYZ generateSample();
 
-	~GpuEuclideanCluster();
+  ~GpuEuclideanCluster();
 
 private:
-	float *x_, *y_, *z_;
-	int size_;
-	double threshold_;
-	int *cluster_indices_;
-	int *cluster_indices_host_;
-	int min_cluster_pts_;
-	int max_cluster_pts_;
-	int cluster_num_;
+  float *x_, *y_, *z_;
+  int size_;
+  double threshold_;
+  int* cluster_indices_;
+  int* cluster_indices_host_;
+  int min_cluster_pts_;
+  int max_cluster_pts_;
+  int cluster_num_;
 
-
-	void exclusiveScan(int *input, int ele_num, int *sum);
+  void exclusiveScan(int* input, int ele_num, int* sum);
 };
 
 #endif
