2359880ebd273dfa8acb6efa7daaf6523df0a3b5
Masaya Kataoka <ms.kataoka@gmail.com>
2018-09-26 13:56:23 +0900

    remove control command monitor

--- ros/src/util/packages/autoware_rviz_plugins/src/vehicle_monitor/vehicle_status_monitor.cpp
+++ ros/src/util/packages/autoware_rviz_plugins/src/vehicle_monitor/vehicle_status_monitor.cpp
@@ -2,7 +2,18 @@
 
 namespace autoware_rviz_plugins{
     VehicleStatusMonitor::VehicleStatusMonitor() : rviz::Display(){
-
+        width_ = 320;
+        height_ = 320;
+        left_property_ = boost::make_shared<rviz::IntProperty>("Left position", 0, "Left position of the monitor.",this, SLOT(update_left_()));
+        top_property_ = boost::make_shared<rviz::IntProperty>("Top position", 0, "Top position of the monitor.",this, SLOT(update_top_()));
+        alpha_property_ = boost::make_shared<rviz::FloatProperty>("Alpha", 0, "alpha of the monitor.",this, SLOT(update_alpha_()));
+        speed_unit_property_  = boost::make_shared<rviz::EnumProperty>("Speed Unit", "km/h" , "Unit of the speed",this, SLOT(update_speed_unit_()));
+        speed_unit_property_->addOption("km/h", KM_PER_HOUR);
+        speed_unit_property_->addOption("m/s", M_PER_SEC);
+        angle_unit_property_ = boost::make_shared<rviz::EnumProperty>("Angle Unit", "rad" , "Unit of the angle",this, SLOT(update_angle_unit_()));
+        angle_unit_property_->addOption("rad", RAD);
+        angle_unit_property_->addOption("deg", DEG);
+        topic_property_ = boost::make_shared<rviz::RosTopicProperty>("Topic", "",ros::message_traits::datatype<autoware_msgs::VehicleStatus>(),"autoware_msgs::VehicleStatus topic to subscribe to.",this, SLOT(update_topic_()));
     }
 
     VehicleStatusMonitor::~VehicleStatusMonitor(){
@@ -10,6 +21,11 @@ namespace autoware_rviz_plugins{
     }
 
     void VehicleStatusMonitor::onInitialize(){
+        update_top_();
+        update_left_();
+        update_alpha_();
+        update_speed_unit_();
+        update_topic_();
         return;
     }
 
@@ -17,7 +33,106 @@ namespace autoware_rviz_plugins{
         return;
     }
 
+    void VehicleStatusMonitor::update(float wall_dt, float ros_dt){
+        draw_monitor_();
+        return;
+    }
+
+    void VehicleStatusMonitor::update_speed_unit_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        speed_unit_ = speed_unit_property_->getOptionInt();
+        return;
+    }
+
+    void VehicleStatusMonitor::update_angle_unit_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        angle_unit_ = angle_unit_property_->getOptionInt();
+        return;
+    }
+
+    void VehicleStatusMonitor::draw_monitor_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        if(!last_command_data_)
+        {
+            return;
+        }
+        /*
+        Functions to draw monitor
+        */
+        if(!overlay_)
+        {
+            static int count = 0;
+            rviz::UniformStringStream ss;
+            ss << "VehicleStatusMonitorObject" << count++;
+            overlay_.reset(new OverlayObject(ss.str()));
+            overlay_->show();
+        }
+        if(overlay_)
+        {
+            overlay_->setDimensions(width_,height_);
+            overlay_->setPosition(monitor_left_,monitor_top_);
+        }
+        overlay_->updateTextureSize(width_,height_);
+        ScopedPixelBuffer buffer = overlay_->getBuffer();
+        QImage Hud = buffer.getQImage(*overlay_);
+        for (unsigned int i = 0; i < overlay_->getTextureWidth(); i++) {
+            for (unsigned int j = 0; j < overlay_->getTextureHeight(); j++) {
+                Hud.setPixel(i, j, QColor(0,0,0,(int)(255*alpha_)).rgba());
+            }
+        } 
+        QPainter painter(&Hud);
+        return;
+    }
+
     void VehicleStatusMonitor::processMessage(const autoware_msgs::VehicleStatus::ConstPtr& msg){
+        boost::mutex::scoped_lock lock(mutex_);
+        last_command_data_ = *msg;
+        return;
+    }
+
+    void VehicleStatusMonitor::update_topic_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        sub_.shutdown();
+        last_command_data_ = boost::none;
+        topic_name_ = topic_property_->getTopicStd();
+        if (topic_name_.length() > 0 && topic_name_ != "/")
+        {
+            sub_ = nh_.subscribe(topic_name_, 1, &VehicleStatusMonitor::processMessage, this);
+        }
+        return;
+    }
+
+    void  VehicleStatusMonitor::update_top_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        monitor_top_ = top_property_->getInt();
+        return;
+    }
+
+    void  VehicleStatusMonitor::update_left_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        monitor_left_ = left_property_->getInt();
+        return;
+    }
+
+    void  VehicleStatusMonitor::update_alpha_(){
+        boost::mutex::scoped_lock lock(mutex_);
+        alpha_ = alpha_property_->getFloat();
+        return;
+    }
+
+    void VehicleStatusMonitor::onEnable(){
+        if (overlay_) {
+            overlay_->show();
+        }
+        //subscribe();
+        return;
+    }
+
+    void VehicleStatusMonitor::onDisable(){
+        if (overlay_) {
+            overlay_->hide();
+        }
+        //unsubscribe();
         return;
     }
 }
--- ros/src/util/packages/autoware_rviz_plugins/src/vehicle_monitor/vehicle_status_monitor.h
+++ ros/src/util/packages/autoware_rviz_plugins/src/vehicle_monitor/vehicle_status_monitor.h
@@ -3,16 +3,17 @@
 
 //headers in autoware
 #include <autoware_msgs/VehicleStatus.h>
+#include "overlay_utils.h"
 
 // headers in ROS
 #include <ros/package.h>
 #include <ros/ros.h>
 
-// headers for opencv
-#include <opencv2/core/core.hpp>
-
 // headers in Qt
 #include <QWidget>
+#include <QPainter>
+#include <QImage>
+#include <QRect>
 
 // headers in rviz
 #include <rviz/message_filter_display.h>
@@ -23,6 +24,20 @@
 #include <rviz/properties/status_property.h>
 #include <rviz/uniform_string_stream.h>
 
+//headers in boost
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <boost/optional.hpp>
+
+//headers in STL
+#include <math.h>
+
+#define KM_PER_HOUR 0
+#define M_PER_SEC 1
+
+#define RAD 0
+#define DEG 1
+
 namespace autoware_rviz_plugins {
     class VehicleStatusMonitor : public rviz::Display{
     Q_OBJECT
@@ -32,8 +47,35 @@ namespace autoware_rviz_plugins {
     protected:
         virtual void onInitialize();
         virtual void reset();
+        virtual void update(float wall_dt, float ros_dt);
+        virtual void onEnable();
+        virtual void onDisable();
     private:
+        autoware_rviz_plugins::OverlayObject::Ptr overlay_;
         void processMessage(const autoware_msgs::VehicleStatus::ConstPtr& msg);
+        void draw_monitor_();
+        boost::shared_ptr<rviz::RosTopicProperty> topic_property_;
+        boost::shared_ptr<rviz::IntProperty> top_property_;
+        boost::shared_ptr<rviz::IntProperty> left_property_;
+        boost::shared_ptr<rviz::FloatProperty> alpha_property_;
+        boost::shared_ptr<rviz::EnumProperty> speed_unit_property_;
+        boost::shared_ptr<rviz::EnumProperty> angle_unit_property_;
+        boost::optional<autoware_msgs::VehicleStatus> last_command_data_;
+        ros::Subscriber sub_;
+        ros::NodeHandle nh_;
+        boost::mutex mutex_;
+        int monitor_top_,monitor_left_;
+        float alpha_;
+        double width_,height_;
+        int speed_unit_,angle_unit_;
+        std::string topic_name_;
+    protected Q_SLOTS:
+        void update_topic_();
+        void update_top_();
+        void update_left_();
+        void update_alpha_();
+        void update_speed_unit_();
+        void update_angle_unit_();
     };
 }
 #endif //VHICLE_STATE_MONITOR_H_INCLUDED
