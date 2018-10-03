a5d77b1d71ae62639f1bc3250e962df046b16b4c
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-03 17:30:42 +0900

    add roslib depends

--- ros/src/simulation/lgsvl_simulator_bridge/include/lgsvl_simulator_bridge/lgsvl_simulator_launcher.h
+++ ros/src/simulation/lgsvl_simulator_bridge/include/lgsvl_simulator_bridge/lgsvl_simulator_launcher.h
@@ -1,7 +1,11 @@
 #ifndef LGSVL_SIMULATOR_LAUNCHER_H_INCLUDED
 #define LGSVL_SIMULATOR_LAUNCHER_H_INCLUDED
 
+//headers in ROS
 #include <ros/package.h>
+
+//headers in STL
+#include <stdlib.h>
 
 class lgsvl_simulator_launcher{
 public:
--- ros/src/simulation/lgsvl_simulator_bridge/src/lgsvl_simulator_launcher.cpp
+++ ros/src/simulation/lgsvl_simulator_bridge/src/lgsvl_simulator_launcher.cpp
@@ -1,8 +1,8 @@
 #include <lgsvl_simulator_bridge/lgsvl_simulator_launcher.h>
 
 lgsvl_simulator_launcher::lgsvl_simulator_launcher(){
-    //std::string package_path = ros::package::getPath("lgsvl_simulator_bridge");
-    //exec_path_ = package_path + "/bin/auto-simulator-linux64-474/simulator";
+    std::string package_path = ros::package::getPath("lgsvl_simulator_bridge");
+    exec_path_ = package_path + "/bin/auto-simulator-linux64-474/simulator";
 }
 
 lgsvl_simulator_launcher::~lgsvl_simulator_launcher(){
