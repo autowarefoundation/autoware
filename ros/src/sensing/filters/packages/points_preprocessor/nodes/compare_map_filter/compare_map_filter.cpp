84607b859c38dbeb49b124b5164b7f188778f957
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-24 16:11:38 +0900

    Enable to use tf2 on points_preprocessor
    enables to use tf2 in concat filter
    
    apply clang_format
    
    enables to use tf2 in cloud_transformer node
    
    apply clang-format to the cloud_transformer node
    
    remove unused headers
    
    enabales to use tf2 in compare_map filter
    
    remove tf depends
    
    remove tf2::doTransform in compare_map filter
    
    fix map cloud ptr tf
    
    add toROSMsg function
    
    fix transform function
    
    fix transform function
    
    remove tf depends
    
    apply clang-format

--- ros/src/sensing/filters/packages/points_preprocessor/nodes/compare_map_filter/compare_map_filter.cpp
+++ ros/src/sensing/filters/packages/points_preprocessor/nodes/compare_map_filter/compare_map_filter.cpp
@@ -5,7 +5,8 @@
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this
+ *  * Redistributions of source code must retain the above copyright notice,
+ * this
  *    list of conditions and the following disclaimer.
  *
  *  * Redistributions in binary form must reproduce the above copyright notice,
@@ -18,32 +19,35 @@
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
  *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <ros/ros.h>
 
-#include <tf/tf.h>
-
-#include <sensor_msgs/point_cloud_conversion.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
 
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl_ros/transforms.h>
-#include <pcl/search/kdtree.h>
 #include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/search/kdtree.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 #include <autoware_config_msgs/ConfigCompareMapFilter.h>
 
-class CompareMapFilter
-{
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+class CompareMapFilter {
 public:
   CompareMapFilter();
 
@@ -57,7 +61,8 @@ private:
   ros::Publisher match_points_pub_;
   ros::Publisher unmatch_points_pub_;
 
-  tf::TransformListener* tf_listener_;
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 
   pcl::KdTreeFLANN<pcl::PointXYZI> tree_;
 
@@ -67,85 +72,104 @@ private:
 
   std::string map_frame_;
 
-  void configCallback(const autoware_config_msgs::ConfigCompareMapFilter::ConstPtr& config_msg_ptr);
-  void pointsMapCallback(const sensor_msgs::PointCloud2::ConstPtr& map_cloud_msg_ptr);
-  void sensorPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& sensorTF_cloud_msg_ptr);
-  void searchMatchingCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr,
-                           pcl::PointCloud<pcl::PointXYZI>::Ptr match_cloud_ptr,
-                           pcl::PointCloud<pcl::PointXYZI>::Ptr unmatch_cloud_ptr);
+  void
+  configCallback(const autoware_config_msgs::ConfigCompareMapFilter::ConstPtr
+                     &config_msg_ptr);
+  void pointsMapCallback(
+      const sensor_msgs::PointCloud2::ConstPtr &map_cloud_msg_ptr);
+  void sensorPointsCallback(
+      const sensor_msgs::PointCloud2::ConstPtr &sensorTF_cloud_msg_ptr);
+  void
+  searchMatchingCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr,
+                      pcl::PointCloud<pcl::PointXYZI>::Ptr match_cloud_ptr,
+                      pcl::PointCloud<pcl::PointXYZI>::Ptr unmatch_cloud_ptr);
 };
 
 CompareMapFilter::CompareMapFilter()
-  : nh_()
-  , nh_private_("~")
-  , tf_listener_(new tf::TransformListener)
-  , distance_threshold_(0.2)
-  , min_clipping_height_(-2.0)
-  , max_clipping_height_(0.5)
-  , map_frame_("/map")
-{
-  nh_private_.param("distance_threshold", distance_threshold_, distance_threshold_);
-  nh_private_.param("min_clipping_height", min_clipping_height_, min_clipping_height_);
-  nh_private_.param("max_clipping_height", max_clipping_height_, max_clipping_height_);
+    : nh_(), nh_private_("~"), tf_listener_(tf_buffer_),
+      distance_threshold_(0.2), min_clipping_height_(-2.0),
+      max_clipping_height_(0.5), map_frame_("map") {
+  nh_private_.param("distance_threshold", distance_threshold_,
+                    distance_threshold_);
+  nh_private_.param("min_clipping_height", min_clipping_height_,
+                    min_clipping_height_);
+  nh_private_.param("max_clipping_height", max_clipping_height_,
+                    max_clipping_height_);
 
-  config_sub_ = nh_.subscribe("/config/compare_map_filter", 10, &CompareMapFilter::configCallback, this);
-  sensor_points_sub_ = nh_.subscribe("/points_raw", 1, &CompareMapFilter::sensorPointsCallback, this);
-  map_sub_ = nh_.subscribe("/points_map", 10, &CompareMapFilter::pointsMapCallback, this);
-  match_points_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/points_ground", 10);
-  unmatch_points_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/points_no_ground", 10);
+  config_sub_ = nh_.subscribe("/config/compare_map_filter", 10,
+                              &CompareMapFilter::configCallback, this);
+  sensor_points_sub_ = nh_.subscribe(
+      "/points_raw", 1, &CompareMapFilter::sensorPointsCallback, this);
+  map_sub_ = nh_.subscribe("/points_map", 10,
+                           &CompareMapFilter::pointsMapCallback, this);
+  match_points_pub_ =
+      nh_.advertise<sensor_msgs::PointCloud2>("/points_ground", 10);
+  unmatch_points_pub_ =
+      nh_.advertise<sensor_msgs::PointCloud2>("/points_no_ground", 10);
 }
 
-void CompareMapFilter::configCallback(const autoware_config_msgs::ConfigCompareMapFilter::ConstPtr& config_msg_ptr)
-{
+void CompareMapFilter::configCallback(
+    const autoware_config_msgs::ConfigCompareMapFilter::ConstPtr
+        &config_msg_ptr) {
   distance_threshold_ = config_msg_ptr->distance_threshold;
   min_clipping_height_ = config_msg_ptr->min_clipping_height;
   max_clipping_height_ = config_msg_ptr->max_clipping_height;
 }
 
-void CompareMapFilter::pointsMapCallback(const sensor_msgs::PointCloud2::ConstPtr& map_cloud_msg_ptr)
-{
-  pcl::PointCloud<pcl::PointXYZI>::Ptr map_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+void CompareMapFilter::pointsMapCallback(
+    const sensor_msgs::PointCloud2::ConstPtr &map_cloud_msg_ptr) {
+  pcl::PointCloud<pcl::PointXYZI>::Ptr map_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
   pcl::fromROSMsg(*map_cloud_msg_ptr, *map_cloud_ptr);
   tree_.setInputCloud(map_cloud_ptr);
 
   map_frame_ = map_cloud_msg_ptr->header.frame_id;
 }
 
-void CompareMapFilter::sensorPointsCallback(const sensor_msgs::PointCloud2::ConstPtr& sensorTF_cloud_msg_ptr)
-{
+void CompareMapFilter::sensorPointsCallback(
+    const sensor_msgs::PointCloud2::ConstPtr &sensorTF_cloud_msg_ptr) {
   const ros::Time sensor_time = sensorTF_cloud_msg_ptr->header.stamp;
   const std::string sensor_frame = sensorTF_cloud_msg_ptr->header.frame_id;
 
-  pcl::PointCloud<pcl::PointXYZI>::Ptr sensorTF_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr sensorTF_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
   pcl::fromROSMsg(*sensorTF_cloud_msg_ptr, *sensorTF_cloud_ptr);
 
-  pcl::PointCloud<pcl::PointXYZI>::Ptr sensorTF_clipping_height_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr sensorTF_clipping_height_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
   sensorTF_clipping_height_cloud_ptr->header = sensorTF_cloud_ptr->header;
-  for (size_t i = 0; i < sensorTF_cloud_ptr->points.size(); ++i)
-  {
+  for (size_t i = 0; i < sensorTF_cloud_ptr->points.size(); ++i) {
     if (sensorTF_cloud_ptr->points[i].z > min_clipping_height_ &&
-        sensorTF_cloud_ptr->points[i].z < max_clipping_height_)
-    {
-      sensorTF_clipping_height_cloud_ptr->points.push_back(sensorTF_cloud_ptr->points[i]);
+        sensorTF_cloud_ptr->points[i].z < max_clipping_height_) {
+      sensorTF_clipping_height_cloud_ptr->points.push_back(
+          sensorTF_cloud_ptr->points[i]);
     }
   }
 
-  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
-  try
-  {
-    tf_listener_->waitForTransform(map_frame_, sensor_frame, sensor_time, ros::Duration(3.0));
-    pcl_ros::transformPointCloud(map_frame_, sensor_time, *sensorTF_clipping_height_cloud_ptr, sensor_frame,
-                                 *mapTF_cloud_ptr, *tf_listener_);
-  }
-  catch (tf::TransformException& ex)
-  {
-    ROS_ERROR("Transform error: %s", ex.what());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
+  sensor_msgs::PointCloud2 sensorTF_clipping_height_cloud_msg, mapTF_cloud_msg;
+
+  geometry_msgs::TransformStamped transform_stamped;
+  try {
+    transform_stamped = tf_buffer_.lookupTransform(
+        map_frame_, sensor_frame, sensor_time, ros::Duration(1.0));
+  } catch (tf2::TransformException &ex) {
+    ROS_WARN("%s", ex.what());
     return;
   }
+  pcl::toROSMsg(*sensorTF_clipping_height_cloud_ptr,
+                sensorTF_clipping_height_cloud_msg);
+  tf2::doTransform(sensorTF_clipping_height_cloud_msg, mapTF_cloud_msg,
+                   transform_stamped);
+  pcl::fromROSMsg(mapTF_cloud_msg, *mapTF_cloud_ptr);
 
-  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_match_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
-  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_unmatch_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
-  searchMatchingCloud(mapTF_cloud_ptr, mapTF_match_cloud_ptr, mapTF_unmatch_cloud_ptr);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_match_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr mapTF_unmatch_cloud_ptr(
+      new pcl::PointCloud<pcl::PointXYZI>);
+  searchMatchingCloud(mapTF_cloud_ptr, mapTF_match_cloud_ptr,
+                      mapTF_unmatch_cloud_ptr);
 
   sensor_msgs::PointCloud2 mapTF_match_cloud_msg;
   pcl::toROSMsg(*mapTF_match_cloud_ptr, mapTF_match_cloud_msg);
@@ -154,15 +178,15 @@ void CompareMapFilter::sensorPointsCallback(const sensor_msgs::PointCloud2::Cons
   mapTF_match_cloud_msg.fields = sensorTF_cloud_msg_ptr->fields;
 
   sensor_msgs::PointCloud2 sensorTF_match_cloud_msg;
-  try
-  {
-    pcl_ros::transformPointCloud(sensor_frame, mapTF_match_cloud_msg, sensorTF_match_cloud_msg, *tf_listener_);
-  }
-  catch (tf::TransformException& ex)
-  {
-    ROS_ERROR("Transform error: %s", ex.what());
+  try {
+    transform_stamped = tf_buffer_.lookupTransform(
+        sensor_frame, map_frame_, ros::Time(0), ros::Duration(1.0));
+  } catch (tf2::TransformException &ex) {
+    ROS_WARN("%s", ex.what());
     return;
   }
+  tf2::doTransform(mapTF_match_cloud_msg, sensorTF_match_cloud_msg,
+                   transform_stamped);
   match_points_pub_.publish(sensorTF_match_cloud_msg);
 
   sensor_msgs::PointCloud2 mapTF_unmatch_cloud_msg;
@@ -172,22 +196,17 @@ void CompareMapFilter::sensorPointsCallback(const sensor_msgs::PointCloud2::Cons
   mapTF_unmatch_cloud_msg.fields = sensorTF_cloud_msg_ptr->fields;
 
   sensor_msgs::PointCloud2 sensorTF_unmatch_cloud_msg;
-  try
-  {
-    pcl_ros::transformPointCloud(sensor_frame, mapTF_unmatch_cloud_msg, sensorTF_unmatch_cloud_msg, *tf_listener_);
-  }
-  catch (tf::TransformException& ex)
-  {
-    ROS_ERROR("Transform error: %s", ex.what());
-    return;
-  }
+  tf2::doTransform(mapTF_unmatch_cloud_msg, sensorTF_unmatch_cloud_msg,
+                   transform_stamped);
   unmatch_points_pub_.publish(sensorTF_unmatch_cloud_msg);
 }
 
-void CompareMapFilter::searchMatchingCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr,
-                                           pcl::PointCloud<pcl::PointXYZI>::Ptr match_cloud_ptr,
-                                           pcl::PointCloud<pcl::PointXYZI>::Ptr unmatch_cloud_ptr)
-{
+void CompareMapFilter::searchMatchingCloud(
+    const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr,
+    pcl::PointCloud<pcl::PointXYZI>::Ptr match_cloud_ptr,
+    pcl::PointCloud<pcl::PointXYZI>::Ptr unmatch_cloud_ptr) {
+  unmatch_cloud_ptr->header = in_cloud_ptr->header;
+  match_cloud_ptr->header = in_cloud_ptr->header;
   match_cloud_ptr->points.clear();
   unmatch_cloud_ptr->points.clear();
 
@@ -196,22 +215,17 @@ void CompareMapFilter::searchMatchingCloud(const pcl::PointCloud<pcl::PointXYZI>
 
   std::vector<int> nn_indices(1);
   std::vector<float> nn_dists(1);
-  for (size_t i = 0; i < in_cloud_ptr->points.size(); ++i)
-  {
+  for (size_t i = 0; i < in_cloud_ptr->points.size(); ++i) {
     tree_.nearestKSearch(in_cloud_ptr->points[i], 1, nn_indices, nn_dists);
-    if (nn_dists[0] <= distance_threshold_)
-    {
+    if (nn_dists[0] <= distance_threshold_) {
       match_cloud_ptr->points.push_back(in_cloud_ptr->points[i]);
-    }
-    else
-    {
+    } else {
       unmatch_cloud_ptr->points.push_back(in_cloud_ptr->points[i]);
     }
   }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "compare_map_filter");
   CompareMapFilter node;
   ros::spin();
--- ros/src/sensing/filters/packages/points_preprocessor/nodes/points_concat_filter/points_concat_filter.cpp
+++ ros/src/sensing/filters/packages/points_preprocessor/nodes/points_concat_filter/points_concat_filter.cpp
@@ -18,32 +18,35 @@
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
  *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
  *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <geometry_msgs/TransformStamped.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl_ros/transforms.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <tf/tf.h>
-#include <tf/transform_listener.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #include <velodyne_pointcloud/point_types.h>
 #include <yaml-cpp/yaml.h>
 
-class PointsConcatFilter
-{
+class PointsConcatFilter {
 public:
   PointsConcatFilter();
 
@@ -51,9 +54,9 @@ private:
   typedef pcl::PointXYZI PointT;
   typedef pcl::PointCloud<PointT> PointCloudT;
   typedef sensor_msgs::PointCloud2 PointCloudMsgT;
-  typedef message_filters::sync_policies::ApproximateTime<PointCloudMsgT, PointCloudMsgT, PointCloudMsgT,
-                                                          PointCloudMsgT, PointCloudMsgT, PointCloudMsgT,
-                                                          PointCloudMsgT, PointCloudMsgT>
+  typedef message_filters::sync_policies::ApproximateTime<
+      PointCloudMsgT, PointCloudMsgT, PointCloudMsgT, PointCloudMsgT,
+      PointCloudMsgT, PointCloudMsgT, PointCloudMsgT, PointCloudMsgT>
       SyncPolicyT;
 
   ros::NodeHandle node_handle_, private_node_handle_;
@@ -61,84 +64,91 @@ private:
   message_filters::Synchronizer<SyncPolicyT> *cloud_synchronizer_;
   ros::Subscriber config_subscriber_;
   ros::Publisher cloud_publisher_;
-  tf::TransformListener tf_listener_;
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 
   size_t input_topics_size_;
   std::string input_topics_;
   std::string output_frame_id_;
 
-  void pointcloud_callback(const PointCloudMsgT::ConstPtr &msg1, const PointCloudMsgT::ConstPtr &msg2,
-                           const PointCloudMsgT::ConstPtr &msg3, const PointCloudMsgT::ConstPtr &msg4,
-                           const PointCloudMsgT::ConstPtr &msg5, const PointCloudMsgT::ConstPtr &msg6,
-                           const PointCloudMsgT::ConstPtr &msg7, const PointCloudMsgT::ConstPtr &msg8);
+  void pointcloud_callback(const PointCloudMsgT::ConstPtr &msg1,
+                           const PointCloudMsgT::ConstPtr &msg2,
+                           const PointCloudMsgT::ConstPtr &msg3,
+                           const PointCloudMsgT::ConstPtr &msg4,
+                           const PointCloudMsgT::ConstPtr &msg5,
+                           const PointCloudMsgT::ConstPtr &msg6,
+                           const PointCloudMsgT::ConstPtr &msg7,
+                           const PointCloudMsgT::ConstPtr &msg8);
 };
 
-PointsConcatFilter::PointsConcatFilter() : node_handle_(), private_node_handle_("~"), tf_listener_()
-{
-  private_node_handle_.param("input_topics", input_topics_, std::string("[/points_alpha, /points_beta]"));
-  private_node_handle_.param("output_frame_id", output_frame_id_, std::string("velodyne"));
+PointsConcatFilter::PointsConcatFilter()
+    : node_handle_(), private_node_handle_("~"), tf_listener_(tf_buffer_) {
+  private_node_handle_.param("input_topics", input_topics_,
+                             std::string("[/points_alpha, /points_beta]"));
+  private_node_handle_.param("output_frame_id", output_frame_id_,
+                             std::string("velodyne"));
 
   YAML::Node topics = YAML::Load(input_topics_);
   input_topics_size_ = topics.size();
-  if (input_topics_size_ < 2 || 8 < input_topics_size_)
-  {
+  if (input_topics_size_ < 2 || 8 < input_topics_size_) {
     ROS_ERROR("The size of input_topics must be between 2 and 8");
     ros::shutdown();
   }
-  for (size_t i = 0; i < 8; ++i)
-  {
-    if (i < input_topics_size_)
-    {
-      cloud_subscribers_[i] =
-          new message_filters::Subscriber<PointCloudMsgT>(node_handle_, topics[i].as<std::string>(), 1);
-    }
-    else
-    {
-      cloud_subscribers_[i] =
-          new message_filters::Subscriber<PointCloudMsgT>(node_handle_, topics[0].as<std::string>(), 1);
+  for (size_t i = 0; i < 8; ++i) {
+    if (i < input_topics_size_) {
+      cloud_subscribers_[i] = new message_filters::Subscriber<PointCloudMsgT>(
+          node_handle_, topics[i].as<std::string>(), 1);
+    } else {
+      cloud_subscribers_[i] = new message_filters::Subscriber<PointCloudMsgT>(
+          node_handle_, topics[0].as<std::string>(), 1);
     }
   }
   cloud_synchronizer_ = new message_filters::Synchronizer<SyncPolicyT>(
-      SyncPolicyT(10), *cloud_subscribers_[0], *cloud_subscribers_[1], *cloud_subscribers_[2], *cloud_subscribers_[3],
-      *cloud_subscribers_[4], *cloud_subscribers_[5], *cloud_subscribers_[6], *cloud_subscribers_[7]);
+      SyncPolicyT(10), *cloud_subscribers_[0], *cloud_subscribers_[1],
+      *cloud_subscribers_[2], *cloud_subscribers_[3], *cloud_subscribers_[4],
+      *cloud_subscribers_[5], *cloud_subscribers_[6], *cloud_subscribers_[7]);
   cloud_synchronizer_->registerCallback(
-      boost::bind(&PointsConcatFilter::pointcloud_callback, this, _1, _2, _3, _4, _5, _6, _7, _8));
-  cloud_publisher_ = node_handle_.advertise<PointCloudMsgT>("/points_concat", 1);
+      boost::bind(&PointsConcatFilter::pointcloud_callback, this, _1, _2, _3,
+                  _4, _5, _6, _7, _8));
+  cloud_publisher_ =
+      node_handle_.advertise<PointCloudMsgT>("/points_concat", 1);
 }
 
-void PointsConcatFilter::pointcloud_callback(const PointCloudMsgT::ConstPtr &msg1, const PointCloudMsgT::ConstPtr &msg2,
-                                             const PointCloudMsgT::ConstPtr &msg3, const PointCloudMsgT::ConstPtr &msg4,
-                                             const PointCloudMsgT::ConstPtr &msg5, const PointCloudMsgT::ConstPtr &msg6,
-                                             const PointCloudMsgT::ConstPtr &msg7, const PointCloudMsgT::ConstPtr &msg8)
-{
+void PointsConcatFilter::pointcloud_callback(
+    const PointCloudMsgT::ConstPtr &msg1, const PointCloudMsgT::ConstPtr &msg2,
+    const PointCloudMsgT::ConstPtr &msg3, const PointCloudMsgT::ConstPtr &msg4,
+    const PointCloudMsgT::ConstPtr &msg5, const PointCloudMsgT::ConstPtr &msg6,
+    const PointCloudMsgT::ConstPtr &msg7,
+    const PointCloudMsgT::ConstPtr &msg8) {
   assert(2 <= input_topics_size_ && input_topics_size_ <= 8);
 
-  PointCloudMsgT::ConstPtr msgs[8] = { msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8 };
+  PointCloudMsgT::ConstPtr msgs[8] = {msg1, msg2, msg3, msg4,
+                                      msg5, msg6, msg7, msg8};
   PointCloudT::Ptr cloud_sources[8];
   PointCloudT::Ptr cloud_concatenated(new PointCloudT);
 
   // transform points
-  try
-  {
-    for (size_t i = 0; i < input_topics_size_; ++i)
-    {
-      // Note: If you use kinetic, you can directly receive messages as
-      // PointCloutT.
-      cloud_sources[i] = PointCloudT().makeShared();
-      pcl::fromROSMsg(*msgs[i], *cloud_sources[i]);
-      tf_listener_.waitForTransform(output_frame_id_, msgs[i]->header.frame_id, ros::Time(0), ros::Duration(1.0));
-      pcl_ros::transformPointCloud(output_frame_id_, *cloud_sources[i], *cloud_sources[i], tf_listener_);
+  for (size_t i = 0; i < input_topics_size_; ++i) {
+    // Note: If you use kinetic, you can directly receive messages as
+    // PointCloutT.
+    cloud_sources[i] = PointCloudT().makeShared();
+    // pcl::fromROSMsg(*msgs[i], *cloud_sources[i]);
+    geometry_msgs::TransformStamped transform_stamped;
+    try {
+      transform_stamped =
+          tf_buffer_.lookupTransform(output_frame_id_, msgs[i]->header.frame_id,
+                                     ros::Time(0), ros::Duration(1.0));
+    } catch (tf2::TransformException &ex) {
+      ROS_WARN("%s", ex.what());
+      return;
     }
-  }
-  catch (tf::TransformException &ex)
-  {
-    ROS_ERROR("%s", ex.what());
-    return;
+    sensor_msgs::PointCloud2 transformed_cloud;
+    tf2::doTransform(*msgs[i], transformed_cloud, transform_stamped);
+    pcl::fromROSMsg(transformed_cloud, *cloud_sources[i]);
   }
 
   // merge points
-  for (size_t i = 0; i < input_topics_size_; ++i)
-  {
+  for (size_t i = 0; i < input_topics_size_; ++i) {
     *cloud_concatenated += *cloud_sources[i];
   }
 
@@ -148,8 +158,7 @@ void PointsConcatFilter::pointcloud_callback(const PointCloudMsgT::ConstPtr &msg
   cloud_publisher_.publish(cloud_concatenated);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "points_concat_filter");
   PointsConcatFilter node;
   ros::spin();
