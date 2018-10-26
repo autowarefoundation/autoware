ef187c5b564634282a5914e9d511d9f9b3c6da58
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-26 13:53:19 +0900

    remove unused headers

--- ros/src/sensing/filters/packages/points_preprocessor/nodes/cloud_transformer/cloud_transformer_node.cpp
+++ ros/src/sensing/filters/packages/points_preprocessor/nodes/cloud_transformer/cloud_transformer_node.cpp
@@ -37,10 +37,8 @@
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl_ros/transforms.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <tf/transform_listener.h>
 #include <velodyne_pointcloud/point_types.h>
 
 #include <tf2/transform_datatypes.h>
