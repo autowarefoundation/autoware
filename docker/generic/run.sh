7f254c604a34fce48fb095553e199e06cb3cf5dd
Servando German <servando.german.serrano@linaro.org>
2018-11-08 10:14:11 +0000

    Updated run.sh

--- docker/generic/run.sh
+++ docker/generic/run.sh
@@ -36,6 +36,11 @@ while getopts ":ht:r:s:" opt; do
   esac
 done
 
+if [ "${TAG}" -eq "latest-indigo" ] ; then
+  echo "Indigo is deprecated and will be removed in a future release, please use Kinetic instead"
+  sleep 2
+fi
+
 echo "Using $DOCKER_HUB_REPO:$TAG"
 echo "Shared directory: ${HOST_DIR}"
 
