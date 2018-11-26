19826834d10f79b5d90dc9ba1498961198e74c99
Esteve Fernandez <esteve@apache.org>
2018-11-26 18:28:51 +0100

    Drop support for ROS Indigo

--- docker/generic/build.sh
+++ docker/generic/build.sh
@@ -5,10 +5,6 @@ if [ "$1" = "kinetic" ]
 then
     echo "Use $1"
     docker build -t autoware-$1 -f Dockerfile.$1 ./../.. --no-cache
-elif [ "$1" = "indigo" ]
-then
-    echo "Indigo is deprecated and will be removed in a future release, please use Kinetic instead"
-    docker build -t autoware-$1 -f Dockerfile.$1 ./../.. --no-cache
 else
-    echo "Select distribution, kinetic|indigo"
+    echo "Select distribution, kinetic"
 fi
