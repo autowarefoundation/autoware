28b524824eccd98c1262ea561bef26fbdbc2251f
amc-nu <abrahammonrroy@yahoo.com>
2018-12-11 20:12:11 +0900

    * Added 3D Models for different classes in visualization * 2D Rect node visualize_rects added to visualization_package

--- ros/src/computing/perception/detection/visualizers/packages/visualize_detected_objects/src/visualize_detected_objects.cpp
+++ ros/src/computing/perception/detection/visualizers/packages/visualize_detected_objects/src/visualize_detected_objects.cpp
@@ -1,31 +1,20 @@
 /*
- *  Copyright (c) 2018, Nagoya University
- *  All rights reserved.
+ * Copyright 2018 Nagoya University. All rights reserved.
  *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  * Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  * Neither the name of Autoware nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************
+ *  v1.0: amc-nu (abrahammonrroy@yahoo.com)
  */
 
 #include "visualize_detected_objects.h"
@@ -69,6 +58,10 @@ VisualizeDetectedObjects::VisualizeDetectedObjects() : arrow_height_(0.5), label
   private_nh_.param<std::vector<double>>("box_color", color, {51.,128.,204.,0.8});
   box_color_ = ParseColor(color);
   ROS_INFO("[%s] box_color: %s", __APP_NAME__, ColorToString(box_color_).c_str());
+
+  private_nh_.param<std::vector<double>>("model_color", color, {190.,190.,190.,0.5});
+  model_color_ = ParseColor(color);
+  ROS_INFO("[%s] model_color: %s", __APP_NAME__, ColorToString(model_color_).c_str());
 
   private_nh_.param<std::vector<double>>("centroid_color", color, {77.,121.,255.,0.8});
   centroid_color_ = ParseColor(color);
@@ -262,7 +255,7 @@ VisualizeDetectedObjects::ObjectsToModels(const autoware_msgs::DetectedObjectArr
       model.action = visualization_msgs::Marker::ADD;
       model.ns = ros_namespace_ + "/model_markers";
       model.mesh_use_embedded_materials = false;
-      model.color = label_color_;
+      model.color = model_color_;
       if(object.label == "car")
       {
         model.mesh_resource = "package://detected_objects_visualizer/models/car.dae";
@@ -274,6 +267,14 @@ VisualizeDetectedObjects::ObjectsToModels(const autoware_msgs::DetectedObjectArr
       else if (object.label == "bicycle" || object.label == "bike")
       {
         model.mesh_resource = "package://detected_objects_visualizer/models/bike.dae";
+      }
+      else if (object.label == "bus")
+      {
+        model.mesh_resource = "package://detected_objects_visualizer/models/bus.dae";
+      }
+      else if(object.label == "truck")
+      {
+        model.mesh_resource = "package://detected_objects_visualizer/models/truck.dae";
       }
       else
       {
