3e4f21000ac6cc353e6cd4af41d75b2877dfb8d6
Servando German <servando.german.serrano@linaro.org>
2018-12-12 16:33:39 +0000

    Replaced std::cout checks

--- ros/src/common/libs/state_machine_lib/test/src/test_state_context.cpp
+++ ros/src/common/libs/state_machine_lib/test/src/test_state_context.cpp
@@ -23,75 +23,86 @@
 
 #include "state_machine_lib/state_context.hpp"
 
+class TestClass
+{
+public:
+  TestClass(){
+    counter_ = 0;
+  }
+
+  int counter_;
+  void increaseCounter(const std::string&)
+  {
+    counter_++;
+  }
+};
+
 class TestSuite: public ::testing::Test {
 public:
-	TestSuite(){}
-	~TestSuite(){}
+  TestSuite(){}
+  ~TestSuite(){}
+
+  TestClass test_obj_;
+
 };
 
 TEST_F(TestSuite, StateContextConstructor){
 
-	std::string file_name = "testStates.yaml";
-	std::string msg_name = "test_states";
-	state_machine::StateContext state_context(file_name, msg_name);
+  std::string file_name = "testStates.yaml";
+  std::string msg_name = "test_states";
+  state_machine::StateContext state_context(file_name, msg_name);
 
-	// Check generated dot file
-	std::ifstream generated_dot_file("/tmp/"+msg_name+".dot");
-	std::stringstream dot_file_string;
-	dot_file_string << generated_dot_file.rdbuf();
+  // Check generated dot file
+  std::ifstream generated_dot_file("/tmp/"+msg_name+".dot");
+  std::stringstream dot_file_string;
+  dot_file_string << generated_dot_file.rdbuf();
 
-	std::ifstream reference_dot_file("reference.dot");
-	std::stringstream reference_dot_file_string;
-	reference_dot_file_string << reference_dot_file.rdbuf();
+  std::ifstream reference_dot_file("reference.dot");
+  std::stringstream reference_dot_file_string;
+  reference_dot_file_string << reference_dot_file.rdbuf();
 
-	ASSERT_STREQ(dot_file_string.str().c_str(), reference_dot_file_string.str().c_str()) << "The generated dot file should be " << reference_dot_file_string.str();
+  ASSERT_STREQ(dot_file_string.str().c_str(), reference_dot_file_string.str().c_str()) << "The generated dot file should be " << reference_dot_file_string.str();
 
-	// Check start state
-	std::shared_ptr<state_machine::State> start_state;
-	start_state = state_context.getStartState();
-	std::string test_string;
-	test_string = "Start\n";
-	ASSERT_STREQ(start_state->getStateName().c_str(), "Start") << "First state should be Start";
-	ASSERT_STREQ(state_context.getStateText().c_str(), test_string.c_str()) << "Text should be " << test_string;
-	ASSERT_STREQ(state_context.getAvailableTransition().c_str(), "started:Init,") << "Available transition should be: started:Init,";
+  // Check start state
+  std::shared_ptr<state_machine::State> start_state;
+  start_state = state_context.getStartState();
+  std::string test_string;
+  test_string = "Start\n";
+  ASSERT_STREQ(start_state->getStateName().c_str(), "Start") << "First state should be Start";
+  ASSERT_STREQ(state_context.getStateText().c_str(), test_string.c_str()) << "Text should be " << test_string;
+  ASSERT_STREQ(state_context.getAvailableTransition().c_str(), "started:Init,") << "Available transition should be: started:Init,";
 }
 
 TEST_F(TestSuite, ChangeStates){
 
-	std::string file_name = "testStates.yaml";
-	std::string msg_name = "test_states";
-	state_machine::StateContext state_context(file_name, msg_name);
+  std::string file_name = "testStates.yaml";
+  std::string msg_name = "test_states";
+  state_machine::StateContext state_context(file_name, msg_name);
 
-	state_context.nextState("started");
-	ASSERT_STREQ(state_context.getStateText().c_str(), "Init\n") << "Text should be: Init\n";
-	ASSERT_STREQ(state_context.getAvailableTransition().c_str(), "init_start:Intermediate,") << "Available transition should be: init_start:Intermediate,";
+  state_context.nextState("started");
+  ASSERT_STREQ(state_context.getStateText().c_str(), "Init\n") << "Text should be: Init\n";
+  ASSERT_STREQ(state_context.getAvailableTransition().c_str(), "init_start:Intermediate,") << "Available transition should be: init_start:Intermediate,";
 }
-
-void auxFunc2(const std::string&){
-	std::cout << "Test output";
-};
 
 TEST_F(TestSuite, SetCallbacksStateContext){
 
-	std::string file_name = "testStates.yaml";
-	std::string msg_name = "test_states";
-	state_machine::StateContext state_context(file_name, msg_name);
+  std::string file_name = "testStates.yaml";
+  std::string msg_name = "test_states";
+  state_machine::StateContext state_context(file_name, msg_name);
 
-	// Set callbacks
-	std::function<void(const std::string&)> _f = &auxFunc2;
-	ASSERT_TRUE(state_context.setCallback(state_machine::CallbackType::UPDATE, "Start", _f));
-	ASSERT_TRUE(state_context.setCallback(state_machine::CallbackType::EXIT, "Start", _f));
+  // Set callbacks
+  int counter = test_obj_.counter_;
 
-	std::ostringstream oss;
-	std::streambuf* p_cout_streambuf = std::cout.rdbuf();
-	std::cout.rdbuf(oss.rdbuf());
+  std::function<void(const std::string&)> _f = [this] (const std::string&) { test_obj_.increaseCounter(std::string("string")); };
+  ASSERT_TRUE(state_context.setCallback(state_machine::CallbackType::UPDATE, "Start", _f));
+  ASSERT_TRUE(state_context.setCallback(state_machine::CallbackType::EXIT, "Start", _f));
 
-	state_context.onUpdate();
-	std::cout.rdbuf(p_cout_streambuf); // restore
-	ASSERT_TRUE(oss && oss.str() == "Test output") << "onUpdate should show 'Test output'";
+  state_context.onUpdate();
+  counter++;
+  ASSERT_EQ(test_obj_.counter_, counter) << "counter should be : " << counter;
 
-	// Set Callback for unexisting state
-	ASSERT_TRUE(!state_context.setCallback(state_machine::CallbackType::UPDATE, "NoState", _f)) << "Should be false";
+  // Set Callback for unexisting state
+  ASSERT_TRUE(!state_context.setCallback(state_machine::CallbackType::UPDATE, "NoState", _f)) << "Should be false";
 }
 
 
