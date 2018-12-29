19a243894cfe26793ea52682ffa4ccc3d913695d
MasayaKataoka <ms.kataoka@gmail.com>
2018-12-29 18:22:42 +0900

    add value_json_func args

--- ros/src/system/autoware_health_checker/include/autoware_health_checker/node_status_publisher.h
+++ ros/src/system/autoware_health_checker/include/autoware_health_checker/node_status_publisher.h
@@ -31,14 +31,18 @@ public:
     // std::pair<double,double> first value is min value and second value is max value
     void CHECK_RANGE(std::string key,double value,std::pair<double,double> warn_value,std::pair<double,double> error_value,std::pair<double,double> fatal_value,std::string description);
     template<class T>
-    void CHECK_VALUE(std::string key,T value,std::function<uint8_t(T value)> check_func,std::string description)
+    void CHECK_VALUE(std::string key,T value,std::function<uint8_t(T value)> check_func,std::function<boost::property_tree::ptree(T value)> value_json_func,std::string description)
     {
         addNewBuffer(key);
         uint8_t check_result = check_func(value);
+        boost::property_tree::ptree pt = value_json_func(value);
+        std::stringstream ss;
+        write_json(ss, pt);
         autoware_system_msgs::DiagnosticStatus new_status;
         new_status.type = autoware_system_msgs::DiagnosticStatus::OUT_OF_RANGE;
         new_status.level = check_result;
         new_status.description = description;
+        new_status.description = ss.str();
         diag_buffers_[key]->addDiag(new_status);
     }
     void CHECK_RATE(std::string key,double warn_rate,double error_rate,double fatal_rate,std::string description);
