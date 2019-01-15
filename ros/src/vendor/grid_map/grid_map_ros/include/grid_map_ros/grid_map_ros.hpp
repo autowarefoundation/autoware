0c35ac508a78cdb5bed0dedcf1b0bed1a3837469
Esteve Fernandez <esteve@apache.org>
2019-01-15 11:43:43 +0000

    Added grid_map_ros from grid_map@1.6.0

---
+++ ros/src/vendor/grid_map/grid_map_ros/include/grid_map_ros/grid_map_ros.hpp
@@ -0,0 +1,15 @@
+/*
+ * grid_map.hpp
+ *
+ *  Created on: Jul 14, 2014
+ *      Author: Péter Fankhauser
+ *	 Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#pragma once
+
+#include <grid_map_core/grid_map_core.hpp>
+#include <grid_map_ros/message_traits.hpp>
+#include <grid_map_ros/GridMapRosConverter.hpp>
+#include <grid_map_ros/PolygonRosConverter.hpp>
+#include <grid_map_ros/GridMapMsgHelpers.hpp>
