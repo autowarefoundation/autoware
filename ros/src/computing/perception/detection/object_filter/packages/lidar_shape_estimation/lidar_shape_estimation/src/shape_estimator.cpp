1fec1524d997f242bbde24993d9bd2bd9f6a17e9
amc-nu <abrahammonrroy@yahoo.com>
2019-01-16 12:38:53 +0900

    * Fix install commands * Fix license

--- ros/src/computing/perception/detection/object_filter/packages/lidar_shape_estimation/lidar_shape_estimation/src/shape_estimator.cpp
+++ ros/src/computing/perception/detection/object_filter/packages/lidar_shape_estimation/lidar_shape_estimation/src/shape_estimator.cpp
@@ -1,6 +1,5 @@
 /*
- *  Copyright (c) 2018, TierIV, Inc.
- *  All rights reserved.
+ * Copyright 2018 Autoware Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  *
  * v1.0 Yukihiro Saito
  */
