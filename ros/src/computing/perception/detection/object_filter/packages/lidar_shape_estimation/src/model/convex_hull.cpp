c6da6036a2a8c1159a3892579fdb556fac39fee9
amc-nu <abrahammonrroy@yahoo.com>
2019-01-16 20:17:50 +0900

    Added explanation for unavailable model

--- ros/src/computing/perception/detection/object_filter/packages/lidar_shape_estimation/src/model/convex_hull.cpp
+++ ros/src/computing/perception/detection/object_filter/packages/lidar_shape_estimation/src/model/convex_hull.cpp
@@ -21,5 +21,7 @@
 
 bool ConvexHullModel::estimate(const pcl::PointCloud<pcl::PointXYZ>& cluster, autoware_msgs::DetectedObject& output)
 {
+  //currently no compatible model is available
+  //boilerplate
   return true;
 }
