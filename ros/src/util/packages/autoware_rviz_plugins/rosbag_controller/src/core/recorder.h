32d0390d50439dc7686bf7a5c208e8ef0f0e2b21
ChenxiTU <tu.chenxi@g.sp.m.is.nagoya-u.ac.jp>
2019-01-28 15:16:14 +0900

    split two licenses

--- ros/src/util/packages/autoware_rviz_plugins/rosbag_controller/src/core/recorder.h
+++ ros/src/util/packages/autoware_rviz_plugins/rosbag_controller/src/core/recorder.h
@@ -12,7 +12,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*
+**********************************************************************
 * Software License Agreement (BSD License)
 *
 *  Copyright (c) 2008, Willow Garage, Inc.
