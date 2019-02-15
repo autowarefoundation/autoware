5901b2d8bd976f76bbc506ebb24f27813d14cbbc
amc-nu <abrahammonrroy@yahoo.com>
2019-02-15 15:17:38 +0900

    * Added install commands * Don't force raw image format * Removed mutex causing multiple cameras to don't publish * Removed crash due to the use of incompatible API on a camera * Switched to ROS logging instead of std out * Commented DLTL code

--- ros/src/sensing/drivers/camera/packages/pointgrey/nodes/spinnaker/spinnaker.cpp
+++ ros/src/sensing/drivers/camera/packages/pointgrey/nodes/spinnaker/spinnaker.cpp
@@ -15,29 +15,32 @@
  * limitations under the License.
  */
 
-#include "SpinGenApi/SpinnakerGenApi.h"
-#include "Spinnaker.h"
+#include <thread>
+#include <sstream>
+#include <iostream>
+
+#include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
-#include <iostream>
-#include <mutex>
-#include <opencv2/core/core.hpp>
-#include <ros/ros.h>
+#include <std_msgs/Header.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
-#include <sstream>
-#include <std_msgs/Header.h>
-#include <thread>
+
+#include <opencv2/core/core.hpp>
+
+#include "SpinGenApi/SpinnakerGenApi.h"
+#include "Spinnaker.h"
 
 using namespace Spinnaker;
 using namespace Spinnaker::GenApi;
 using namespace Spinnaker::GenICam;
 using namespace std;
 
-std::mutex mtx;
+#define __APP_NAME__ "autoware_spinnaker"
 
-class SpinnakerCamera {
+class SpinnakerCamera
+{
 public:
   SpinnakerCamera();
   ~SpinnakerCamera();
@@ -46,33 +49,41 @@ public:
 
 private:
   ros::NodeHandle nh_, private_nh_;
-  image_transport::ImageTransport *image_transport_;
+  image_transport::ImageTransport* image_transport_;
   std::vector<image_transport::Publisher> image_pubs_;
   Spinnaker::SystemPtr system_;
   Spinnaker::CameraList camList_;
-  CameraPtr *pCamList_;
+  CameraPtr* pCamList_;
 
   // config
   int width_;
   int height_;
   double fps_;
   int dltl_;
+  std::string format_;
 
-  Spinnaker::GenApi::INodeMap *node_map_;
+  Spinnaker::GenApi::INodeMap* node_map_;
 };
 
 SpinnakerCamera::SpinnakerCamera()
-    : nh_(), private_nh_("~"), system_(Spinnaker::System::GetInstance()),
-      camList_(system_->GetCameras()), width_(0), height_(0), fps_(0),
-      dltl_(0) {
+  : nh_()
+  , private_nh_("~")
+  , system_(Spinnaker::System::GetInstance())
+  , camList_(system_->GetCameras())
+  , width_(0)
+  , height_(0)
+  , fps_(0)
+  , dltl_(0)
+{
   private_nh_.param("width", width_, 1440);
   private_nh_.param("height", height_, 1080);
   private_nh_.param("fps", fps_, 60.0);
   private_nh_.param("dltl", dltl_, 100000000);
   unsigned int num_cameras = camList_.GetSize();
-  cout << "Number of cameras detected: " << num_cameras << endl;
-  if (num_cameras < 1) {
-    std::cerr << "Error: This program requires at least 1 camera." << std::endl;
+  ROS_INFO_STREAM("[" << __APP_NAME__ << "] Number of cameras detected: " << num_cameras);
+  if (num_cameras < 1)
+  {
+    ROS_ERROR("[%s] Error: This program requires at least 1 camera.", __APP_NAME__);
     camList_.Clear();
     system_->ReleaseInstance();
     std::exit(-1);
@@ -80,76 +91,119 @@ SpinnakerCamera::SpinnakerCamera()
   image_transport_ = new image_transport::ImageTransport(nh_);
   pCamList_ = new CameraPtr[num_cameras];
 
-  try {
+  try
+  {
     vector<gcstring> strSerialNumbers(camList_.GetSize());
-    for (int i = 0; i < camList_.GetSize(); i++) {
+    for (int i = 0; i < camList_.GetSize(); i++)
+    {
       pCamList_[i] = camList_.GetByIndex(i);
       pCamList_[i]->Init();
       node_map_ = &pCamList_[i]->GetNodeMap();
       // config
       pCamList_[i]->Width.SetValue(width_);
       pCamList_[i]->Height.SetValue(height_);
-      CIntegerPtr ptrDeviceLinkThroughputLimit =
-          node_map_->GetNode("DeviceLinkThroughputLimit");
-      ptrDeviceLinkThroughputLimit->SetValue(dltl_);
-      CFloatPtr ptrAcquisitionFrameRate =
-          node_map_->GetNode("AcquisitionFrameRate");
-      CBooleanPtr ptrAcquisitionFrameRateEnable =
-          node_map_->GetNode("AcquisitionFrameRateEnable");
-      ptrAcquisitionFrameRateEnable->SetValue(true);
-      ptrAcquisitionFrameRate->SetValue(fps_);
 
-      CEnumerationPtr ptrAcquisitionMode =
-          pCamList_[i]->GetNodeMap().GetNode("AcquisitionMode");
-      if (!IsAvailable(ptrAcquisitionMode) || !IsWritable(ptrAcquisitionMode)) {
-        cout << "Unable to set acquisition mode to continuous (node retrieval; "
-                "camera "
-             << i << "). Aborting..." << endl
-             << endl;
+      ///////////////////////                 DeviceLinkThroughputLimit   /////////////////////////////
+      /*CIntegerPtr ptrDeviceLinkThroughputLimit = node_map_->GetNode("DeviceLinkThroughputLimit");
+      if (IsAvailable(ptrDeviceLinkThroughputLimit) && IsWritable(ptrDeviceLinkThroughputLimit))
+      {
+        std::cout << "DeviceLinkThroughputLimit" << ptrDeviceLinkThroughputLimit->GetValue() << std::endl;
+        ptrDeviceLinkThroughputLimit->SetValue(dltl_);
+      }
+      else
+      {
+        ROS_WARN("[%s] This camera does not support DeviceLinkThroughputLimit, using default value.", __APP_NAME__);
+      }*/
+
+      ///////////////////////                 FrameRate                   /////////////////////////////
+      CFloatPtr ptrAcquisitionFrameRate = node_map_->GetNode("AcquisitionFrameRate");
+      CBooleanPtr ptrAcquisitionFrameRateEnable = node_map_->GetNode("AcquisitionFrameRateEnable");
+      if (IsAvailable(ptrAcquisitionFrameRate) && IsWritable(ptrAcquisitionFrameRate))
+      {
+        ptrAcquisitionFrameRateEnable->SetValue(true);
+        ptrAcquisitionFrameRate->SetValue(fps_);
+      }
+      else
+      {
+        ROS_WARN("[%s] This camera does not support AcquisitionFrameRate, using default value.", __APP_NAME__);
       }
 
-      CEnumEntryPtr ptrAcquisitionModeContinuous =
-          ptrAcquisitionMode->GetEntryByName("Continuous");
-      if (!IsAvailable(ptrAcquisitionModeContinuous) ||
-          !IsReadable(ptrAcquisitionModeContinuous)) {
-        cout << "Unable to set acquisition mode to continuous (entry "
-                "'continuous' retrieval "
-             << i << "). Aborting..." << endl
-             << endl;
+      ///////////////////////                 PixelFormat                 /////////////////////////////
+      CEnumerationPtr ptrPixelFormat = node_map_->GetNode("PixelFormat");
+      if (IsAvailable(ptrPixelFormat) && IsWritable(ptrPixelFormat))
+      {
+        ROS_INFO_STREAM("Current Format" << ptrPixelFormat->GetCurrentEntry()->GetSymbolic());
+        /*gcstring pixel_format = format_.c_str();
+
+        CEnumEntryPtr ptrPixelFormatSetup = ptrPixelFormat->GetEntryByName(pixel_format);
+        if (IsAvailable(ptrPixelFormatSetup) && IsReadable(ptrPixelFormatSetup))
+        {
+          int64_t pixelFormatSetup = ptrPixelFormatSetup->GetValue();
+          ptrPixelFormat->SetIntValue(ptrPixelFormatSetup);
+
+          ROS_INFO_STREAM("[" << __APP_NAME__ << "] Pixel format set to " <<
+                                            ptrPixelFormat->GetCurrentEntry()->GetSymbolic());
+        }
+        else
+        {
+          ROS_WARN("[%s] %s pixel format not available. Using default.", __APP_NAME__, format_.c_str());
+        }*/
+      }
+      else
+      {
+        ROS_WARN("[%s] Pixel format cannot be changed on this camera. Using default.", __APP_NAME__);
       }
 
-      int64_t acquisitionModeContinuous =
-          ptrAcquisitionModeContinuous->GetValue();
+      ///////////////////////                 AcquisitionMode                 /////////////////////////////
+      CEnumerationPtr ptrAcquisitionMode = pCamList_[i]->GetNodeMap().GetNode("AcquisitionMode");
+      if (!IsAvailable(ptrAcquisitionMode) || !IsWritable(ptrAcquisitionMode))
+      {
+        ROS_FATAL("[%s] Unable to set acquisition mode to continuous (node retrieval; "
+                  "camera ",
+                  __APP_NAME__);
+      }
 
+      ///////////////////////                 ContinuousMode                  /////////////////////////////
+      CEnumEntryPtr ptrAcquisitionModeContinuous = ptrAcquisitionMode->GetEntryByName("Continuous");
+      if (!IsAvailable(ptrAcquisitionModeContinuous) || !IsReadable(ptrAcquisitionModeContinuous))
+      {
+        ROS_FATAL("[%s] Unable to set acquisition mode to continuous (entry "
+                  "'continuous' retrieval. Aborting...",
+                  __APP_NAME__);
+      }
+
+      int64_t acquisitionModeContinuous = ptrAcquisitionModeContinuous->GetValue();
       ptrAcquisitionMode->SetIntValue(acquisitionModeContinuous);
 
-      cout << "[camera" << i << "] acquisition mode set to continuous..."
-           << endl;
+      ROS_INFO("[%s] [camera %d] acquisition mode set to continuous...", __APP_NAME__, i);
 
       strSerialNumbers[i] = "";
-      CStringPtr ptrStringSerial =
-          pCamList_[i]->GetTLDeviceNodeMap().GetNode("DeviceSerialNumber");
-      if (IsAvailable(ptrStringSerial) && IsReadable(ptrStringSerial)) {
+      CStringPtr ptrStringSerial = pCamList_[i]->GetTLDeviceNodeMap().GetNode("DeviceSerialNumber");
+      if (IsAvailable(ptrStringSerial) && IsReadable(ptrStringSerial))
+      {
         strSerialNumbers[i] = ptrStringSerial->GetValue();
-        cout << "[camera" << i << "] serial number set to "
-             << strSerialNumbers[i] << "..." << endl;
+        ROS_INFO("[%s] [camera %d] serial number set to %s", __APP_NAME__, i, strSerialNumbers[i].c_str());
       }
       std::string topic("image_raw");
       if (camList_.GetSize() > 1)
         topic = "camera" + std::to_string(i) + "/" + topic;
       image_pubs_.push_back(image_transport_->advertise(topic, 100));
     }
-  } catch (Spinnaker::Exception &e) {
-    cout << "Error: " << e.what() << endl;
+  }
+  catch (Spinnaker::Exception& e)
+  {
+    ROS_ERROR("[%s] Error: %s", __APP_NAME__, e.what());
   }
 }
 
-SpinnakerCamera::~SpinnakerCamera() {
-  for (int i = 0; i < camList_.GetSize(); i++) {
+SpinnakerCamera::~SpinnakerCamera()
+{
+  for (int i = 0; i < camList_.GetSize(); i++)
+  {
     pCamList_[i]->EndAcquisition();
     pCamList_[i]->DeInit();
     pCamList_[i] = NULL;
-    cout << "[camera" << i << "] shutdown..." << endl;
+    ROS_INFO("[%s] Shutting down camera %d", __APP_NAME__, i);
   }
   delete[] pCamList_;
   node_map_ = NULL;
@@ -157,63 +211,99 @@ SpinnakerCamera::~SpinnakerCamera() {
   system_->ReleaseInstance();
 }
 
-void SpinnakerCamera::publish_image(int index) {
-  while (ros::ok()) {
-    try {
-      mtx.lock();
+void SpinnakerCamera::publish_image(int index)
+{
+  while (ros::ok())
+  {
+    try
+    {
       ImagePtr pResultImage = pCamList_[index]->GetNextImage();
-      if (pResultImage->IsIncomplete()) {
-        cout << "[camera" << index << "] Image incomplete with image status "
-             << pResultImage->GetImageStatus() << "..." << endl
-             << endl;
-      } else {
+      if (pResultImage->IsIncomplete())
+      {
+        ROS_WARN("[%s] [camera %d] Image incomplete with image status %d", __APP_NAME__, index,
+                 pResultImage->GetImageStatus());
+      }
+      else
+      {
         ros::Time receive_time = ros::Time::now();
 
         // create opencv mat
-        ImagePtr convertedImage =
-            pResultImage->Convert(PixelFormat_BGR8, NEAREST_NEIGHBOR);
-        unsigned int XPadding = convertedImage->GetXPadding();
-        unsigned int YPadding = convertedImage->GetYPadding();
-        unsigned int rowsize = convertedImage->GetWidth();
-        unsigned int colsize = convertedImage->GetHeight();
+        int pixel_format = pResultImage->GetPixelFormat();
+        std::string encoding_pattern;
+        switch (pixel_format)
+        {
+          case PixelFormatEnums::PixelFormat_BayerRG8:
+            encoding_pattern = "bayer_rggb8";
+            break;
+          case PixelFormatEnums::PixelFormat_BayerGR8:
+            encoding_pattern = "bayer_grbg8";
+            break;
+          case PixelFormatEnums::PixelFormat_BayerGB8:
+            encoding_pattern = "bayer_gbrg8";
+            break;
+          case PixelFormatEnums::PixelFormat_BayerBG8:
+            encoding_pattern = "bayer_bggr8";
+            break;
+          case PixelFormatEnums::PixelFormat_RGB8:
+            encoding_pattern = "rgb8";
+            break;
+          case PixelFormatEnums::PixelFormat_BGR8:
+            encoding_pattern = "bgr8";
+            break;
+          default:
+            encoding_pattern = "mono8";
+        }
+        unsigned int XPadding = pResultImage->GetXPadding();
+        unsigned int YPadding = pResultImage->GetYPadding();
+        unsigned int rowsize = pResultImage->GetWidth();
+        unsigned int colsize = pResultImage->GetHeight();
 
-        // image data contains padding. When allocating Mat container size,
-        // you need to account for the X,Y image data padding.
-        cv::Mat cvimg =
-            cv::Mat(colsize + YPadding, rowsize + XPadding, CV_8UC3,
-                    convertedImage->GetData(), convertedImage->GetStride());
         // create and publish ros message
         std_msgs::Header header;
+        sensor_msgs::Image msg;
         header.stamp = receive_time;
-        header.frame_id = (camList_.GetSize() > 1)
-                              ? "camera" + std::to_string(index)
-                              : "camera";
-        sensor_msgs::ImagePtr msg =
-            cv_bridge::CvImage(header, "bgr8", cvimg).toImageMsg();
+        header.frame_id = (camList_.GetSize() > 1) ? "camera" + std::to_string(index) : "camera";
+
+        msg.header = header;
+        msg.height = pResultImage->GetHeight();
+        msg.width = pResultImage->GetWidth();
+        msg.encoding = encoding_pattern;
+        msg.step = pResultImage->GetStride();
+
+        size_t image_size = pResultImage->GetImageSize();
+        msg.data.resize(image_size);
+        memcpy(msg.data.data(), pResultImage->GetData(), image_size);
+
         image_pubs_[index].publish(msg);
       }
       pResultImage->Release();
-      mtx.unlock();
-    } catch (Spinnaker::Exception &e) {
-      cout << "Error: " << e.what() << endl;
+    }
+    catch (Spinnaker::Exception& e)
+    {
+      ROS_ERROR("[%s] Error: %s", __APP_NAME__, e.what());
     }
   }
 }
-void SpinnakerCamera::spin() {
-  for (int i = 0; i < camList_.GetSize(); i++) {
+void SpinnakerCamera::spin()
+{
+  for (int i = 0; i < camList_.GetSize(); i++)
+  {
     pCamList_[i]->BeginAcquisition();
-    cout << "[camera" << i << "] started acquiring images..." << endl;
+    ROS_INFO("[%s] [camera %d] started acquisition", __APP_NAME__, i);
   }
   vector<std::shared_ptr<std::thread>> threads(camList_.GetSize());
-  for (int i = 0; i < camList_.GetSize(); i++) {
+  for (int i = 0; i < camList_.GetSize(); i++)
+  {
     threads[i] = make_shared<thread>(&SpinnakerCamera::publish_image, this, i);
   }
-  for (auto &thread : threads) {
+  for (auto& thread : threads)
+  {
     thread->join();
   }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "spinnaker");
   SpinnakerCamera node;
   node.spin();
