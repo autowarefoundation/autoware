b4c35c3d95a904b6bb303df242300a5b1ce0e691
Isamu Takagi <isamu.takagi@tier4.jp>
2019-03-03 16:50:42 +0900

    Fix setup.py to install all packages

--- ros/src/util/packages/autoware_launcher/setup.py
+++ ros/src/util/packages/autoware_launcher/setup.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['autoware_launcher'],
-     package_dir={'': 'src'}
+    package_dir={"autoware_launcher": "src/autoware_launcher"},
+    packages=[
+        "autoware_launcher",
+        "autoware_launcher.tool",
+        "autoware_launcher.core",
+        "autoware_launcher.gui",
+        "autoware_launcher.gui.simulation",
+        "autoware_launcher.gui.plugins",
+    ],
 )
 
 setup(**setup_args)
-
