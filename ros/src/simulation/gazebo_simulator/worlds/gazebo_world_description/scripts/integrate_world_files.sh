fdf9888014bebffad8f2f27fcd3b8e7b52cf44b6
Yukihiro SAITO <yukky.saito@gmail.com>
2019-03-12 21:38:51 +0900

    remove copy script and use symlink

--- ros/src/simulation/gazebo_simulator/worlds/gazebo_world_description/scripts/integrate_world_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-echo "Copy world file to gazebo_world_description"
-cp -n $SCRIPT_DIR/../../external/osrf_citysim/worlds/citysim_gazebo7.world $SCRIPT_DIR/../worlds/
-cp -n $SCRIPT_DIR/../../external/osrf_citysim/worlds/citysim_gazebo9.world $SCRIPT_DIR/../worlds/
-cp -n $SCRIPT_DIR/../../external/car_demo/car_demo/worlds/mcity.world $SCRIPT_DIR/../worlds/
-cp -n $SCRIPT_DIR/../../external/car_demo/car_demo/worlds/mcity_r1.world $SCRIPT_DIR/../worlds/
