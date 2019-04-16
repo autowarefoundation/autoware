20feab948c16b4f2323e614eb575844fa066bebd
David <david@lifecyclist.co.nz>
2019-04-16 12:51:09 +0900

    Revert "Apply ROS clang format"
    This reverts commit 88563f1aa768430d78b6e4f9f9612d0680ae2385.
    
    Signed-off-by: David <david@lifecyclist.co.nz>

--- ros/src/computing/perception/detection/visualizers/packages/integrated_viewer/node/data_rate_checker_plugin/data_rate_checker_plugin.cpp
+++ ros/src/computing/perception/detection/visualizers/packages/integrated_viewer/node/data_rate_checker_plugin/data_rate_checker_plugin.cpp
@@ -7,247 +7,238 @@
 
 #include "data_rate_checker_plugin.h"
 
-namespace integrated_viewer
-{
-const QString DataRateCheckerPlugin::kBlankTopic = "-----";
-std::string DummyMsg::md5 = "*";
-std::string DummyMsg::data_type = "/";
+namespace integrated_viewer {
 
-DataRateCheckerPlugin::DataRateCheckerPlugin(QWidget* parent) : rviz::Panel(parent)
-{
-  // Initialize form
-  ui_.setupUi(this);
+    const QString DataRateCheckerPlugin::kBlankTopic = "-----";
+    std::string DummyMsg::md5 = "*";
+    std::string DummyMsg::data_type = "/";
 
-  // Set minimum frequency parameter
-  ui_.min_frequency_spin_box_->setMinimum(1);
-  ui_.min_frequency_spin_box_->setMaximum(10000);
-  ui_.min_frequency_spin_box_->setValue(5);
-  ui_.topic_frequency_lcd_->setMinimumWidth(ui_.topic_frequency_lcd_->width() + 2);
+    DataRateCheckerPlugin::DataRateCheckerPlugin(QWidget *parent)
+            : rviz::Panel(parent) {
 
-  UpdateTopicList();
+        // Initialize form
+        ui_.setupUi(this);
 
-  // If combobox is clicked, topic list will be update
-  ui_.topic_combo_box_->installEventFilter(this);
-}  // DataRateCheckerPlugin::DataRateCheckerPlugin
+        // Set minimum frequency parameter
+        ui_.min_frequency_spin_box_->setMinimum(1);
+        ui_.min_frequency_spin_box_->setMaximum(10000);
+        ui_.min_frequency_spin_box_->setValue(5);
+        ui_.topic_frequency_lcd_->setMinimumWidth(ui_.topic_frequency_lcd_->width()+2);
 
-void DataRateCheckerPlugin::UpdateTopicList(void)
-{
-  // The topic list that can be selected from the UI
-  QStringList topic_list;
+        UpdateTopicList();
 
-  // The topic name currently chosen
-  QString topic_current = ui_.topic_combo_box_->currentText();
+        // If combobox is clicked, topic list will be update
+        ui_.topic_combo_box_->installEventFilter(this);
+    } // DataRateCheckerPlugin::DataRateCheckerPlugin
 
-  if (topic_current == "")
-  {
-    topic_current = kBlankTopic;
-    ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
-    ui_.status_text_->setText(QString("No topic selected"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
-    ui_.topic_frequency_lcd_->display(0.0);
-  }
 
-  // Insert blank topic name to the top of the lists
-  topic_list << kBlankTopic;
+    void DataRateCheckerPlugin::UpdateTopicList(void) {
+        // The topic list that can be selected from the UI
+        QStringList topic_list;
 
-  // Get all available topic
-  ros::master::V_TopicInfo master_topics;
-  ros::master::getTopics(master_topics);
+        // The topic name currently chosen
+        QString topic_current = ui_.topic_combo_box_->currentText();
 
-  // Convert topic names
-  for (ros::master::V_TopicInfo::iterator it = master_topics.begin(); it != master_topics.end(); it++)
-  {
-    const ros::master::TopicInfo& info = *it;
-    const QString topic_name = QString::fromStdString(info.name);
-    const QString topic_type = QString::fromStdString(info.datatype);
-    topic_list << topic_name;
-  }
+        if (topic_current == "") {
+            topic_current = kBlankTopic;
+            ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
+            ui_.status_text_->setText(QString("No topic selected"));
+            ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
+            ui_.topic_frequency_lcd_->display(0.0);
+        }
 
-  // Sort alphabetically
-  topic_list.sort();
+        // Insert blank topic name to the top of the lists
+        topic_list << kBlankTopic;
 
-  // Remove all list items from combo box
-  ui_.topic_combo_box_->clear();
+        // Get all available topic
+        ros::master::V_TopicInfo master_topics;
+        ros::master::getTopics(master_topics);
 
-  // Set new items to combo box
-  ui_.topic_combo_box_->addItems(topic_list);
+        // Convert topic names
+        for (ros::master::V_TopicInfo::iterator it = master_topics.begin(); it != master_topics.end(); it++) {
+            const ros::master::TopicInfo &info = *it;
+            const QString topic_name = QString::fromStdString(info.name);
+            const QString topic_type = QString::fromStdString(info.datatype);
+            topic_list << topic_name;
+        }
 
-  ui_.topic_combo_box_->insertSeparator(1);
+        // Sort alphabetically
+        topic_list.sort();
 
-  // Set last topic as current
-  int topic_index = ui_.topic_combo_box_->findText(topic_current);
+        // Remove all list items from combo box
+        ui_.topic_combo_box_->clear();
 
-  if (topic_index != -1)
-  {
-    ui_.topic_combo_box_->setCurrentIndex(topic_index);
-  }
-  else
-  {
-    ui_.topic_combo_box_->setCurrentIndex(ui_.topic_combo_box_->findText(kBlankTopic));
-    topic_sub_.shutdown();
-    timer_.stop();
-    ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
-    ui_.status_text_->setText(QString("No topic selected"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
-    ui_.topic_frequency_lcd_->display(0.0);
-    times_.clear();
-    last_message_time_ = -1;
-  }
-}  // DataRateCheckerPlugin::UpdateTopicList
+        // Set new items to combo box
+        ui_.topic_combo_box_->addItems(topic_list);
 
-// The behavior of combo box
-void DataRateCheckerPlugin::on_topic_combo_box__activated(int index)
-{
-  // Extract selected topic name from combo box
-  std::string selected_topic = ui_.topic_combo_box_->itemText(index).toStdString();
-  if (selected_topic == kBlankTopic.toStdString() || selected_topic == "")
-  {
-    topic_sub_.shutdown();
-    timer_.stop();
-    ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
-    ui_.status_text_->setText(QString("No topic selected"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
-    ui_.topic_frequency_lcd_->display(0.0);
-    times_.clear();
-    last_message_time_ = -1;
-    return;
-  }
-  times_.clear();
-  last_message_time_ = -1;
-  // If selected topic is not blank or empty, start callback functions
-  topic_sub_ = node_handle_.subscribe(selected_topic, 1000, &DataRateCheckerPlugin::MessageCallback, this);
+        ui_.topic_combo_box_->insertSeparator(1);
 
-  timer_ = node_handle_.createWallTimer(ros::WallDuration(1.0), &DataRateCheckerPlugin::TimerCallback, this);
+        // Set last topic as current
+        int topic_index = ui_.topic_combo_box_->findText(topic_current);
 
-}  // DataRateCheckerPlugin::on_topic_combo_box__activated
+        if (topic_index != -1) {
+            ui_.topic_combo_box_->setCurrentIndex(topic_index);
+        }
+        else {
+          ui_.topic_combo_box_->setCurrentIndex(ui_.topic_combo_box_->findText(kBlankTopic));
+          topic_sub_.shutdown();
+          timer_.stop();
+          ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
+          ui_.status_text_->setText(QString("No topic selected"));
+          ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
+          ui_.topic_frequency_lcd_->display(0.0);
+          times_.clear();
+          last_message_time_ = -1;
+        }
+    } // DataRateCheckerPlugin::UpdateTopicList
 
-void DataRateCheckerPlugin::MessageCallback(const DummyMsg& msg)
-{
-  // Record time message arrives
-  ros::Time ros_message_time = ros::Time::now();
+    // The behavior of combo box
+    void DataRateCheckerPlugin::on_topic_combo_box__activated(int index) {
+        // Extract selected topic name from combo box
+        std::string selected_topic = ui_.topic_combo_box_->itemText(index).toStdString();
+        if (selected_topic == kBlankTopic.toStdString() || selected_topic == "") {
+            topic_sub_.shutdown();
+            timer_.stop();
+            ui_.status_icon_->setStyleSheet("QLabel {color: #b3b3b3;}");
+            ui_.status_text_->setText(QString("No topic selected"));
+            ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #b3b3b3;}");
+            ui_.topic_frequency_lcd_->display(0.0);
+            times_.clear();
+            last_message_time_ = -1;
+            return;
+        }
+        times_.clear();
+        last_message_time_ = -1;
+        // If selected topic is not blank or empty, start callback functions
+        topic_sub_ = node_handle_.subscribe(selected_topic,
+                                            1000,
+                                            &DataRateCheckerPlugin::MessageCallback,
+                                            this);
 
-  if (ros_message_time.is_zero())
-  {
-    times_.clear();
-    last_message_time_ = -1;
-    return;
-  }
+        timer_ = node_handle_.createWallTimer(ros::WallDuration(1.0),
+                                              &DataRateCheckerPlugin::TimerCallback,
+                                              this);
 
-  double message_time = ros_message_time.toSec();
+    } // DataRateCheckerPlugin::on_topic_combo_box__activated
 
-  if (last_message_time_ < 0 || last_message_time_ > message_time)
-  {
-    times_.clear();
-    last_message_time_ = message_time;
-    return;
-  }
-  times_.push_back(message_time - last_message_time_);
-  last_message_time_ = message_time;
+    void DataRateCheckerPlugin::MessageCallback(const DummyMsg &msg) {
 
-  if (times_.size() + 1 > window_size_)
-  {
-    times_.pop_front();
-  }
-}  // DataRateCheckerPlugin::MessageCallback
+      // Record time message arrives
+      ros::Time ros_message_time = ros::Time::now();
 
-void DataRateCheckerPlugin::TimerCallback(const ros::WallTimerEvent& event)
-{
-  // Warn if no new messages have arrived
-  if (last_message_time_ == -1 || times_.size() == 0)
-  {
-    ui_.status_icon_->setStyleSheet("QLabel {color: #ff0000}");
-    ui_.status_text_->setText(QString("No incoming messages"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #ff0000;}");
-    ui_.topic_frequency_lcd_->display(0.0);
-
-    // Clear the statistics buffer
-    times_.clear();
-    last_message_time_ = -1;
-    return;
-  }
-
-  // Check average frame rates
-  double avg_delay = std::accumulate(times_.begin(), times_.end(), 0.0) / times_.size();
-  double frequency = 1.0 / avg_delay;
-
-  // Warn if message frequency is too low
-  if (frequency < (double)(ui_.min_frequency_spin_box_->value()))
-  {
-    ui_.status_icon_->setStyleSheet("QLabel {color: #ff0000;}");
-    ui_.status_text_->setText(QString("Message rate is too low"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #ff0000;}");
-    ui_.topic_frequency_lcd_->display(frequency);
-    return;
-  }
-
-  // Otherwise, we are good!
-  else
-  {
-    ui_.status_icon_->setStyleSheet("QLabel {color: #00ff00;}");
-    ui_.status_text_->setText(QString("Message rate is OK"));
-    ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #00ff00;}");
-    ui_.topic_frequency_lcd_->display(frequency);
-    return;
-  }
-}  // DataRateCheckerPlugin::TimerCallback
-
-void DataRateCheckerPlugin::resizeEvent(QResizeEvent*)
-{
-}  // DataRateCheckerPlugin::resizeEvent
-
-bool DataRateCheckerPlugin::eventFilter(QObject* object, QEvent* event)
-{
-  if (event->type() == QEvent::MouseButtonPress)
-  {
-    // Combo box will update its contents if this filter is applied
-    UpdateTopicList();
-  }
-  return QObject::eventFilter(object, event);
-}  // DataRateCheckerPlugin::eventFilter
-
-void DataRateCheckerPlugin::save(rviz::Config config) const
-{
-  rviz::Panel::save(config);
-  config.mapSetValue("Topic", ui_.topic_combo_box_->currentText());
-  config.mapSetValue("Min rate", ui_.min_frequency_spin_box_->value());
-}  // DataRateCheckerPlugin::save
-
-void DataRateCheckerPlugin::load(const rviz::Config& config)
-{
-  rviz::Panel::load(config);
-  QString topic;
-  int min_frequency;
-  if (config.mapGetString("Topic", &topic))
-  {
-    // Extract selected topic name from combo box
-    std::string selected_topic = topic.toStdString();
-    if (selected_topic != kBlankTopic.toStdString() && selected_topic != "")
-    {
-      UpdateTopicList();
-      int topic_index = ui_.topic_combo_box_->findText(topic);
-      // If the load topic doesn't exist, load it anyway to wait for the topic to become active
-      if (topic_index == -1)
-      {
-        QStringList dummy_topic_list;
-        dummy_topic_list << topic;
-        ui_.topic_combo_box_->addItems(dummy_topic_list);
-        topic_index = ui_.topic_combo_box_->findText(topic);
+      if (ros_message_time.is_zero()) {
+        times_.clear();
+        last_message_time_ = -1;
+        return;
       }
-      times_.clear();
-      last_message_time_ = -1;
-      ui_.topic_combo_box_->setCurrentIndex(topic_index);
-      topic_sub_ = node_handle_.subscribe(selected_topic, 1000, &DataRateCheckerPlugin::MessageCallback, this);
 
-      timer_ = node_handle_.createWallTimer(ros::WallDuration(1.0), &DataRateCheckerPlugin::TimerCallback, this);
-    }
-  }
-  if (config.mapGetInt("Min rate", &min_frequency))
-  {
-    ui_.min_frequency_spin_box_->setValue(min_frequency);
-  }
-}  // DataRateCheckerPlugin::load
+      double message_time = ros_message_time.toSec();
 
-}  // End namespace integrated_viewer
+      if (last_message_time_ < 0 || last_message_time_ > message_time) {
+        times_.clear();
+        last_message_time_ = message_time;
+        return;
+      }
+      times_.push_back(message_time - last_message_time_);
+      last_message_time_ = message_time;
+
+      if (times_.size() + 1 > window_size_) {
+        times_.pop_front();
+      }
+    } // DataRateCheckerPlugin::MessageCallback
+
+    void DataRateCheckerPlugin::TimerCallback(const ros::WallTimerEvent &event) {
+
+      // Warn if no new messages have arrived
+      if (last_message_time_ == -1 || times_.size() == 0) {
+        ui_.status_icon_->setStyleSheet("QLabel {color: #ff0000}");
+        ui_.status_text_->setText(QString("No incoming messages"));
+        ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #ff0000;}");
+        ui_.topic_frequency_lcd_->display(0.0);
+
+        // Clear the statistics buffer
+        times_.clear();
+        last_message_time_ = -1;
+        return;
+      }
+
+      // Check average frame rates
+      double avg_delay = std::accumulate(times_.begin(), times_.end(), 0.0) / times_.size();
+      double frequency = 1.0 / avg_delay;
+
+      // Warn if message frequency is too low
+      if (frequency < (double)(ui_.min_frequency_spin_box_->value())) {
+        ui_.status_icon_->setStyleSheet("QLabel {color: #ff0000;}");
+        ui_.status_text_->setText(QString("Message rate is too low"));
+        ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #ff0000;}");
+        ui_.topic_frequency_lcd_->display(frequency);
+        return;
+      }
+
+      // Otherwise, we are good!
+      else {
+        ui_.status_icon_->setStyleSheet("QLabel {color: #00ff00;}");
+        ui_.status_text_->setText(QString ("Message rate is OK"));
+        ui_.topic_frequency_lcd_->setStyleSheet("QLCDNumber {color: #00ff00;}");
+        ui_.topic_frequency_lcd_->display(frequency);
+        return;
+      }
+    } // DataRateCheckerPlugin::TimerCallback
+
+    void DataRateCheckerPlugin::resizeEvent(QResizeEvent *) {
+    } // DataRateCheckerPlugin::resizeEvent
+
+    bool DataRateCheckerPlugin::eventFilter(QObject *object, QEvent *event) {
+        if (event->type() == QEvent::MouseButtonPress) {
+            // Combo box will update its contents if this filter is applied
+            UpdateTopicList();
+        }
+        return QObject::eventFilter(object, event);
+    } // DataRateCheckerPlugin::eventFilter
+
+    void DataRateCheckerPlugin::save(rviz::Config config) const {
+      rviz::Panel::save(config);
+      config.mapSetValue("Topic", ui_.topic_combo_box_->currentText());
+      config.mapSetValue("Min rate", ui_.min_frequency_spin_box_->value());
+    } // DataRateCheckerPlugin::save
+
+    void DataRateCheckerPlugin::load(const rviz::Config& config) {
+      rviz::Panel::load(config);
+      QString topic;
+      int min_frequency;
+      if(config.mapGetString ("Topic", &topic))
+      {
+        // Extract selected topic name from combo box
+        std::string selected_topic = topic.toStdString();
+        if (selected_topic != kBlankTopic.toStdString() && selected_topic != "") {
+          UpdateTopicList();
+          int topic_index = ui_.topic_combo_box_->findText(topic);
+          // If the load topic doesn't exist, load it anyway to wait for the topic to become active
+          if (topic_index == -1) {
+            QStringList dummy_topic_list;
+            dummy_topic_list << topic;
+            ui_.topic_combo_box_->addItems(dummy_topic_list);
+            topic_index = ui_.topic_combo_box_->findText(topic);
+          }
+          times_.clear();
+          last_message_time_ = -1;
+          ui_.topic_combo_box_->setCurrentIndex(topic_index);
+          topic_sub_ = node_handle_.subscribe(selected_topic,
+                                              1000,
+                                              &DataRateCheckerPlugin::MessageCallback,
+                                              this);
+
+          timer_ = node_handle_.createWallTimer(ros::WallDuration(1.0),
+                                                &DataRateCheckerPlugin::TimerCallback,
+                                                this);
+        }
+      }
+      if(config.mapGetInt ("Min rate", &min_frequency)) {
+        ui_.min_frequency_spin_box_->setValue(min_frequency);
+      }
+    } // DataRateCheckerPlugin::load
+
+} // End namespace integrated_viewer
 
 // Tell pluginlib about this class.  Every class which should be
 // loadable by pluginlib::ClassLoader must have these two lines
