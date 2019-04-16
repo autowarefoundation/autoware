e019f162d9341de7cb693d8298dfd01ad3d08db2
Geoffrey Biggs <geoff.biggs@tier4.jp>
2019-04-16 14:52:53 +0900

    Move javad_navsat_driver package to drivers repository
    Signed-off-by: Geoffrey Biggs <geoff.biggs@tier4.jp>

--- ros/src/sensing/drivers/gnss/packages/javad_navsat_driver/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['libjavad_navsat_driver'],
-    package_dir={'': 'lib'}
-)
-
-setup(**d)
