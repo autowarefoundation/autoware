038c853420f611c374b6bbc2c32ae1d4abdc2460
TakaHoribe <horibe.takamasa@gmail.com>
2019-05-12 00:04:28 +0900

    twist_generator: add runtime_manager & change node name
    Signed-off-by: TakaHoribe <horibe.takamasa@gmail.com>

--- ros/src/computing/perception/localization/packages/twist_generator/src/vehicle_status_converter.cpp
+++ ros/src/computing/perception/localization/packages/twist_generator/src/vehicle_status_converter.cpp
@@ -16,6 +16,7 @@
 
 #include <ros/ros.h>
 
+#include <std_msgs/Float32MultiArray.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <autoware_msgs/VehicleStatus.h>
 #include <nav_msgs/Odometry.h>
@@ -23,15 +24,15 @@
 static const double KMPH2MPS = 1000.0 / 3600.0;
 static const double DEG2RAD = 3.141592 / 180.0;
 
-class TwistGenerator
+class VehicleStatusConverter
 {
 public:
-  TwistGenerator() : nh_(""), pnh_("~")
+  VehicleStatusConverter() : nh_(""), pnh_("~")
   {
     pub_twist_ = nh_.advertise<geometry_msgs::TwistStamped>("/can_twist", 1);
-    pub_twist_adapted_ = nh_.advertise<geometry_msgs::TwistStamped>("/can_twist_adapted", 1);
-    sub_vehicle_status_ = nh_.subscribe("/vehicle_status", 1, &TwistGenerator::callbackVehicleStatus, this);
-    sub_estimate_twist_ = nh_.subscribe("/estimate_twist", 1, &TwistGenerator::callbackEstimateTwist, this);
+    pub_correction_coeff_ = pnh_.advertise<std_msgs::Float32MultiArray>("convert_correction_coeff", 1);
+    sub_vehicle_status_ = nh_.subscribe("/vehicle_status", 1, &VehicleStatusConverter::callbackVehicleStatus, this);
+    sub_estimate_twist_ = nh_.subscribe("/estimate_twist", 1, &VehicleStatusConverter::callbackEstimateTwist, this);
 
     pnh_.param("wheelbase", wheelbase_, double(2.9));
     pnh_.param("steering_gear_ratio", steering_gear_ratio_, double(15.0));
@@ -49,16 +50,18 @@ public:
     }
     ROS_INFO("set as\n * wheelbase: %f\n * steering_gear_ratio: %f", wheelbase_, steering_gear_ratio_);
 
-    adaptive_coefficient_ = 1.0; // adaptive coefficient for angular velocity calculaton
-    Pn_ = 1000.0;                // initial covariance
-    rho_ = 0.999;                // forgetting factor
+    adaptive_coefficient_wz_ = 1.0; // adaptive coefficient for angular velocity calculaton
+    adaptive_coefficient_vx_ = 1.0; // adaptive coefficient for angular velocity calculaton
+    Pn_wz_ = 1000.0;                // initial covariance
+    Pn_vx_ = 1000.0;                // initial covariance
+    rho_ = 0.999;                   // forgetting factor
   };
-  ~TwistGenerator(){};
+  ~VehicleStatusConverter(){};
 
 private:
   ros::NodeHandle nh_, pnh_;
   ros::Subscriber sub_vehicle_status_, sub_estimate_twist_;
-  ros::Publisher pub_twist_, pub_twist_adapted_;
+  ros::Publisher pub_twist_, pub_correction_coeff_;
   autoware_msgs::VehicleStatus current_vehicle_status_;
 
   /* parameters for twist calculation */
@@ -67,7 +70,7 @@ private:
 
   /* for adaptive estimation */
   bool enable_adaptive_estimate_;
-  double Pn_, rho_, adaptive_coefficient_;
+  double Pn_vx_, Pn_wz_, rho_, adaptive_coefficient_vx_, adaptive_coefficient_wz_;
 
   void callbackVehicleStatus(const autoware_msgs::VehicleStatus &msg)
   {
@@ -76,45 +79,60 @@ private:
     const double steer_rad = msg.angle / steering_gear_ratio_ * DEG2RAD; // convert handle angle [deg] to tire angle [rad]
 
     geometry_msgs::TwistStamped twist_stamped;
-    twist_stamped.header = msg.header;
+    twist_stamped.header.stamp = msg.header.stamp;
+    twist_stamped.header.frame_id = "/base_link";
     twist_stamped.twist.linear.x = vel_mps;
     twist_stamped.twist.angular.z = vel_mps * std::tan(steer_rad) / wheelbase_;
     if (enable_adaptive_estimate_)
     {
-      twist_stamped.twist.angular.z *= adaptive_coefficient_;
+      twist_stamped.twist.linear.x *= adaptive_coefficient_vx_;
+      twist_stamped.twist.angular.z *= adaptive_coefficient_wz_;
     }
     pub_twist_.publish(twist_stamped);
   }
 
   void callbackEstimateTwist(const geometry_msgs::TwistStamped &estimate_twist)
   {
-    const double w_ndt = estimate_twist.twist.angular.z;
     const double vel_mps = current_vehicle_status_.speed * KMPH2MPS;
     const double steer_rad = current_vehicle_status_.angle / steering_gear_ratio_ * DEG2RAD;
 
     if (vel_mps > 1.0) // update only when estimate angular vel is reliable enough
     {
-      updateAdaptiveCoeff(w_ndt, vel_mps, steer_rad);
+      updateAdaptiveCoeffVel(estimate_twist.twist.linear.x, vel_mps);
+      updateAdaptiveCoeffAngvel(estimate_twist.twist.angular.z, vel_mps, steer_rad);
     }
+
+    std_msgs::Float32MultiArray msg;
+    msg.data.push_back(adaptive_coefficient_vx_);
+    msg.data.push_back(adaptive_coefficient_wz_);
+    pub_correction_coeff_.publish(msg);
   }
 
-  void updateAdaptiveCoeff(const double &w_ndt, const double &vel, const double &steer)
+  void updateAdaptiveCoeffAngvel(const double &w_ndt, const double &vel, const double &steer)
   {
-    /* estimate wheelbase by Recursive Least Squares Method */
+    /* estimate angular velocity correction coeff by Recursive Least Squares Method */
     const double zn = vel * std::tan(steer) / wheelbase_;
-    const double num = rho_ + zn * Pn_ * zn;
-    Pn_ = (Pn_ - (Pn_ * zn * zn * Pn_) / num) / rho_; // update estimate variance
-    const double temp = adaptive_coefficient_ + (Pn_ * zn / num) * (w_ndt - zn * adaptive_coefficient_);
-    adaptive_coefficient_ = std::max(std::min(temp, 1.2), 0.8); // limit changes
+    const double num = rho_ + zn * Pn_wz_ * zn;
+    Pn_wz_ = (Pn_wz_ - (Pn_wz_ * zn * zn * Pn_wz_) / num) / rho_; // update estimate variance
+    const double temp = adaptive_coefficient_wz_ + (Pn_wz_ * zn / num) * (w_ndt - zn * adaptive_coefficient_wz_);
+    adaptive_coefficient_wz_ = std::max(std::min(temp, 1.5), 0.5); // limit changes
+  }
 
-    printf("adaptive_coefficient_ = %f, Pn = %f\n", adaptive_coefficient_, Pn_);
+    void updateAdaptiveCoeffVel(const double &v_ndt, const double &vel)
+  {
+    /* estimate velocity correction coeff by Recursive Least Squares Method */
+    const double zn = vel;
+    const double num = rho_ + zn * Pn_vx_ * zn;
+    Pn_vx_ = (Pn_vx_ - (Pn_vx_ * zn * zn * Pn_vx_) / num) / rho_; // update estimate variance
+    const double temp = adaptive_coefficient_vx_ + (Pn_vx_ * zn / num) * (v_ndt - zn * adaptive_coefficient_vx_);
+    adaptive_coefficient_vx_ = std::max(std::min(temp, 1.5), 0.5); // limit changes
   }
 };
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "twist_generator");
-  TwistGenerator obj;
+  VehicleStatusConverter obj;
   ros::spin();
   return 0;
 };
