04ba46e888dbf9ae7fe055f7f8055020ca277dc8
amc-nu <abrahammonrroy@yahoo.com>
2019-05-21 09:24:31 +0900

    Switch CMake CUDA detection to use a single var
    Signed-off-by: amc-nu <abrahammonrroy@yahoo.com>

--- ros/src/common/cmake/autoware_build_flags/cmake/autoware_build_flags-extras.cmake
+++ ros/src/common/cmake/autoware_build_flags/cmake/autoware_build_flags-extras.cmake
@@ -11,22 +11,22 @@ else()
   set(CMAKE_CXX_STANDARD 11)
 endif()
 
-find_package(CUDA QUIET)
-find_package(Eigen3 QUIET)
-
 option(ENABLE_CUDA "Enable_CUDA" FALSE)
 
-####### Melodic Checks
-if (ENABLE_CUDA AND CUDA_FOUND AND "$ENV{ROS_DISTRO}" STREQUAL "melodic" )
-  if(${CUDA_VERSION} VERSION_GREATER "9.1" AND ${CMAKE_VERSION} VERSION_LESS "3.12.3")
+if (ENABLE_CUDA)
+  find_package(CUDA REQUIRED)
+  find_package(Eigen3 REQUIRED)
+
+  if(${CUDA_VERSION} VERSION_GREATER "9.1"
+        AND ${CMAKE_VERSION} VERSION_LESS "3.12.3")
     unset(CUDA_cublas_device_LIBRARY CACHE)
     set(CUDA_cublas_device_LIBRARY ${CUDA_cublas_LIBRARY})
     set(CUDA_CUBLAS_LIBRARIES ${CUDA_cublas_LIBRARY})
   endif()
-  if (${EIGEN3_VERSION_STRING} VERSION_LESS "3.3.7")
-      message(FATAL_ERROR "GPU support on Melodic requires Eigen version>= 3.3.7")
+  if ("$ENV{ROS_DISTRO}" STREQUAL "melodic" AND ${EIGEN3_VERSION_STRING} VERSION_LESS "3.3.7")
+    message(FATAL_ERROR "GPU support on Melodic requires Eigen version>= 3.3.7")
   endif()
+
 else()
   message(WARNING "CUDA support is disabled. Use -DENABLE_CUDA=TRUE and install Eigen >= 3.3.7 to enable it")
 endif()
-####### End Melodic Checks
