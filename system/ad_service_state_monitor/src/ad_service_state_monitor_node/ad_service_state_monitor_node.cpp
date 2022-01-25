50f6888f249bf22f82f2497a19ec0b002da9ceed
Hiroki OTA <hiroki.ota@tier4.jp>
2022-01-25 15:24:11 +0900

    fix(ad_service_state_monitor): rad2deg conversion (#252)
    Signed-off-by: h-ohta <hiroki.ota@tier4.jp>

--- system/ad_service_state_monitor/src/ad_service_state_monitor_node/ad_service_state_monitor_node.cpp
+++ system/ad_service_state_monitor/src/ad_service_state_monitor_node/ad_service_state_monitor_node.cpp
@@ -398,8 +398,8 @@ AutowareStateMonitorNode::AutowareStateMonitorNode()
 
   // Parameter for StateMachine
   state_param_.th_arrived_distance_m = this->declare_parameter("th_arrived_distance_m", 1.0);
-  state_param_.th_arrived_angle =
-    this->declare_parameter("th_arrived_angle_deg", tier4_autoware_utils::deg2rad(45.0));
+  const auto th_arrived_angle_deg = this->declare_parameter("th_arrived_angle_deg", 45.0);
+  state_param_.th_arrived_angle = tier4_autoware_utils::deg2rad(th_arrived_angle_deg);
   state_param_.th_stopped_time_sec = this->declare_parameter("th_stopped_time_sec", 1.0);
   state_param_.th_stopped_velocity_mps = this->declare_parameter("th_stopped_velocity_mps", 0.01);
 
