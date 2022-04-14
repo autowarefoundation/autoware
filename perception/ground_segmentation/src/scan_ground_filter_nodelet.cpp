f68121e5487037de857343d3924b249e5afcfde2
Shunsuke Miura <37187849+miursh@users.noreply.github.com>
2022-04-14 19:39:12 +0900

    feat(scan_ground_filter): change launch option and threshold (#670)
    * Add care for near but high points
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * ci(pre-commit): autofix
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- perception/ground_segmentation/src/scan_ground_filter_nodelet.cpp
+++ perception/ground_segmentation/src/scan_ground_filter_nodelet.cpp
@@ -178,6 +178,10 @@ void ScanGroundFilterComponent::classifyPointCloud(
       } else {
         calculate_slope = true;
       }
+      if (is_point_close_to_prev) {
+        height_from_gnd = p->orig_point->z - ground_cluster.getAverageHeight();
+        radius_distance_from_gnd = p->radius - ground_cluster.getAverageRadius();
+      }
       if (calculate_slope) {
         // far from the previous point
 
