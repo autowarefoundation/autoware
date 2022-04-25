35707654451e952e42e45d8879aa1836c0d488e8
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2022-04-25 09:33:02 +0900

    fix(multi_object_tracker): modify build error in rolling (#785)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- perception/multi_object_tracker/src/tracker/model/unknown_tracker.cpp
+++ perception/multi_object_tracker/src/tracker/model/unknown_tracker.cpp
@@ -16,7 +16,13 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
+
+#ifdef USE_TF2_GEOMETRY_MSGS_DEPRECATED_HEADER
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#endif
+
 #define EIGEN_MPL2_ONLY
 #include "multi_object_tracker/tracker/model/unknown_tracker.hpp"
 #include "multi_object_tracker/utils/utils.hpp"
