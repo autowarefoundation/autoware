da61e26494803a6396ccbbc0610b686e9c44f562
Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>
2022-09-01 17:09:40 +0900

    fix(mission_planner): fix function name renaming (#1760)
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- planning/mission_planner/src/mission_planner_lanelet2/utility_functions.cpp
+++ planning/mission_planner/src/mission_planner_lanelet2/utility_functions.cpp
@@ -28,14 +28,14 @@ bool exists(const std::unordered_set<lanelet::Id> & set, const lanelet::Id & id)
   return set.find(id) != set.end();
 }
 
-std::string toString(const geometry_msgs::msg::Pose & pose)
+std::string to_string(const geometry_msgs::msg::Pose & pose)
 {
   std::stringstream ss;
   ss << "(" << pose.position.x << ", " << pose.position.y << "," << pose.position.z << ")";
   return ss.str();
 }
 
-void setColor(std_msgs::msg::ColorRGBA * cl, double r, double g, double b, double a)
+void set_color(std_msgs::msg::ColorRGBA * cl, double r, double g, double b, double a)
 {
   cl->r = r;
   cl->g = g;
@@ -43,7 +43,7 @@ void setColor(std_msgs::msg::ColorRGBA * cl, double r, double g, double b, doubl
   cl->a = a;
 }
 
-void insertMarkerArray(
+void insert_marker_array(
   visualization_msgs::msg::MarkerArray * a1, const visualization_msgs::msg::MarkerArray & a2)
 {
   a1->markers.insert(a1->markers.end(), a2.markers.begin(), a2.markers.end());
