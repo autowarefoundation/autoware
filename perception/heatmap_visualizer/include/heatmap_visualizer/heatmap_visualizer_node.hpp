57c4b1fbc95748d59f0f77b1ac84a2c976f17fb8
Tomoya Kimura <tomoya.kimura@tier4.jp>
2022-09-05 19:41:21 +0900

    fix(heatmap_visualizer): add unknown class (#1781)
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>
    
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- perception/heatmap_visualizer/include/heatmap_visualizer/heatmap_visualizer_node.hpp
+++ perception/heatmap_visualizer/include/heatmap_visualizer/heatmap_visualizer_node.hpp
@@ -66,8 +66,8 @@ private:
   float map_length_;
   float map_resolution_;
   bool use_confidence_;
-  std::vector<std::string> class_names_{"CAR",     "TRUCK",     "BUS",       "TRAILER",
-                                        "BICYCLE", "MOTORBIKE", "PEDESTRIAN"};
+  std::vector<std::string> class_names_{"UNKNWON", "CAR",     "TRUCK",     "BUS",
+                                        "TRAILER", "BICYCLE", "MOTORBIKE", "PEDESTRIAN"};
   bool rename_car_to_truck_and_bus_;
 
   // Number of width and height cells
