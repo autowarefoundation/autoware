99f43ce90ab01773cba073625afe001f49e966f1
Yutaka Shimizu <43805014+purewater0901@users.noreply.github.com>
2022-09-12 22:22:38 +0900

    feat(motion_utils): add threshold (#1839)
    * feat(motion_utils): add threshold
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    * change to constants.hpp
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    * delete environment
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- common/motion_utils/include/motion_utils/constants.hpp
+++ common/motion_utils/include/motion_utils/constants.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Tier IV, Inc.
+// Copyright 2022 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef MOTION_UTILS__MOTION_UTILS_HPP_
-#define MOTION_UTILS__MOTION_UTILS_HPP_
+#ifndef MOTION_UTILS__CONSTANTS_HPP_
+#define MOTION_UTILS__CONSTANTS_HPP_
 
-#include "motion_utils/constants.hpp"
-#include "motion_utils/marker/marker_helper.hpp"
-#include "motion_utils/resample/resample.hpp"
-#include "motion_utils/trajectory/interpolation.hpp"
-#include "motion_utils/trajectory/path_with_lane_id.hpp"
-#include "motion_utils/trajectory/tmp_conversion.hpp"
-#include "motion_utils/trajectory/trajectory.hpp"
-#include "motion_utils/vehicle/vehicle_state_checker.hpp"
+namespace motion_utils
+{
+constexpr double overlap_threshold = 0.1;
+}  // namespace motion_utils
 
-#endif  // MOTION_UTILS__MOTION_UTILS_HPP_
+#endif  // MOTION_UTILS__CONSTANTS_HPP_
