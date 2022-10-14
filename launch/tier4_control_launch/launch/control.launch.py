fd145e2bfae1ad6824a5357db9b16ae607a8a56a
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2022-10-14 11:03:29 +0900

    feat(control_launch): add longitudinal controller mode (#2062)
    feature(control_launch): add longitudinal controller mode
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- launch/tier4_control_launch/launch/control.launch.py
+++ launch/tier4_control_launch/launch/control.launch.py
@@ -306,6 +306,13 @@ def generate_launch_description():
         "lateral controller mode: `mpc_follower` or `pure_pursuit`",
     )
 
+    # longitudinal controller mode
+    add_launch_arg(
+        "longitudinal_controller_mode",
+        "pid",
+        "longitudinal controller mode: `pid`",
+    )
+
     add_launch_arg(
         "vehicle_info_param_file",
         [
