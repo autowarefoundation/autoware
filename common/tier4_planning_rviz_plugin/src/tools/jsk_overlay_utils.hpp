fbf5b1cc21685dcc9bda2b9b5443afcfa9dd58db
Takayuki Murooka <takayuki5168@gmail.com>
2022-10-14 10:32:44 +0900

    fix(tier4_planning/vehicle_rviz_plugin): fixed license (#2059)
    * fix(tier4_planning/vehicle_rviz_plugin): fixed license
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix build error
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- common/tier4_planning_rviz_plugin/src/tools/jsk_overlay_utils.hpp
+++ common/tier4_planning_rviz_plugin/src/tools/jsk_overlay_utils.hpp
@@ -1,7 +1,21 @@
+// Copyright 2022 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Copyright (c) 2014, JSK Lab
 // All rights reserved.
 //
-// Software License Agreement (BSD License 2.0)
+// Software License Agreement (BSD License)
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
