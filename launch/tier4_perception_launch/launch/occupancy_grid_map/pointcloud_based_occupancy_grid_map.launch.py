41f47be4004bcf027d8c1188a6dbe7d8370f3287
Kaan Çolak <kaancolak95@gmail.com>
2022-10-25 11:03:02 +0300

    fix(tier4_perception_launch): fix missing container argument (#2087)
    * fix(tier4_perception_launch): fix missing container argument
    
    Signed-off-by: Kaan Colak <kcolak@leodrive.ai>
    
    * fix(tier4_perception_launch): rm unused param
    
    Signed-off-by: Kaan Colak <kcolak@leodrive.ai>
    
    Signed-off-by: Kaan Colak <kcolak@leodrive.ai>

--- launch/tier4_perception_launch/launch/occupancy_grid_map/pointcloud_based_occupancy_grid_map.launch.py
+++ launch/tier4_perception_launch/launch/occupancy_grid_map/pointcloud_based_occupancy_grid_map.launch.py
@@ -16,8 +16,6 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
-from launch.conditions import LaunchConfigurationEquals
-from launch.conditions import LaunchConfigurationNotEquals
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
@@ -62,26 +60,27 @@ def generate_launch_description():
     ]
 
     occupancy_grid_map_container = ComposableNodeContainer(
-        condition=LaunchConfigurationEquals("container", ""),
-        name="occupancy_grid_map_container",
+        name=LaunchConfiguration("container_name"),
         namespace="",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
         composable_node_descriptions=composable_nodes,
+        condition=UnlessCondition(LaunchConfiguration("use_pointcloud_container")),
         output="screen",
     )
 
     load_composable_nodes = LoadComposableNodes(
-        condition=LaunchConfigurationNotEquals("container", ""),
         composable_node_descriptions=composable_nodes,
-        target_container=LaunchConfiguration("container"),
+        target_container=LaunchConfiguration("container_name"),
+        condition=IfCondition(LaunchConfiguration("use_pointcloud_container")),
     )
 
     return LaunchDescription(
         [
-            add_launch_arg("container", ""),
-            add_launch_arg("use_multithread", "false"),
-            add_launch_arg("use_intra_process", "false"),
+            add_launch_arg("use_multithread", "False"),
+            add_launch_arg("use_intra_process", "True"),
+            add_launch_arg("use_pointcloud_container", "False"),
+            add_launch_arg("container_name", "occupancy_grid_map_container"),
             add_launch_arg("input/obstacle_pointcloud", "no_ground/oneshot/pointcloud"),
             add_launch_arg("input/raw_pointcloud", "concatenated/pointcloud"),
             add_launch_arg("output", "occupancy_grid"),
