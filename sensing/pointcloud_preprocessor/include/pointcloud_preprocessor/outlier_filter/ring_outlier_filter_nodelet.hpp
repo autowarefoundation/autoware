12686e595573e2fee82567b9ee40290eef5a846a
M. Fatih Cırıt <xmfcx@users.noreply.github.com>
2022-11-21 23:45:54 +0300

    fix(pointcloud_preprocessor): remove unused definition (#2342)
    Signed-off-by: M. Fatih Cırıt <mfc@leodrive.ai>

--- sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/outlier_filter/ring_outlier_filter_nodelet.hpp
+++ sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/outlier_filter/ring_outlier_filter_nodelet.hpp
@@ -25,7 +25,6 @@
 namespace pointcloud_preprocessor
 {
 using autoware_point_types::PointXYZI;
-using autoware_point_types::PointXYZIRADRT;
 using point_cloud_msg_wrapper::PointCloud2Modifier;
 
 class RingOutlierFilterComponent : public pointcloud_preprocessor::Filter
