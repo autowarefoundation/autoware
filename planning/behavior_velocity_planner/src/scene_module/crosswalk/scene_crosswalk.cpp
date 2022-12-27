175e23d6e782ca1037f36d6b0f66adb2267e3f5b
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2022-12-27 17:51:45 +0900

    fix(crosswalk): fix base path for stop point caluculation (#2584)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_velocity_planner/src/scene_module/crosswalk/scene_crosswalk.cpp
+++ planning/behavior_velocity_planner/src/scene_module/crosswalk/scene_crosswalk.cpp
@@ -446,7 +446,7 @@ boost::optional<geometry_msgs::msg::Point> CrosswalkModule::findNearestStopPoint
   const auto stop_line_distance = exist_stopline_in_map ? 0.0 : planner_param_.stop_line_distance;
   const auto margin = stop_at_stop_line ? stop_line_distance + base_link2front
                                         : planner_param_.stop_margin + base_link2front;
-  const auto stop_pose = calcLongitudinalOffsetPose(sparse_resample_path.points, p_stop, -margin);
+  const auto stop_pose = calcLongitudinalOffsetPose(ego_path.points, p_stop, -margin);
 
   if (!stop_pose) {
     return {};
