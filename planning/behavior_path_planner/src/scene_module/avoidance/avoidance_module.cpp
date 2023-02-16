548477729129d7aac211720e0b204b79cf94f2c3
Takayuki Murooka <takayuki5168@gmail.com>
2023-02-17 01:33:38 +0900

    feat(behavior_path_planner): unshifted pose as closest previous path (#2877)
    * feat(motion_utils): add calc interpolated point for path with lane id
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * format
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * feat(behavior_path_planner): unshifted pose as closest previous path
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * minor fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * format
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    ---------
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -3199,8 +3199,9 @@ Pose AvoidanceModule::getUnshiftedEgoPose(const ShiftedPath & prev_path) const
   // un-shifted fot current ideal pose
   const auto closest = findNearestIndex(prev_path.path.points, ego_pose.position);
 
-  Pose unshifted_pose = ego_pose;
-  unshifted_pose.orientation = prev_path.path.points.at(closest).point.pose.orientation;
+  // NOTE: Considering avoidance by motion, we set unshifted_pose as previous path instead of
+  // ego_pose.
+  Pose unshifted_pose = motion_utils::calcInterpolatedPoint(prev_path.path, ego_pose).point.pose;
 
   util::shiftPose(&unshifted_pose, -prev_path.shift_length.at(closest));
   unshifted_pose.orientation = ego_pose.orientation;
