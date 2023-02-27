cf0e53b2b9ca5582a11c4b05b8e1c6d1589e66c7
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2023-02-27 15:32:26 +0900

    fix(setup): install pip in the setup script (#3290)
    fix: pip install ansible
    
    Signed-off-by: taikitanaka3 <taiki.tanaka@tier4.jp>

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -113,10 +113,15 @@ if ! (command -v git >/dev/null 2>&1); then
     sudo apt-get -y install git
 fi
 
+# Install pip for ansible
+if ! (python3 -m pip --version >/dev/null 2>&1); then
+    sudo apt-get -y update
+    sudo apt-get -y install python3-pip python3-venv
+fi
+
 # Install pipx for ansible
 if ! (python3 -m pipx --version >/dev/null 2>&1); then
     sudo apt-get -y update
-    sudo apt-get -y install python3-pip python3-venv
     python3 -m pip install --user pipx
 fi
 
