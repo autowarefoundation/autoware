eaa39d4b0fe296c7c42c25c41a3cbd613c0e4c6b
Yutaka Shimizu <43805014+purewater0901@users.noreply.github.com>
2023-04-06 20:00:28 +0900

    feat(behavior_path_planner): clean convert function (#3296)
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- planning/behavior_path_planner/src/utilities.cpp
+++ planning/behavior_path_planner/src/utilities.cpp
@@ -145,12 +145,8 @@ PredictedPath convertToPredictedPath(
   const double initial_velocity = std::abs(vehicle_twist.linear.x);
   const double lane_change_velocity = std::max(initial_velocity + acceleration * prepare_time, 0.0);
 
-  // first point
-  predicted_path.path.push_back(
-    motion_utils::calcInterpolatedPose(path.points, vehicle_pose_frenet.length));
-
   // prepare segment
-  for (double t = resolution; t < prepare_time; t += resolution) {
+  for (double t = 0.0; t < prepare_time; t += resolution) {
     const double length = initial_velocity * t + 0.5 * acceleration * t * t;
     predicted_path.path.push_back(
       motion_utils::calcInterpolatedPose(path.points, vehicle_pose_frenet.length + length));
