57b64a64e12b7c1063cbfd762d5cf79bec883e4d
Naophis <naoto.hiramatsu@tier4.jp>
2023-04-17 14:55:13 +0900

    bug fix

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR
 cd ../
 WORK_SCAPE=$(pwd)
 source ./install/setup.bash
+MAP_DIR=$(pwd)/assets
 
 ros2 launch autoware_launch autoware.launch.xml vehicle_model:=logiee-s-tc sensor_model:=logiee-s-tc_sensor_kit map_path:=/PATH/TO/YOUR/MAP
 
