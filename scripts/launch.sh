25bb124e3c800453793669839508a7572ca22a2e
Naophis <naoto.hiramatsu@tier4.jp>
2023-04-17 13:18:35 +0900

    update launch shell

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,7 +2,10 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 HOME_DIR=$(pwd)
 
 cd $SCRIPT_DIR
+cd ../
+WORK_SCAPE=$(pwd)
+source ./install/setup.bash
 
-ros2 launch autoware_launch autoware.launch.xml vehicle_model:=logiee-s-tc sensor_kit:=logiee-s-tc_sensor_kit_launch map_path:=/PATH/TO/YOUR/MAP
+ros2 launch autoware_launch autoware.launch.xml vehicle_model:=logiee-s-tc sensor_model:=logiee-s-tc_sensor_kit map_path:=/PATH/TO/YOUR/MAP
 
 cd $HOME_DIR
