4af956bdcfe8b0851ce6570679334257b0197383
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-04-24 07:48:07 +0900

    perf(avoidance): remove redundant calculation process (#3495)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -101,11 +101,10 @@ bool AvoidanceModule::isExecutionRequested() const
     return true;
   }
 
-  // Check avoidance targets exist
-  auto avoid_data = calcAvoidancePlanningData(debug_data_);
-
   // Check ego is in preferred lane
 #ifdef USE_OLD_ARCHITECTURE
+  const auto avoid_data = calcAvoidancePlanningData(debug_data_);
+
   const auto current_lanes = utils::getCurrentLanes(planner_data_);
   lanelet::ConstLanelet current_lane;
   lanelet::utils::query::getClosestLanelet(
@@ -116,7 +115,7 @@ bool AvoidanceModule::isExecutionRequested() const
     return false;
   }
 #else
-  fillShiftLine(avoid_data, debug_data_);
+  const auto avoid_data = avoidance_data_;
 #endif
 
   if (parameters_->publish_debug_marker) {
@@ -140,16 +139,6 @@ bool AvoidanceModule::isExecutionRequested() const
 bool AvoidanceModule::isExecutionReady() const
 {
   DEBUG_PRINT("AVOIDANCE isExecutionReady");
-
-  {
-    DebugData debug;
-    static_cast<void>(calcAvoidancePlanningData(debug));
-  }
-
-  if (current_state_ == ModuleStatus::RUNNING) {
-    return true;
-  }
-
   return true;
 }
 
