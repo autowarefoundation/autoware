463d4e2e3f5531181ebb04483558227e9790ff04
Fumiya Watanabe <rej55.g@gmail.com>
2023-06-13 14:22:49 +0900

    fix(behavior_path_planner): unable to fix bound (#3942)
    Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

--- planning/behavior_path_planner/src/utils/utils.cpp
+++ planning/behavior_path_planner/src/utils/utils.cpp
@@ -1510,8 +1510,11 @@ void generateDrivableArea(
   }
 
   // make bound longitudinally monotonic
-  makeBoundLongitudinallyMonotonic(path, true);   // for left bound
-  makeBoundLongitudinallyMonotonic(path, false);  // for right bound
+  // TODO(Murooka) Fix makeBoundLongitudinallyMonotonic
+  if (enable_expanding_polygon) {
+    makeBoundLongitudinallyMonotonic(path, true);   // for left bound
+    makeBoundLongitudinallyMonotonic(path, false);  // for right bound
+  }
 }
 
 // calculate bounds from drivable lanes and hatched road markings
