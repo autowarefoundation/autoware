a9cb76c3f6089138fe68cb08070d3f2846d11958
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-06-22 12:52:04 +0900

    chore(mission_planner): fix route marker (#4032)
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- planning/mission_planner/src/lanelet2_plugins/default_planner.cpp
+++ planning/mission_planner/src/lanelet2_plugins/default_planner.cpp
@@ -204,7 +204,7 @@ PlannerPlugin::MarkerArray DefaultPlanner::visualize(const LaneletRoute & route)
   std_msgs::msg::ColorRGBA cl_end;
   std_msgs::msg::ColorRGBA cl_normal;
   std_msgs::msg::ColorRGBA cl_goal;
-  set_color(&cl_route, 0.2, 0.4, 0.2, 0.05);
+  set_color(&cl_route, 0.8, 0.99, 0.8, 0.15);
   set_color(&cl_goal, 0.2, 0.4, 0.4, 0.05);
   set_color(&cl_end, 0.2, 0.2, 0.4, 0.05);
   set_color(&cl_normal, 0.2, 0.4, 0.2, 0.05);
