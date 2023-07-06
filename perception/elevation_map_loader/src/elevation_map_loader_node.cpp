cde4c92bcb1ebb7f94d97d92a9ef65d72dea793c
Shintaro Tomie <58775300+Shin-kyoto@users.noreply.github.com>
2023-07-07 00:51:08 +0900

    feat(elevation_map_loader): add error handling for std::runtime_error (#4187)
    * feat(elevation_map_loader): Add error handling for std::runtime_error
    
    Signed-off-by: Shin-kyoto <58775300+Shin-kyoto@users.noreply.github.com>
    
    * feat(elevation_map_loader): add error message output
    
    Signed-off-by: Shin-kyoto <58775300+Shin-kyoto@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Shin-kyoto <58775300+Shin-kyoto@users.noreply.github.com>

--- perception/elevation_map_loader/src/elevation_map_loader_node.cpp
+++ perception/elevation_map_loader/src/elevation_map_loader_node.cpp
@@ -141,7 +141,8 @@ void ElevationMapLoaderNode::publish()
     try {
       is_bag_loaded = grid_map::GridMapRosConverter::loadFromBag(
         *data_manager_.elevation_map_path_, "elevation_map", elevation_map_);
-    } catch (rosbag2_storage_plugins::SqliteException & e) {
+    } catch (const std::runtime_error & e) {
+      RCLCPP_ERROR(this->get_logger(), e.what());
       is_bag_loaded = false;
     }
     if (!is_bag_loaded) {
