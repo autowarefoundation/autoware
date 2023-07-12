0ef26b89a0ea2a5442801b0636fcf8a4ca28eac0
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-07-12 13:19:26 +0900

    fix(avoidance_by_lc): fix execution requesting logic (#4245)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/lane_change/avoidance_by_lane_change.cpp
+++ planning/behavior_path_planner/src/scene_module/lane_change/avoidance_by_lane_change.cpp
@@ -55,8 +55,7 @@ std::pair<bool, bool> AvoidanceByLaneChange::getSafePath(LaneChangePath & safe_p
     return {false, false};
   }
 
-  const auto direction = utils::avoidance::isOnRight(o_front) ? Direction::LEFT : Direction::RIGHT;
-  const auto target_lanes = getLaneChangeLanes(current_lanes, direction);
+  const auto target_lanes = getLaneChangeLanes(current_lanes, direction_);
 
   if (target_lanes.empty()) {
     return {false, false};
@@ -65,7 +64,7 @@ std::pair<bool, bool> AvoidanceByLaneChange::getSafePath(LaneChangePath & safe_p
   // find candidate paths
   LaneChangePaths valid_paths{};
   const auto found_safe_path =
-    getLaneChangePaths(current_lanes, target_lanes, direction, &valid_paths);
+    getLaneChangePaths(current_lanes, target_lanes, direction_, &valid_paths);
 
   if (valid_paths.empty()) {
     return {false, false};
@@ -95,6 +94,14 @@ void AvoidanceByLaneChange::updateSpecialData()
 
   avoidance_debug_data_ = DebugData();
   avoidance_data_ = calcAvoidancePlanningData(avoidance_debug_data_);
+
+  if (avoidance_data_.target_objects.empty()) {
+    direction_ = Direction::NONE;
+  } else {
+    direction_ = utils::avoidance::isOnRight(avoidance_data_.target_objects.front())
+                   ? Direction::LEFT
+                   : Direction::RIGHT;
+  }
 
   utils::avoidance::updateRegisteredObject(registered_objects_, avoidance_data_.target_objects, p);
   utils::avoidance::compensateDetectionLost(
