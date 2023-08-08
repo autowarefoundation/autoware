e10db1371e32cfd2ba1f12a289e2af40679e47c3
Yutaka Shimizu <43805014+purewater0901@users.noreply.github.com>
2023-08-08 23:12:26 +0900

    fix(lane_change): add distance check for the lane change acceleration sampling (#4556)
    * fix(lane_change): add distance checker in acceleration sampling
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    * fix format
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    * update
    
    Signed-off-by: yutaka <purewater0901@gmail.com>
    
    ---------
    
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- planning/behavior_path_planner/src/scene_module/lane_change/normal.cpp
+++ planning/behavior_path_planner/src/scene_module/lane_change/normal.cpp
@@ -540,6 +540,11 @@ std::vector<double> NormalLaneChange::sampleLongitudinalAccValues(
     current_velocity, common_parameters,
     route_handler.getLateralIntervalsToPreferredLane(current_lanes.back()), max_acc);
 
+  if (max_lane_change_length > utils::getDistanceToEndOfLane(current_pose, current_lanes)) {
+    return utils::lane_change::getAccelerationValues(
+      min_acc, max_acc, longitudinal_acc_sampling_num);
+  }
+
   // if maximum lane change length is less than length to goal or the end of target lanes, only
   // sample max acc
   if (route_handler.isInGoalRouteSection(target_lanes.back())) {
