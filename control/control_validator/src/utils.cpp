3a370d4e648b1baeeb83299ea2bef581cb405d9b
keisuke <31088744+keiota@users.noreply.github.com>
2023-09-03 23:23:15 +0900

    fix(control_validator): resolve the bug causing inappropriate diagnostic messages. (#4846)
    * fix(control_validator): fix reverseTrajectoryPoints to get correct trajectory array
    
    Signed-off-by: keiota <k3robot@gmail.com>
    
    * fix(control_validator): fix line breaks
    
    Signed-off-by: keiota <k3robot@gmail.com>
    
    * style(pre-commit): autofix
    
    * Update control/control_validator/src/utils.cpp
    
    ---------
    
    Signed-off-by: keiota <k3robot@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kyoichi Sugahara <kyoichi.sugahara@tier4.jp>

--- control/control_validator/src/utils.cpp
+++ control/control_validator/src/utils.cpp
@@ -44,7 +44,8 @@ void insertPointInPredictedTrajectory(
 
 TrajectoryPoints reverseTrajectoryPoints(const TrajectoryPoints & trajectory_points)
 {
-  TrajectoryPoints reversed_trajectory_points(trajectory_points.size());
+  TrajectoryPoints reversed_trajectory_points;
+  reversed_trajectory_points.reserve(trajectory_points.size());
   std::reverse_copy(
     trajectory_points.begin(), trajectory_points.end(),
     std::back_inserter(reversed_trajectory_points));
@@ -106,7 +107,7 @@ Trajectory alignTrajectoryWithReferenceTrajectory(
   // predicted_trajectory:   　　　　p1-----p2-----p3----//------pN
   // trajectory:                               t1--------//------tN
   // ↓
-  // predicted_trajectory:   　　　　        tNew--p3----//------pN
+  // predicted_trajectory:   　　　　        pNew--p3----//------pN
   // trajectory:                               t1--------//------tN
   auto predicted_trajectory_point_removed = removeFrontTrajectoryPoint(
     trajectory_points, modified_trajectory_points, predicted_trajectory_points);
