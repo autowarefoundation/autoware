cdc568db1e4d3180f2c7c9c8c1cca78e7a8374dc
Muhammed Yavuz Köseoğlu <koseoglumu18@itu.edu.tr>
2023-09-11 14:24:02 +0300

    feat(ndt_scan_matcher): Adding state switch mecahism for nearest_voxel_transformation_likelihood: For deciding does NDT have problems while matching scans (#4854)
    * exe time added to diagnostics and switch state by voxel likelihood
    
    * style(pre-commit): autofix
    
    * type casting corrected
    
    * ndt score condition parameterized
    
    * remove exe_time from diagnostic
    
    * style(pre-commit): autofix
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
+++ localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
@@ -291,6 +291,13 @@ void NDTScanMatcher::timer_diagnostic()
       diag_status_msg.level = diagnostic_msgs::msg::DiagnosticStatus::ERROR;
       diag_status_msg.message += "skipping_publish_num exceed limit. ";
     }
+    if (
+      state_ptr_->count("nearest_voxel_transformation_likelihood") &&
+      std::stod((*state_ptr_)["nearest_voxel_transformation_likelihood"]) <
+        converged_param_nearest_voxel_transformation_likelihood_) {
+      diag_status_msg.level = diagnostic_msgs::msg::DiagnosticStatus::WARN;
+      diag_status_msg.message += "NDT score is unreliably low. ";
+    }
     // Ignore local optimal solution
     if (
       state_ptr_->count("is_local_optimal_solution_oscillation") &&
