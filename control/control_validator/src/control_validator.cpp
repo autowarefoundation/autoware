3e28ab51de1dfd3a2a8fe91bb7f9a5d771326b19
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-09-12 18:02:35 +0900

    chore(control_validator): use throttle for the error log (#4960)
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- control/control_validator/src/control_validator.cpp
+++ control/control_validator/src/control_validator.cpp
@@ -163,7 +163,9 @@ void ControlValidator::publishDebugInfo()
 void ControlValidator::validate(const Trajectory & predicted_trajectory)
 {
   if (predicted_trajectory.points.size() < 2) {
-    RCLCPP_ERROR(get_logger(), "predicted_trajectory size is less than 2. Cannot validate.");
+    RCLCPP_ERROR_THROTTLE(
+      get_logger(), *get_clock(), 1000,
+      "predicted_trajectory size is less than 2. Cannot validate.");
     return;
   }
 
