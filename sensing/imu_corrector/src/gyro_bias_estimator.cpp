e2123a2146f0ba44be65b3604a26b5d3f2c60699
kminoda <44218668+kminoda@users.noreply.github.com>
2023-09-25 12:12:21 +0900

    feat(imu_corrector): add gyro_bias estimation in diag (#5054)
    * feat(imu_corrector): add gyro_bias estimation in diag
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * update
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    ---------
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- sensing/imu_corrector/src/gyro_bias_estimator.cpp
+++ sensing/imu_corrector/src/gyro_bias_estimator.cpp
@@ -83,6 +83,14 @@ void GyroBiasEstimator::update_diagnostics(diagnostic_updater::DiagnosticStatusW
     stat.add("gyro_bias", "Bias estimation is not yet ready because of insufficient data.");
     stat.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "Not initialized");
   } else {
+    stat.add("gyro_bias_x_for_imu_corrector", gyro_bias_.value().x);
+    stat.add("gyro_bias_y_for_imu_corrector", gyro_bias_.value().y);
+    stat.add("gyro_bias_z_for_imu_corrector", gyro_bias_.value().z);
+
+    stat.add("estimated_gyro_bias_x", gyro_bias_.value().x - angular_velocity_offset_x_);
+    stat.add("estimated_gyro_bias_y", gyro_bias_.value().y - angular_velocity_offset_y_);
+    stat.add("estimated_gyro_bias_z", gyro_bias_.value().z - angular_velocity_offset_z_);
+
     // Validation
     const bool is_bias_small_enough =
       std::abs(gyro_bias_.value().x - angular_velocity_offset_x_) < gyro_bias_threshold_ &&
