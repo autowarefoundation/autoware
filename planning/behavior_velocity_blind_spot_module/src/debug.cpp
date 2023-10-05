877b04b133175fc39444a9400cd83ae8f22de5b4
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-10-05 15:51:15 +0900

    fix(blind_spot): fix runtime crash (#5227)
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_blind_spot_module/src/debug.cpp
+++ planning/behavior_velocity_blind_spot_module/src/debug.cpp
@@ -19,6 +19,8 @@
 #include <motion_utils/marker/virtual_wall_marker_creator.hpp>
 #include <tier4_autoware_utils/ros/marker_helper.hpp>
 
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
 #include <tf2/utils.h>
 
 #include <string>
