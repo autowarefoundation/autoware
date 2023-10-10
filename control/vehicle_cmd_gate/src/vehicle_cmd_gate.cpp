9ddb07ae06c269d8cf007bab8fe402c24de54223
kminoda <44218668+kminoda@users.noreply.github.com>
2023-10-10 14:06:17 +0900

    fix(vehicle_cmd_gate): fix filtering condition (#5250)
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- control/vehicle_cmd_gate/src/vehicle_cmd_gate.cpp
+++ control/vehicle_cmd_gate/src/vehicle_cmd_gate.cpp
@@ -555,9 +555,7 @@ AckermannControlCommand VehicleCmdGate::filterControlCommand(const AckermannCont
   // set prev value for both to keep consistency over switching:
   // Actual steer, vel, acc should be considered in manual mode to prevent sudden motion when
   // switching from manual to autonomous
-  const auto in_autonomous =
-    (mode.mode == OperationModeState::AUTONOMOUS && mode.is_autoware_control_enabled);
-  auto prev_values = in_autonomous ? out : current_status_cmd;
+  auto prev_values = mode.is_autoware_control_enabled ? out : current_status_cmd;
 
   if (ego_is_stopped) {
     prev_values.longitudinal = out.longitudinal;
