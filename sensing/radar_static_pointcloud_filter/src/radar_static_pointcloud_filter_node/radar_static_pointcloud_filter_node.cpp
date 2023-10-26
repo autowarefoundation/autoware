27f28d679e226f53a9ce3c7501a61dcab783bfa5
Satoshi Tanaka <16330533+scepter914@users.noreply.github.com>
2023-10-27 00:38:12 +0900

    chore(radar_static_pointcloud_filter): fix copyright (#5316)
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- sensing/radar_static_pointcloud_filter/src/radar_static_pointcloud_filter_node/radar_static_pointcloud_filter_node.cpp
+++ sensing/radar_static_pointcloud_filter/src/radar_static_pointcloud_filter_node/radar_static_pointcloud_filter_node.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 Foxconn, TIER IV, Inc.
+// Copyright 2022 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
