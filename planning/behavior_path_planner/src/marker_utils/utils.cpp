dc5d76afeaf4c513458877af46ef7673034ddbd4
Zulfaqar Azmi <93502286+zulfaqar-azmi-t4@users.noreply.github.com>
2023-11-14 16:33:19 +0900

    fix(lane_change): fix object debug marker not having point (#5577)
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/src/marker_utils/utils.cpp
+++ planning/behavior_path_planner/src/marker_utils/utils.cpp
@@ -18,6 +18,7 @@
 #include "behavior_path_planner/utils/path_safety_checker/path_safety_checker_parameters.hpp"
 #include "behavior_path_planner/utils/path_utils.hpp"
 
+#include <tier4_autoware_utils/geometry/boost_polygon_utils.hpp>
 #include <tier4_autoware_utils/ros/marker_helper.hpp>
 #include <tier4_autoware_utils/ros/uuid_helper.hpp>
 
@@ -45,6 +46,7 @@ CollisionCheckDebugPair createObjectDebug(const ExtendedPredictedObject & obj)
 {
   CollisionCheckDebug debug;
   debug.current_obj_pose = obj.initial_pose.pose;
+  debug.extended_obj_polygon = tier4_autoware_utils::toPolygon2d(obj.initial_pose.pose, obj.shape);
   debug.current_twist = obj.initial_twist.twist;
   return {tier4_autoware_utils::toHexString(obj.uuid), debug};
 }
