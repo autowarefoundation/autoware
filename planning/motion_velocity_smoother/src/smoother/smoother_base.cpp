e4e129ee5b4a44ee9d1c4010094d78dec7f09907
Vincent Richard <richard-v@macnica.co.jp>
2023-12-28 10:47:37 +0900

    fix(motion_velocity_smoother): front wheel steer rate calculation (#5965)
    #5964
    
    Signed-off-by: Vincent Richard <richard-v@macnica.co.jp>

--- planning/motion_velocity_smoother/src/smoother/smoother_base.cpp
+++ planning/motion_velocity_smoother/src/smoother/smoother_base.cpp
@@ -224,8 +224,8 @@ TrajectoryPoints SmootherBase::applySteeringRateLimit(
     auto & steer_back = output.at(i).front_wheel_angle_rad;
 
     // calculate the just 2 steering angle
-    steer_front = std::atan(base_param_.wheel_base * curvature_v.at(i));
-    steer_back = std::atan(base_param_.wheel_base * curvature_v.at(i + 1));
+    steer_front = std::atan(base_param_.wheel_base * curvature_v.at(i + 1));
+    steer_back = std::atan(base_param_.wheel_base * curvature_v.at(i));
 
     const auto mean_vel = 0.5 * (v_front + v_back);
     const auto dt = std::max(points_interval / mean_vel, std::numeric_limits<double>::epsilon());
