4784c3347f8454874adff44e204533a30ae2d926
Takayuki Murooka <takayuki5168@gmail.com>
2024-01-07 13:48:49 +0900

    fix(motion_utils): fix the right bound assignment in resamplePath (#6020)
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- common/motion_utils/src/resample/resample.cpp
+++ common/motion_utils/src/resample/resample.cpp
@@ -486,7 +486,7 @@ autoware_auto_planning_msgs::msg::Path resamplePath(
   autoware_auto_planning_msgs::msg::Path resampled_path;
   resampled_path.header = input_path.header;
   resampled_path.left_bound = input_path.left_bound;
-  resampled_path.right_bound = resampled_path.right_bound;
+  resampled_path.right_bound = input_path.right_bound;
   resampled_path.points.resize(interpolated_pose.size());
   for (size_t i = 0; i < resampled_path.points.size(); ++i) {
     autoware_auto_planning_msgs::msg::PathPoint path_point;
