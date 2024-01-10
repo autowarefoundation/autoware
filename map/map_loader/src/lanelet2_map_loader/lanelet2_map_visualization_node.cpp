27d866807e97daa7e3cbc6bd2854614913d5e591
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2024-01-10 13:25:30 +0900

    fix(map_loader): show traffic light regulatory element id per lanelet (#6028)
    * fix(map_loader): show traffic light regulatory element id per lanelet
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * feat(map_loader): show traffic light id
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    ---------
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    Co-authored-by: Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>

--- map/map_loader/src/lanelet2_map_loader/lanelet2_map_visualization_node.cpp
+++ map/map_loader/src/lanelet2_map_loader/lanelet2_map_visualization_node.cpp
@@ -216,6 +216,12 @@ void Lanelet2MapVisualizationNode::onMapBin(
     &map_marker_array,
     lanelet::visualization::autowareTrafficLightsAsMarkerArray(aw_tl_reg_elems, cl_trafficlights));
   insertMarkerArray(
+    &map_marker_array, lanelet::visualization::generateTrafficLightRegulatoryElementIdMaker(
+                         road_lanelets, cl_trafficlights));
+  insertMarkerArray(
+    &map_marker_array, lanelet::visualization::generateTrafficLightRegulatoryElementIdMaker(
+                         crosswalk_lanelets, cl_trafficlights));
+  insertMarkerArray(
     &map_marker_array,
     lanelet::visualization::generateTrafficLightIdMaker(aw_tl_reg_elems, cl_trafficlights));
   insertMarkerArray(
