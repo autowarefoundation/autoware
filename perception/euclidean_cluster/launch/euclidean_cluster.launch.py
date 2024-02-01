742f44a5e647807d164740dd1185066ed363b6fb
kminoda <44218668+kminoda@users.noreply.github.com>
2024-02-01 17:43:47 +0900

    feat(detection): add container option (#6228)
    * feat(lidar_centerpoint,image_projection_based_fusion): add pointcloud_container option
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * revert lidar_perception_model
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * style(pre-commit): autofix
    
    * fix: add options
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix: fix default param
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * update node name
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix: fix IfCondition
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix pointpainting namespace
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix: fix launch args
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix(euclidean_cluster): do not launch individual container when use_pointcloud_container is true
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix(euclidean_cluster): fix launch condition
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * fix(euclidean_cluster): fix launch condition
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    
    * Update perception/lidar_centerpoint/launch/lidar_centerpoint.launch.xml
    
    Co-authored-by: Shunsuke Miura <37187849+miursh@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: kminoda <koji.minoda@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Shunsuke Miura <37187849+miursh@users.noreply.github.com>
    Co-authored-by: Kenzo Lobos Tsunekawa <kenzo.lobos@tier4.jp>

--- perception/euclidean_cluster/launch/euclidean_cluster.launch.py
+++ perception/euclidean_cluster/launch/euclidean_cluster.launch.py
@@ -76,6 +76,13 @@ def launch_setup(context, *args, **kwargs):
         executable="component_container",
         composable_node_descriptions=[],
         output="screen",
+        condition=UnlessCondition(LaunchConfiguration("use_pointcloud_container")),
+    )
+
+    target_container = (
+        LaunchConfiguration("pointcloud_container_name")
+        if IfCondition(LaunchConfiguration("use_pointcloud_container")).evaluate(context)
+        else container
     )
 
     use_low_height_pointcloud_loader = LoadComposableNodes(
@@ -83,13 +90,13 @@ def launch_setup(context, *args, **kwargs):
             low_height_cropbox_filter_component,
             use_low_height_euclidean_component,
         ],
-        target_container=container,
+        target_container=target_container,
         condition=IfCondition(LaunchConfiguration("use_low_height_cropbox")),
     )
 
     disuse_low_height_pointcloud_loader = LoadComposableNodes(
         composable_node_descriptions=[disuse_low_height_euclidean_component],
-        target_container=container,
+        target_container=target_container,
         condition=UnlessCondition(LaunchConfiguration("use_low_height_cropbox")),
     )
 
@@ -106,6 +113,8 @@ def generate_launch_description():
             add_launch_arg("input_map", "/map/pointcloud_map"),
             add_launch_arg("output_clusters", "clusters"),
             add_launch_arg("use_low_height_cropbox", "false"),
+            add_launch_arg("use_pointcloud_container", "false"),
+            add_launch_arg("pointcloud_container_name", "pointcloud_container"),
             add_launch_arg(
                 "euclidean_param_path",
                 [
--- perception/euclidean_cluster/launch/voxel_grid_based_euclidean_cluster.launch.py
+++ perception/euclidean_cluster/launch/voxel_grid_based_euclidean_cluster.launch.py
@@ -77,6 +77,13 @@ def launch_setup(context, *args, **kwargs):
         executable="component_container",
         composable_node_descriptions=[],
         output="screen",
+        condition=UnlessCondition(LaunchConfiguration("use_pointcloud_container")),
+    )
+
+    target_container = (
+        LaunchConfiguration("pointcloud_container_name")
+        if IfCondition(LaunchConfiguration("use_pointcloud_container")).evaluate(context)
+        else container
     )
 
     use_low_height_pointcloud_loader = LoadComposableNodes(
@@ -84,13 +91,13 @@ def launch_setup(context, *args, **kwargs):
             low_height_cropbox_filter_component,
             use_low_height_euclidean_component,
         ],
-        target_container=container,
+        target_container=target_container,
         condition=IfCondition(LaunchConfiguration("use_low_height_cropbox")),
     )
 
     disuse_low_height_pointcloud_loader = LoadComposableNodes(
         composable_node_descriptions=[disuse_low_height_euclidean_component],
-        target_container=container,
+        target_container=target_container,
         condition=UnlessCondition(LaunchConfiguration("use_low_height_cropbox")),
     )
     return [
@@ -110,6 +117,8 @@ def generate_launch_description():
             add_launch_arg("input_map", "/map/pointcloud_map"),
             add_launch_arg("output_clusters", "clusters"),
             add_launch_arg("use_low_height_cropbox", "false"),
+            add_launch_arg("use_pointcloud_container", "false"),
+            add_launch_arg("pointcloud_container_name", "pointcloud_container"),
             add_launch_arg(
                 "voxel_grid_based_euclidean_param_path",
                 [
