7d8ee418c26c9f56c494c17f4625590f2ed674a9
Takayuki Murooka <takayuki5168@gmail.com>
2024-02-14 21:32:22 +0900

    feat(motion_velocity_smoother): use common max_vel (#6388)
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/motion_velocity_smoother/src/motion_velocity_smoother_node.cpp
+++ planning/motion_velocity_smoother/src/motion_velocity_smoother_node.cpp
@@ -173,7 +173,7 @@ rcl_interfaces::msg::SetParametersResult MotionVelocitySmootherNode::onParameter
     update_param_bool("enable_lateral_acc_limit", p.enable_lateral_acc_limit);
     update_param_bool("enable_steering_rate_limit", p.enable_steering_rate_limit);
 
-    update_param("max_velocity", p.max_velocity);
+    update_param("max_vel", p.max_velocity);
     update_param(
       "margin_to_insert_external_velocity_limit", p.margin_to_insert_external_velocity_limit);
     update_param("replan_vel_deviation", p.replan_vel_deviation);
@@ -277,7 +277,7 @@ void MotionVelocitySmootherNode::initCommonParam()
   p.enable_lateral_acc_limit = declare_parameter<bool>("enable_lateral_acc_limit");
   p.enable_steering_rate_limit = declare_parameter<bool>("enable_steering_rate_limit");
 
-  p.max_velocity = declare_parameter<double>("max_velocity");  // 72.0 kmph
+  p.max_velocity = declare_parameter<double>("max_vel");
   p.margin_to_insert_external_velocity_limit =
     declare_parameter<double>("margin_to_insert_external_velocity_limit");
   p.replan_vel_deviation = declare_parameter<double>("replan_vel_deviation");
