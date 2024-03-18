413514b49eb86c2ecdf3aad7f20e18b34b91515e
danielsanchezaran <daniel.sanchez@tier4.jp>
2024-03-18 17:18:51 +0900

    refactor(obstacle_cruise_planner): move slow down params to a clear location (#6644)
    move slow down params to a clear location
    
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- planning/obstacle_cruise_planner/include/obstacle_cruise_planner/common_structs.hpp
+++ planning/obstacle_cruise_planner/include/obstacle_cruise_planner/common_structs.hpp
@@ -177,8 +177,8 @@ struct LongitudinalInfo
     limit_min_accel = node.declare_parameter<double>("limit.min_acc");
     limit_max_jerk = node.declare_parameter<double>("limit.max_jerk");
     limit_min_jerk = node.declare_parameter<double>("limit.min_jerk");
-    slow_down_min_accel = node.declare_parameter<double>("slow_down.min_acc");
-    slow_down_min_jerk = node.declare_parameter<double>("slow_down.min_jerk");
+    slow_down_min_accel = node.declare_parameter<double>("common.slow_down_min_acc");
+    slow_down_min_jerk = node.declare_parameter<double>("common.slow_down_min_jerk");
 
     idling_time = node.declare_parameter<double>("common.idling_time");
     min_ego_accel_for_rss = node.declare_parameter<double>("common.min_ego_accel_for_rss");
@@ -205,8 +205,9 @@ struct LongitudinalInfo
     tier4_autoware_utils::updateParam<double>(parameters, "limit.max_jerk", limit_max_jerk);
     tier4_autoware_utils::updateParam<double>(parameters, "limit.min_jerk", limit_min_jerk);
     tier4_autoware_utils::updateParam<double>(
-      parameters, "slow_down.min_accel", slow_down_min_accel);
-    tier4_autoware_utils::updateParam<double>(parameters, "slow_down.min_jerk", slow_down_min_jerk);
+      parameters, "common.slow_down_min_accel", slow_down_min_accel);
+    tier4_autoware_utils::updateParam<double>(
+      parameters, "common.slow_down_min_jerk", slow_down_min_jerk);
 
     tier4_autoware_utils::updateParam<double>(parameters, "common.idling_time", idling_time);
     tier4_autoware_utils::updateParam<double>(
