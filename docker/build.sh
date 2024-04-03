d6db15aa2b3d8efcc7eda7103f08ae7d46289ebc
Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>
2024-04-03 21:11:08 +0900

    feat: update repos file and workflow (#5)
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- docker/build.sh
+++ docker/build.sh
@@ -71,7 +71,7 @@ docker buildx bake --no-cache --load --progress=plain -f "$SCRIPT_DIR/autoware-u
     --set "*.args.BASE_IMAGE=$base_image" \
     --set "*.args.PREBUILT_BASE_IMAGE=$prebuilt_base_image" \
     --set "*.args.SETUP_ARGS=$setup_args" \
-    --set "devel.tags=ghcr.io/autowarefoundation/autoware-universe:$rosdistro-latest$image_name_suffix" \
-    --set "prebuilt.tags=ghcr.io/autowarefoundation/autoware-universe:$rosdistro-latest-prebuilt$image_name_suffix" \
+    --set "devel.tags=ghcr.io/automotiveaichallenge/autoware-universe:$rosdistro-latest$image_name_suffix" \
+    --set "prebuilt.tags=ghcr.io/automotiveaichallenge/autoware-universe:$rosdistro-latest-prebuilt$image_name_suffix" \
     "${targets[@]}"
 set +x
