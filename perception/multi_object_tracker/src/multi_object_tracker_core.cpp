81c432e8110ca54acc5e6fc4a757eff87724c39b
Taekjin LEE <technolojin@gmail.com>
2024-04-10 13:16:21 +0900

    chore(multi_object_tracker): change node and glog implementation (#6780)
    * chore: change node and glog impl
    
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>
    
    * style(pre-commit): autofix
    
    ---------
    
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: kminoda <44218668+kminoda@users.noreply.github.com>

--- perception/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ perception/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -72,10 +72,6 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
   tf_buffer_(this->get_clock()),
   tf_listener_(tf_buffer_)
 {
-  // glog for debug
-  google::InitGoogleLogging("multi_object_tracker");
-  google::InstallFailureSignalHandler();
-
   // Create publishers and subscribers
   detected_object_sub_ = create_subscription<autoware_auto_perception_msgs::msg::DetectedObjects>(
     "input", rclcpp::QoS{1},
---
+++ perception/multi_object_tracker/src/multi_object_tracker_node.cpp
@@ -0,0 +1,34 @@
+// Copyright 2024 TIER IV, inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "multi_object_tracker/multi_object_tracker_core.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <glog/logging.h>
+
+int main(int argc, char ** argv)
+{
+  google::InitGoogleLogging(argv[0]);  // NOLINT
+  google::InstallFailureSignalHandler();
+
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  auto multi_object_tracker = std::make_shared<MultiObjectTracker>(options);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(multi_object_tracker);
+  exec.spin();
+  rclcpp::shutdown();
+  return 0;
+}
