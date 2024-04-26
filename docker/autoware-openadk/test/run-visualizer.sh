35ae5bcbf8e84c8af14a12770020a376ac57a3f8
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-04-26 13:35:39 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/autoware-openadk/test/run-visualizer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# rviz-visulaizer
-docker run -it --net=host -v /dev/shm:/dev/shm -e ROS_DOMAIN_ID=88 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix ghcr.io/autowarefoundation/autoware-openadk:latest-visualizer /autoware/test-scenario/rviz/launch_rviz.sh
