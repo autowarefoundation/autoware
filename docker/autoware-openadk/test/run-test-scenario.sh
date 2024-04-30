6cd0c73b8e001b2b0163184eb832e9e3739e9364
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-04-30 14:55:20 +0300

    add test artifacts to drive
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/autoware-openadk/test/run-test-scenario.sh
+++ docker/autoware-openadk/test/run-test-scenario.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
-
 set -e
 
+# download test artifacts from google drive
+if [ ! -d ~/autoware_artifacts/test-artifacts ]; then
+    echo "Downloading test artifacts from google drive..."
+    gdown -O ~/autoware_artifacts/ 'https://drive.google.com/uc?export=download&id=1gTCWcye4JxqR7rSxl2b-iiLqgya6ShBR'
+    unzip -d ~/autoware_artifacts ~/autoware_artifacts/test-artifacts.zip
+    rm ~/autoware_artifacts/test-artifacts.zip
+else
+    echo "Test artifacts are already downloaded."
+fi
+
+# enable xhost
 xhost +
-docker compose -f docker-compose.yaml up
+
+# start containers
+echo "Starting containers..."
+docker compose -f test-scenario.docker-compose.yaml up
--- docker/autoware-openadk/test/test-scenario/rviz/launch_rviz.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rviz2 -d /autoware/test-scenario/rviz/scenario_simulator_v2.rviz
