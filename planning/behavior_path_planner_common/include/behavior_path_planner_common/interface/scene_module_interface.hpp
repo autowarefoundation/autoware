92b51143028ad113a77abcd8fb5e7f424c629769
Fumiya Watanabe <rej55.g@gmail.com>
2024-05-27 14:46:10 +0900

    feat(behavior_path_planner_common): update RTC state to SUCCEEDED (#7119)
    Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

--- planning/behavior_path_planner_common/include/behavior_path_planner_common/interface/scene_module_interface.hpp
+++ planning/behavior_path_planner_common/include/behavior_path_planner_common/interface/scene_module_interface.hpp
@@ -47,6 +47,7 @@
 
 #include <algorithm>
 #include <any>
+#include <limits>
 #include <memory>
 #include <string>
 #include <unordered_map>
@@ -185,6 +186,10 @@ public:
   void onExit()
   {
     RCLCPP_DEBUG(getLogger(), "%s %s", name_.c_str(), __func__);
+
+    if (getCurrentStatus() == ModuleStatus::SUCCESS) {
+      updateRTCStatusForSuccess();
+    }
 
     clearWaitingApproval();
     unlockNewModuleLaunch();
@@ -495,6 +500,17 @@ protected:
         ptr->updateCooperateStatus(
           uuid_map_.at(module_name), isExecutionReady(), state, start_distance, finish_distance,
           clock_->now());
+      }
+    }
+  }
+
+  void updateRTCStatusForSuccess()
+  {
+    for (const auto & [module_name, ptr] : rtc_interface_ptr_map_) {
+      if (ptr) {
+        ptr->updateCooperateStatus(
+          uuid_map_.at(module_name), true, State::SUCCEEDED, std::numeric_limits<double>::lowest(),
+          std::numeric_limits<double>::lowest(), clock_->now());
       }
     }
   }
