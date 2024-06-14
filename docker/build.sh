4e7f539d0739058697a73c2546d3065fb8682e70
oguzkaganozt <oguzkaganozt@gmail.com>
2024-06-14 15:35:56 +0300

    feat(docker): fix CUDA compile on devel image and improve run.sh (#4849)
    * modular containers PR phase1
    
    Signed-off-by: Oguz <oguzkaganozt@gmail.com>
    
    * style(pre-commit): autofix
    
    * add download artifacts to devel and runtime image
    
    Signed-off-by: Oguz <oguzkaganozt@gmail.com>
    
    * Update docker/build.sh
    
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * style(pre-commit): autofix
    
    * build from temp source directory and remove dangling images
    
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>
    
    * Revert "build from temp source directory and remove dangling images"
    
    This reverts commit c66a1ddf482d44ad8e05d38185a9d57038ab080f.
    
    * build containers from temp source and remove dangling images
    
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>
    
    * style(pre-commit): autofix
    
    * add vcs pull to update
    
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>
    
    * style(pre-commit): autofix
    
    ---------
    
    Signed-off-by: Oguz <oguzkaganozt@gmail.com>
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/build.sh
+++ docker/build.sh
@@ -97,6 +97,19 @@ load_env() {
     fi
 }
 
+# Clone repositories
+clone_repositories() {
+    cd "$WORKSPACE_ROOT"
+    if [ ! -d "docker/src" ]; then
+        mkdir -p docker/src
+        vcs import docker/src <autoware.repos
+    else
+        echo "Source directory already exists. Updating repositories..."
+        vcs import docker/src <autoware.repos
+        vcs pull docker/src
+    fi
+}
+
 # Build images
 build_images() {
     # https://github.com/docker/buildx/issues/484
@@ -119,11 +132,22 @@ build_images() {
         --set "*.args.BASE_IMAGE=$base_image" \
         --set "*.args.SETUP_ARGS=$setup_args" \
         --set "*.args.LIB_DIR=$lib_dir" \
+        --set "base.tags=ghcr.io/autowarefoundation/autoware:latest-base" \
         --set "devel.tags=ghcr.io/autowarefoundation/autoware:latest-devel$image_name_suffix" \
         --set "prebuilt.tags=ghcr.io/autowarefoundation/autoware:latest-prebuilt$image_name_suffix" \
         --set "runtime.tags=ghcr.io/autowarefoundation/autoware:latest-runtime$image_name_suffix" \
         "${targets[@]}"
     set +x
+}
+
+# Remove temporary source directory
+remove_tmp_src() {
+    rm -rf "$WORKSPACE_ROOT/docker/src"
+}
+
+# Remove dangling images
+remove_dangling_images() {
+    docker image prune -f
 }
 
 # Main script execution
@@ -133,4 +157,7 @@ set_build_options
 set_platform
 set_arch_lib_dir
 load_env
+clone_repositories
 build_images
+remove_tmp_src
+remove_dangling_images
