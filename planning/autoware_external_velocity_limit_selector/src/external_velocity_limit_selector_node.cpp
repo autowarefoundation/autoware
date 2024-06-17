833c3c9db68ba0c38675e4a76915073ae9e10148
Takayuki Murooka <takayuki5168@gmail.com>
2024-06-17 18:51:45 +0900

    feat(external_velocity_limit_selector): rename to include/autoware/{package_name} (#7516)
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/autoware_external_velocity_limit_selector/src/external_velocity_limit_selector_node.cpp
+++ planning/autoware_external_velocity_limit_selector/src/external_velocity_limit_selector_node.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "autoware_external_velocity_limit_selector/external_velocity_limit_selector_node.hpp"
+#include "autoware/external_velocity_limit_selector/external_velocity_limit_selector_node.hpp"
 
 #include <deque>
 #include <memory>
