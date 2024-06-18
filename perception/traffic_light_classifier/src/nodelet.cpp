a8cb02cff47591c678ebc23f7ca3a7c4493dae73
Taekjin LEE <taekjin.lee@tier4.jp>
2024-06-18 10:03:52 +0900

    fix(traffic_light_classifier): check if the given image is empty or not (#7461)
    fix: check if the given image is empty or not
    
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- perception/traffic_light_classifier/src/nodelet.cpp
+++ perception/traffic_light_classifier/src/nodelet.cpp
@@ -157,6 +157,9 @@ void TrafficLightClassifierNodelet::imageRoiCallback(
 
 bool TrafficLightClassifierNodelet::is_harsh_backlight(const cv::Mat & img) const
 {
+  if (img.empty()) {
+    return false;
+  }
   cv::Mat y_cr_cb;
   cv::cvtColor(img, y_cr_cb, cv::COLOR_RGB2YCrCb);
 
