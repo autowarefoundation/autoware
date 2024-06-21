821d14a17d8fa05e62c3f545867c58de8fe60f6c
Go Sakayori <go-sakayori@users.noreply.github.com>
2024-06-21 17:08:24 +0900

    refactor(static_obstacle_avoidance): change logger name for utils    (#7617)
    change logger name for static avoidance utils
    
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/include/autoware/behavior_path_static_obstacle_avoidance_module/utils.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/include/autoware/behavior_path_static_obstacle_avoidance_module/utils.hpp
@@ -34,7 +34,8 @@ using autoware::behavior_path_planner::utils::path_safety_checker::PoseWithVeloc
 using autoware::behavior_path_planner::utils::path_safety_checker::PredictedPathWithPolygon;
 
 static constexpr const char * logger_namespace =
-  "planning.scenario_planning.lane_driving.behavior_planning.behavior_path_planner.avoidance.utils";
+  "planning.scenario_planning.lane_driving.behavior_planning.behavior_path_planner.static_obstacle_"
+  "avoidance.utils";
 
 bool isOnRight(const ObjectData & obj);
 
