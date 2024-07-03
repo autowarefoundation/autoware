376385771433b25dd411188b31ae640a4e437988
badai nguyen <94814556+badai-nguyen@users.noreply.github.com>
2024-07-03 11:31:44 +0900

    fix(euclidean_cluster): fix max_cluster_size bug (#7734)
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/euclidean_cluster/lib/voxel_grid_based_euclidean_cluster.cpp
+++ perception/euclidean_cluster/lib/voxel_grid_based_euclidean_cluster.cpp
@@ -117,7 +117,7 @@ bool VoxelGridBasedEuclideanCluster::cluster(
       voxel_grid_.getCentroidIndexAt(voxel_grid_.getGridCoordinates(point.x, point.y, point.z));
     if (map.find(index) != map.end()) {
       auto & cluster_data_size = clusters_data_size.at(map[index]);
-      if (cluster_data_size + point_step > std::size_t(max_cluster_size_ * point_step)) {
+      if (cluster_data_size > std::size_t(max_cluster_size_ * point_step)) {
         continue;
       }
       std::memcpy(
