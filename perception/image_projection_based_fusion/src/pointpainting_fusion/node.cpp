ea4d3815d8ac505ee9c52c1adac659dac685c307
Koichi98 <45482193+Koichi98@users.noreply.github.com>
2024-07-16 18:06:43 +0900

    fix(image_projection_based_fusion): fix invalidPointerCast (#7927)
    * fix(image_projection_based_fusion): fix invalidPointerCast
    add cppcheck-suppress for reinterpret_cast 
    
    Signed-off-by: Koichi Imai <kotty.0704@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- perception/image_projection_based_fusion/src/pointpainting_fusion/node.cpp
+++ perception/image_projection_based_fusion/src/pointpainting_fusion/node.cpp
@@ -322,9 +322,11 @@ dc   | dc dc dc  dc ||zc|
     int stride = p_step * i;
     unsigned char * data = &painted_pointcloud_msg.data[0];
     unsigned char * output = &painted_pointcloud_msg.data[0];
+    // cppcheck-suppress-begin invalidPointerCast
     float p_x = *reinterpret_cast<const float *>(&data[stride + x_offset]);
     float p_y = *reinterpret_cast<const float *>(&data[stride + y_offset]);
     float p_z = *reinterpret_cast<const float *>(&data[stride + z_offset]);
+    // cppcheck-suppress-end invalidPointerCast
     point_lidar << p_x, p_y, p_z;
     point_camera = lidar2cam_affine * point_lidar;
     p_x = point_camera.x();
@@ -345,6 +347,7 @@ dc   | dc dc dc  dc ||zc|
       int label2d = feature_object.object.classification.front().label;
       if (!isUnknown(label2d) && isInsideBbox(projected_point.x(), projected_point.y(), roi, p_z)) {
         data = &painted_pointcloud_msg.data[0];
+        // cppcheck-suppress invalidPointerCast
         auto p_class = reinterpret_cast<float *>(&output[stride + class_offset]);
         for (const auto & cls : isClassTable_) {
           // add up the class values if the point belongs to multiple classes
