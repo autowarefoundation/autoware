d78a7aebdcf0dfc72bcf3c6fa5ad4d61eea523d9
kobayu858 <129580202+kobayu858@users.noreply.github.com>
2024-07-19 11:56:26 +0900

    fix(autoware_crosswalk_traffic_light_estimator): fix constVariableReference (#8055)
    fix:constVariableReference
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
+++ perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
@@ -209,7 +209,7 @@ void CrosswalkTrafficLightEstimatorNode::updateLastDetectedSignal(
   }
 
   std::vector<int32_t> erase_id_list;
-  for (auto & last_traffic_signal : last_detect_color_) {
+  for (const auto & last_traffic_signal : last_detect_color_) {
     const auto & id = last_traffic_signal.second.first.traffic_light_group_id;
 
     if (traffic_light_id_map.count(id) == 0) {
