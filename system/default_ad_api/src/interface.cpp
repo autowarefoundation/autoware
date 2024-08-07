f732b45ca95be7e1d0a874ec067033aa5ac394b4
Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>
2024-08-07 17:19:13 +0900

    feat(default_ad_api): release adapi v1.5.0 (#8267)
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/default_ad_api/src/interface.cpp
+++ system/default_ad_api/src/interface.cpp
@@ -21,7 +21,7 @@ InterfaceNode::InterfaceNode(const rclcpp::NodeOptions & options) : Node("interf
 {
   const auto on_interface_version = [](auto, auto res) {
     res->major = 1;
-    res->minor = 4;
+    res->minor = 5;
     res->patch = 0;
   };
 
