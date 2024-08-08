50a110a7cc0dfe953b591b3a7a59dabed0508516
Mamoru Sobue <hilo.soblin@gmail.com>
2024-08-08 18:14:49 +0900

    perf(map_based_prediction): remove unncessary withinRoadLanelet() (#8403)
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- perception/autoware_map_based_prediction/src/map_based_prediction_node.cpp
+++ perception/autoware_map_based_prediction/src/map_based_prediction_node.cpp
@@ -662,7 +662,6 @@ ObjectClassification::_label_type changeLabelForPrediction(
     }
 
     case ObjectClassification::PEDESTRIAN: {
-      const bool within_road_lanelet = withinRoadLanelet(object, lanelet_map_ptr_, true);
       const float max_velocity_for_human_mps =
         autoware::universe_utils::kmph2mps(25.0);  // Max human being motion speed is 25km/h
       const double abs_speed = std::hypot(
@@ -670,7 +669,6 @@ ObjectClassification::_label_type changeLabelForPrediction(
         object.kinematics.twist_with_covariance.twist.linear.y);
       const bool high_speed_object = abs_speed > max_velocity_for_human_mps;
       // fast, human-like object: like segway
-      if (within_road_lanelet && high_speed_object) return label;  // currently do nothing
       // return ObjectClassification::MOTORCYCLE;
       if (high_speed_object) return label;  // currently do nothing
       // fast human outside road lanelet will move like unknown object
