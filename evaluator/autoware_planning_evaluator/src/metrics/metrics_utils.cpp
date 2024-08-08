022bd175a87bdf617ed9367fc701ef1a9d79d1de
kobayu858 <129580202+kobayu858@users.noreply.github.com>
2024-08-08 09:07:21 +0900

    fix(autoware_planning_evaluator): fix unreadVariable (#8352)
    * fix:unreadVariable
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix:unreadVariable
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- evaluator/autoware_planning_evaluator/src/metrics/metrics_utils.cpp
+++ evaluator/autoware_planning_evaluator/src/metrics/metrics_utils.cpp
@@ -30,9 +30,8 @@ size_t getIndexAfterDistance(const Trajectory & traj, const size_t curr_id, cons
   const TrajectoryPoint & curr_p = traj.points.at(curr_id);
 
   size_t target_id = curr_id;
-  double current_distance = 0.0;
   for (size_t traj_id = curr_id + 1; traj_id < traj.points.size(); ++traj_id) {
-    current_distance = calcDistance2d(traj.points.at(traj_id), curr_p);
+    double current_distance = calcDistance2d(traj.points.at(traj_id), curr_p);
     if (current_distance >= distance) {
       target_id = traj_id;
       break;
