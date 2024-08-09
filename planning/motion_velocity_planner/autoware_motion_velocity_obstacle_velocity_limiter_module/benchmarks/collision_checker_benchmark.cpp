5a7bb0ff0234f0d8533457225aa2acb41cf47b52
kobayu858 <129580202+kobayu858@users.noreply.github.com>
2024-08-09 12:44:56 +0900

    fix(autoware_motion_velocity_obstacle_velocity_limiter_module): fix unreadVariable (#8366)
    * fix:unreadVariable
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix:unreadVariable
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_obstacle_velocity_limiter_module/benchmarks/collision_checker_benchmark.cpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_obstacle_velocity_limiter_module/benchmarks/collision_checker_benchmark.cpp
@@ -73,10 +73,12 @@ int main()
       const auto naive_constr_end = std::chrono::system_clock::now();
       const auto rtt_check_start = std::chrono::system_clock::now();
       for (const auto & polygon : polygons)
+        // cppcheck-suppress unreadVariable
         const auto rtree_result = rtree_collision_checker.intersections(polygon);
       const auto rtt_check_end = std::chrono::system_clock::now();
       const auto naive_check_start = std::chrono::system_clock::now();
       for (const auto & polygon : polygons)
+        // cppcheck-suppress unreadVariable
         const auto naive_result = naive_collision_checker.intersections(polygon);
       const auto naive_check_end = std::chrono::system_clock::now();
       const auto rtt_constr_time =
