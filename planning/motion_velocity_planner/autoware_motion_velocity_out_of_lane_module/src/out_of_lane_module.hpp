441bfcfe01a2f09f82810f4f741faff525ce6011
Maxime CLEMENT <78338830+maxime-clem@users.noreply.github.com>
2024-08-28 17:16:21 +0900

    fix(out_of_lane): fix noConstructor cppcheck warning (#8636)
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_out_of_lane_module/src/out_of_lane_module.hpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_out_of_lane_module/src/out_of_lane_module.hpp
@@ -30,6 +30,7 @@
 #include <lanelet2_core/LaneletMap.h>
 
 #include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -58,17 +59,17 @@ private:
     out_of_lane::EgoData & ego_data, const PlannerData & planner_data,
     std::optional<geometry_msgs::msg::Pose> & previous_slowdown_pose_, const double slow_velocity);
 
-  out_of_lane::PlannerParam params_;
+  out_of_lane::PlannerParam params_{};
 
   inline static const std::string ns_ = "out_of_lane";
-  std::string module_name_;
-  rclcpp::Clock::SharedPtr clock_;
-  std::optional<geometry_msgs::msg::Pose> previous_slowdown_pose_;
-  rclcpp::Time previous_slowdown_time_;
+  std::string module_name_{"uninitialized"};
+  rclcpp::Clock::SharedPtr clock_{nullptr};
+  std::optional<geometry_msgs::msg::Pose> previous_slowdown_pose_{std::nullopt};
+  rclcpp::Time previous_slowdown_time_{0};
 
 protected:
   // Debug
-  mutable out_of_lane::DebugData debug_data_;
+  mutable out_of_lane::DebugData debug_data_{};
 };
 }  // namespace autoware::motion_velocity_planner
 
