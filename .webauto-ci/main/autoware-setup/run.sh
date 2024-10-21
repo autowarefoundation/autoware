d2d2419674c4bc0e64aaf4763a017000b24e266c
ykataya-fsi <ykataya@fsi.co.jp>
2024-10-21 12:00:38 +0900

    Update run.sh

--- .webauto-ci/main/autoware-setup/run.sh
+++ .webauto-ci/main/autoware-setup/run.sh
@@ -1,3 +1,32 @@
+#!/bin/bash -e
+: "${WEBAUTO_CI_GITHUB_TOKEN:?is not set}"
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
+sudo -E apt-get -y update
+sudo -E apt-get -y install usbutils # For kvaser
+
+export GITHUB_TOKEN="$WEBAUTO_CI_GITHUB_TOKEN"
+git config --global --add url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+ansible_args=()
+ansible_args+=("--extra-vars" "prompt_install_nvidia=y")
+ansible_args+=("--extra-vars" "prompt_download_artifacts=y")
+ansible_args+=("--extra-vars" "data_dir=$HOME/autoware_data")
+
+# read amd64 env file and expand ansible arguments
+source 'amd64.env'
+while read -r env_name; do
+    ansible_args+=("--extra-vars" "${env_name}=${!env_name}")
+done < <(sed "s/=.*//" <amd64.env)
+
+ansible-galaxy collection install -f -r "ansible-galaxy-requirements.yaml"
+ansible-playbook "ansible/playbooks/local_dev_env.yaml" \
+    "${ansible_args[@]}" \
+    -e WORKSPACE_ROOT="$(pwd)" \
+    -e install_devel="y" \
+    --skip-tags vcs
+
+git config --global --unset-all url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf
 : "${WEBAUTO_CI_GITHUB_TOKEN:?is not set}"
 
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
