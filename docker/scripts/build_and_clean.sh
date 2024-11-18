c785d70976c5eca337587d193fed930e3f35c702
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-11-18 17:07:58 +0900

    add force_cuda option
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/build_and_clean.sh
+++ docker/scripts/build_and_clean.sh
@@ -3,11 +3,15 @@
 function build_and_clean() {
     local ccache_dir=$1
     local install_base=$2
+    local cuda_image=$3
+
+    local cmake_args=" -Wno-dev --no-warn-unused-cli"
+    if [ "$cuda_image" = true ]; then
+        cmake_args="$cmake_args -DFORCE_CUDA=1"
+    fi
 
     du -sh "$ccache_dir" && ccache -s &&
-        colcon build --cmake-args \
-            " -Wno-dev" \
-            " --no-warn-unused-cli" \
+        colcon build --cmake-args "$cmake_args" \
             --merge-install \
             --install-base "$install_base" \
             --mixin release compile-commands ccache &&
