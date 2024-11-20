3e3fcad89f07f75e15f2ea65e6b0d0be97ba66ae
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-11-20 12:27:08 +0900

    refine cuda related pkgs build
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/build_and_clean.sh
+++ docker/scripts/build_and_clean.sh
@@ -3,6 +3,7 @@
 function build_and_clean() {
     local ccache_dir=$1
     local install_base=$2
+    local colcon_build_args=$3
 
     du -sh "$ccache_dir" && ccache -s &&
         colcon build --cmake-args \
@@ -10,7 +11,8 @@ function build_and_clean() {
             " --no-warn-unused-cli" \
             --merge-install \
             --install-base "$install_base" \
-            --mixin release compile-commands ccache &&
+            --mixin release compile-commands ccache \
+            "$colcon_build_args" &&
         du -sh "$ccache_dir" && ccache -s &&
         rm -rf /autoware/build /autoware/log
 }
