017395855219f34e8546b57d8f3635c058521b7f
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-11-21 15:43:22 +0900

    remove echo
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/cleanup_apt.sh
+++ docker/scripts/cleanup_apt.sh
@@ -4,9 +4,6 @@ function cleanup_apt() {
     local remove_var_lib_apt_lists=$1
     apt-get autoremove -y && rm -rf "$HOME"/.cache
     if [[ "$remove_var_lib_apt_lists" = true ]]; then
-        echo "----------------------------------------"
-        echo "Removing /var/lib/apt/lists/*"
-        echo "----------------------------------------"
         rm -rf /var/lib/apt/lists/*
     fi
 }
