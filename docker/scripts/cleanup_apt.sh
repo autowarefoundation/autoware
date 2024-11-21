ef74e19c5a60027146e49e83341d5639038e048b
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-11-21 16:59:46 +0900

    fix(docker): fix `cleanup_apt.sh` (#5467)
    * echo
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * fix
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * remove echo
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    
    * style(pre-commit): autofix
    
    ---------
    
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docker/scripts/cleanup_apt.sh
+++ docker/scripts/cleanup_apt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function cleanup() {
-    local remove_var_lib_apt_lists=false
+function cleanup_apt() {
+    local remove_var_lib_apt_lists=$1
     apt-get autoremove -y && rm -rf "$HOME"/.cache
-    if "$remove_var_lib_apt_lists"; then
+    if [[ $remove_var_lib_apt_lists == true ]]; then
         rm -rf /var/lib/apt/lists/*
     fi
 }
 
-cleanup "$@"
+cleanup_apt "$@"
