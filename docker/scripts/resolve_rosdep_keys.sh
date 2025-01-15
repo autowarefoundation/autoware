fc2c6e5a2505a0645179f2bf40beb3e7d8e18974
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-01-15 11:49:36 +0900

    set default
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/resolve_rosdep_keys.sh
+++ docker/scripts/resolve_rosdep_keys.sh
@@ -4,6 +4,9 @@ function resolve_rosdep_keys() {
     local src_path=$1
     local ros_distro=$2
     local dependency_types=$3
+    if [ -z "$dependency_types" ]; then
+        dependency_types="build build_export buildtool buildtool_export exec test"
+    fi
 
     rosdep keys --dependency-types="$dependency_types" --ignore-src --from-paths "$src_path" |
         xargs rosdep resolve --rosdistro "$ros_distro" |
