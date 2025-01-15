df1a1c361dfb0e3cdf76bada12f7add7e0fb3aba
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-01-15 14:37:36 +0900

    add comment
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/resolve_rosdep_keys.sh
+++ docker/scripts/resolve_rosdep_keys.sh
@@ -5,6 +5,7 @@ function resolve_rosdep_keys() {
     local ros_distro=$2
     local rosdep_keys_args=$3
 
+    # shellcheck disable=SC2086
     rosdep keys $rosdep_keys_args --ignore-src --from-paths "$src_path" |
         xargs rosdep resolve --rosdistro "$ros_distro" |
         grep -v '^#' |
