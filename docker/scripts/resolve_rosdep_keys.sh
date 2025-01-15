d8294e7719a230a0652d31068754a2d2e2434a18
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-01-15 12:08:04 +0900

    fix
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/resolve_rosdep_keys.sh
+++ docker/scripts/resolve_rosdep_keys.sh
@@ -3,12 +3,9 @@
 function resolve_rosdep_keys() {
     local src_path=$1
     local ros_distro=$2
-    local dependency_types=$3
-    if [ -z "$dependency_types" ]; then
-        dependency_types="build build_export buildtool buildtool_export exec test"
-    fi
+    local rosdep_keys_args=$3
 
-    rosdep keys --dependency-types="$dependency_types" --ignore-src --from-paths "$src_path" |
+    rosdep keys "$rosdep_keys_args" --ignore-src --from-paths "$src_path" |
         xargs rosdep resolve --rosdistro "$ros_distro" |
         grep -v '^#' |
         sed 's/ \+/\n/g' |
