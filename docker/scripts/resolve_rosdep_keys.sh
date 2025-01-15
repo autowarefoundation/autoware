2c82c84ec304070a249b9e119653e057b3f69f22
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-01-15 13:54:51 +0900

    fix
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/scripts/resolve_rosdep_keys.sh
+++ docker/scripts/resolve_rosdep_keys.sh
@@ -5,7 +5,7 @@ function resolve_rosdep_keys() {
     local ros_distro=$2
     local rosdep_keys_args=$3
 
-    rosdep keys "$rosdep_keys_args" --ignore-src --from-paths "$src_path" |
+    rosdep keys $rosdep_keys_args --ignore-src --from-paths "$src_path" |
         xargs rosdep resolve --rosdistro "$ros_distro" |
         grep -v '^#' |
         sed 's/ \+/\n/g' |
