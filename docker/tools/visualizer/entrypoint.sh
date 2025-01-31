297deffa311e82d34fc61898341b35e4e5786c66
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-01-31 21:45:47 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/tools/visualizer/entrypoint.sh
+++ docker/tools/visualizer/entrypoint.sh
@@ -3,6 +3,12 @@
 # shellcheck disable=SC1090,SC1091
 
 configure_vnc() {
+    # Check if WEB_PASSWORD is provided
+    [ -z "$WEB_PASSWORD" ] && echo -e "\e[31mPassword is needed. Set WEB_PASSWORD environment variable\e[0m" && exit 1
+
+    # Check if RVIZ_CONFIG is provided
+    [ -z "$RVIZ_CONFIG" ] && RVIZ_CONFIG="$(find-pkg-share rviz2)/share/rviz2/launch/default.rviz" || echo -e "\e[31mRVIZ_CONFIG is not set defaulting to default.rviz\e[0m"
+
     # Create Openbox application configuration
     mkdir -p /etc/xdg/openbox
     cat >/etc/xdg/openbox/rc.xml <<'EOF'
@@ -30,7 +36,7 @@ source /opt/autoware/setup.bash
 if [ -n "$RVIZ_CONFIG" ]; then
     exec rviz2 -d "$RVIZ_CONFIG"
 else
-    exec rviz2
+    exec rviz2 -d "$(find-pkg-share rviz2)/share/rviz2/launch/default.rviz"
 fi
 EOF
     chmod +x /usr/local/bin/start-rviz2.sh
@@ -39,7 +45,7 @@ EOF
 
     # Configure VNC password
     mkdir -p ~/.vnc
-    echo "$VNC_PASSWORD" | vncpasswd -f >~/.vnc/passwd && chmod 600 ~/.vnc/passwd
+    echo "$WEB_PASSWORD" | vncpasswd -f >~/.vnc/passwd && chmod 600 ~/.vnc/passwd
 
     # Start VNC server with Openbox
     echo "Starting VNC server with Openbox..."
@@ -62,9 +68,9 @@ EOF
 
     # Print info
     echo -e "\033[32m-------------------------------------------------------------------------\033[0m"
-    echo -e "\033[32mBrowser interface available at local address http://$(hostname -I | cut -d' ' -f1):6080/vnc.html?resize=scale&password=${VNC_PASSWORD}&autoconnect=true\033[0m"
+    echo -e "\033[32mBrowser interface available at local address http://$(hostname -I | cut -d' ' -f1):6080/vnc.html?resize=scale&password=${WEB_PASSWORD}&autoconnect=true\033[0m"
     if ping -c 1 8.8.8.8 >/dev/null 2>&1; then
-        echo -e "\033[32mIf you have a static public ip you can access it on WEB at http://$(curl -s ifconfig.me):6080/vnc.html?resize=scale&password=${VNC_PASSWORD}&autoconnect=true\033[0m"
+        echo -e "\033[32mIf you have a static public ip you can access it on WEB at http://$(curl -s ifconfig.me):6080/vnc.html?resize=scale&password=${WEB_PASSWORD}&autoconnect=true\033[0m"
     else
         echo -e "\033[31mNo internet connection available\033[0m"
     fi
@@ -72,7 +78,7 @@ EOF
 }
 
 # shellcheck disable=SC1090
-[ "$VNC_ENABLED" == "true" ] && configure_vnc
+[ "$WEB_ENABLED" == "true" ] && configure_vnc
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/opt/autoware/setup.bash"
 exec "$@"
