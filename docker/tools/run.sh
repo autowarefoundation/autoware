ce46b2e53d99e8937f10bea1fbe7fc9133f1d01f
Oguz Ozturk <oguzkaganozt@gmail.com>
2025-01-31 21:45:47 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/tools/run.sh
+++ docker/tools/run.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
+# docker run -it --rm \
+#     --net=host \
+#     --platform linux/amd64 \
+#     --name visualizer \
+#     --env WEB_ENABLED=true \
+#     --env WEB_PASSWORD=openadkit \
+#     ghcr.io/autowarefoundation/autoware:visualizer-20250130-amd64
+
+
 docker run -it --rm \
     --net=host \
     --platform linux/amd64 \
-    --name web-visualizer \
-    --env VNC_ENABLED=true \
-    --env VNC_PASSWORD=openadkit \
-    ghcr.io/autowarefoundation/autoware:web-visualizer-20250130-amd64
+    --name simulator \
+    ghcr.io/autowarefoundation/autoware:simulator-20250130-amd64
