8bccbe7af04762ab6e66b448d91297196e849d4d
Yugo Takano <149354988+tfnh621@users.noreply.github.com>
2025-04-10 09:05:15 +0900

    fix(docker): replace `/etc/localtime` mount with `TZ` env to avoid apt tzdata upgrade failure (#5999)
    * refactor: use TZ environment variable instead of mounting /etc/localtime
    
    * Update docker/run.sh
    
    ---------
    
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- docker/run.sh
+++ docker/run.sh
@@ -197,7 +197,7 @@ main() {
     # Launch the container
     set -x
     docker run -it --rm --net=host ${GPU_FLAG} ${USER_ID} ${MOUNT_X} \
-        -e XAUTHORITY=${XAUTHORITY} -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e NVIDIA_DRIVER_CAPABILITIES=all -v /etc/localtime:/etc/localtime:ro \
+        -e XAUTHORITY=${XAUTHORITY} -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e NVIDIA_DRIVER_CAPABILITIES=all -e TZ="$(cat /etc/timezone)" \
         ${WORKSPACE} ${MAP} ${DATA} ${IMAGE} \
         ${LAUNCH_CMD}
 }
